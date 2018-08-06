11da10a060062fd493a72f0ce85da1a04ed34723
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-08-06 10:21:27 -0400

    Separated test data from the main code
    Former-commit-id: 79cd7cdc6d391b7453bec0954a4d2a2245ebe077

--- ornl/sans/sns/eqsans/beam_finder.py
+++ ornl/sans/sns/eqsans/beam_finder.py
@@ -1,37 +1,13 @@
 
-import os
-import sys
-
-from ornl.sans.sns.eqsans.parameters import get_parameters
-
-
-from pprint import pprint
-
-from dotenv import load_dotenv
-load_dotenv()
-
-sys.path.append(os.getenv("MANTID_PATH"))
 from mantid.simpleapi import (
     Load, SANSMaskDTP, Integration, FindCenterOfMassPosition)
 
-# beam center file
-FILENAME = os.path.abspath(os.path.join(os.getenv('DATA_DIRECTORY'), 'eqsans', 'EQSANS_68183_event.nxs'))
-TUBES_TO_MASK = "1,48,53,54,85,123,130,137" # From 1 to 192
-    
 
-def direct_beam_center():
-    ws = Load(FILENAME)
-    SANSMaskDTP(InputWorkspace=ws, Tube=TUBES_TO_MASK)
+def direct_beam_center(filename, tubes_to_mask):
+    ws = Load(filename)
+    SANSMaskDTP(InputWorkspace=ws, Tube=tubes_to_mask)
     # Flatten TOF
     ws_flattened = Integration(InputWorkspace=ws)
     center = FindCenterOfMassPosition(InputWorkspace=ws_flattened)
     center_x, center_y = center
     return center_x, center_y
-    # TO Center the instrument
-    #MoveInstrumentComponent(Workspace=ws, ComponentName='detector1', X=-center_x, Y=-center_y)
-
-
-
-
-if __name__ == "__main__":
-    direct_beam_center()

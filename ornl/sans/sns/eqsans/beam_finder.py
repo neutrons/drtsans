fbd2cad47ff0c5d66dc8c281866fcd0576c8ca0a
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-10-05 17:42:20 -0400

    Tested when tubes are none
    Former-commit-id: cb6beea9c361726d7be7bef8624512b714335aa0

--- ornl/sans/sns/eqsans/beam_finder.py
+++ ornl/sans/sns/eqsans/beam_finder.py
@@ -3,9 +3,10 @@ from mantid.simpleapi import (
     LoadEventNexus, SANSMaskDTP, Integration, FindCenterOfMassPosition)
 
 
-def direct_beam_center(filename, tubes_to_mask):
+def direct_beam_center(filename, tubes_to_mask=None):
     ws = LoadEventNexus(filename)
-    SANSMaskDTP(InputWorkspace=ws, Tube=tubes_to_mask)
+    if tubes_to_mask is not None:
+        SANSMaskDTP(InputWorkspace=ws, Tube=tubes_to_mask)
     # Flatten TOF
     ws_flattened = Integration(InputWorkspace=ws)
     center = FindCenterOfMassPosition(InputWorkspace=ws_flattened)

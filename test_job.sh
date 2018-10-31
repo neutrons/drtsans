332b66ee698c327335e4348071210d25579d5b1f
Jose Borreguero <borreguero@gmail.com>
2018-10-31 11:39:08 -0400

    Refs #24 specify only unit tests
    Former-commit-id: c3493e9405663da459621ba5b47baa28058ab165

--- test_job.sh
+++ test_job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 v=$1  # python version
+test_scope=$2  # 'unit' or 'integration'
 module load mantid/ORNL_SANS_py${v}
 virtualenv -p python${v} testenv${v}
 source testenv${v}/bin/activate
 pip install -r requirements_test.txt
-pytest -v
+pytest -v tests/${test_scope}

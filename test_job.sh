e6618b21a80859dcc5f40ca01281b88b7a5af1f6
Jose Borreguero <borreguero@gmail.com>
2018-10-31 12:02:28 -0400

    Refs #25 fix some minor errors
    Former-commit-id: 170dcc92ad30e21489775a2f2ba217dad818b04f

--- test_job.sh
+++ test_job.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-N_SUB=4  # number of python subprocesses
 PYTHON_VERSION=$1  # python version
 TEST_SCOPE=$2  # 'unit' or 'integration'
 
+N_SUB=4  # number of python subprocesses
+
 module load mantid/ORNL_SANS_py${PYTHON_VERSION}
-virtualenv -p python${${PYTHON_VERSION}} testenv${PYTHON_VERSION}
-source testenv${v}/bin/activate
+virtualenv -p python${PYTHON_VERSION} testenv${PYTHON_VERSION}
+source testenv${PYTHON_VERSION}/bin/activate
 pip install -r requirements_test.txt
 pytest -v -d --tx ${N_SUB}*popen//python=python${PYTHON_VERSION} tests/${TEST_SCOPE}

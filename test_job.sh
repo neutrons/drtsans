22a8836ce4a80437cb5f100b3e5402628d7f6997
Jose Borreguero <borreguero@gmail.com>
2018-10-31 12:12:23 -0400

    Refs #25 try different grouping
    Former-commit-id: ecd9523770e678887144c1352d524fe75fa92ec8

--- test_job.sh
+++ test_job.sh
@@ -3,11 +3,10 @@
 PYTHON_VERSION=$1  # python version
 TEST_SCOPE=$2  # 'unit' or 'integration'
 
-N_SUB=4  # number of python subprocesses
+N_SUB=8  # number of python subprocesses
 
 module load mantid/ORNL_SANS_py${PYTHON_VERSION}
 virtualenv -p python${PYTHON_VERSION} testenv${PYTHON_VERSION}
 source testenv${PYTHON_VERSION}/bin/activate
 pip install -r requirements_test.txt
-#pytest -v -d --tx ${N_SUB}*popen//python=python${PYTHON_VERSION} tests/${TEST_SCOPE}
-pytest -v -n ${N_SUB} --dist=loadscope tests/${TEST_SCOPE}
+pytest -v -d --tx ${N_SUB}*popen//python=python${PYTHON_VERSION} tests/${TEST_SCOPE}

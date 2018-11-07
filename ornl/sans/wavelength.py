485bc894716b6cb3dda09e31c6ff07ce5263b74e
Jose Borreguero <borreguero@gmail.com>
2018-11-06 19:04:26 -0500

    Refs #17 remove unused import
    Former-commit-id: c6b96a2be76c17836b83944a57c1f855ebb0f590

--- ornl/sans/wavelength.py
+++ ornl/sans/wavelength.py
@@ -1,7 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 
 from sortedcontainers import SortedList
-from functools import singledispatch, update_wrapper
 
 
 class Wband(object):

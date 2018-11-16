ec23d5e22c4144078f5a6e8e049043bb004006d4
Jose Borreguero <borreguero@gmail.com>
2018-11-16 09:53:30 -0500

    Refs #29 unit test for the decorator
    Former-commit-id: ea66335411e0110a756cffc739a2d29cebd8a87f

---
+++ tests/unit/new/ornl/test_settings.py
@@ -0,0 +1,29 @@
+from __future__ import (absolute_import, division, print_function)
+
+import pytest
+from ornl.settings import namedtuplefy
+
+def test_namedtuplefy():
+
+    @namedtuplefy
+    def foo(x):
+        return dict(foo=x)
+
+    @namedtuplefy
+    def goo(x):
+        return dict(goo=x)
+
+    y1 = foo(42)
+    z1 = goo(24)
+    y2 = foo(41)
+    z2 = goo(21)
+
+    assert type(y1) == type(y2)
+    assert type(z1) == type(z2)
+    assert type(y1) != type(z1)
+    assert type(y2) != type(z2)
+
+
+if __name__ == '__main__':
+    test_namedtuplefy()
+    #pytest.main()

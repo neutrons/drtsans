a462de517c71ff820def9af4b8693f669d723e01
Jose Borreguero <borreguero@gmail.com>
2018-11-30 17:51:25 -0500

    Refs #32 source and test for wavelength clippings
    Former-commit-id: d74a0aaa7e9f1904ff0ae625911d5bbe9dfe75a2

--- ornl/sans/sns/eqsans/correct_frame.py
+++ ornl/sans/sns/eqsans/correct_frame.py
@@ -2,23 +2,14 @@ from __future__ import (absolute_import, division, print_function)
 
 import numpy as np
 
+from mantid.simpleapi import ConvertUnits, Rebin
+
 from ornl.sans.samplelogs import SampleLogs
 from ornl.sans.sns.eqsans.chopper import EQSANSDiskChopperSet
 from ornl.sans.frame_mode import FrameMode
 from ornl.sans import wavelength as wlg
 from ornl.settings import namedtuplefy
-from ornl.sans.geometry import sample_source_distance, source_detector_distance
-
-
-def replace_tofs(ws):
-    for i in range(ws.getNumberHistograms()):
-        sp = ws.getSpectrum(i)
-        tofs = sp.getTofs()
-        pts = np.asarray(sp.getPulseTimes())
-        sp.clear(False)
-        for tof, pt in zip(tofs, pts):
-            sp.addEventQuickly(tof, pt)
-    return ws
+from ornl.sans.geometry import source_detector_distance
 
 
 @namedtuplefy
@@ -53,10 +44,42 @@ def transmitted_bands(ws):
 
 
 @namedtuplefy
-def transmitted_bands_clipped(ws, ltc, htc, ltc2=None, htc2=None):
+def limiting_tofs(ws, sdd):
+    r"""
+    Minimimum and maximum TOF's for neutrons coming from the lead and skip
+    pulses, assuming a detector were placed at `distance`.
+
+    Parameters
+    ----------
+    ws: MatrixWorkspace
+            Input Workspace containing all necessary info
+    sdd: float
+        Distance from source to detector, in meters
+
+    Returns
+    -------
+    namedtuple
+        Fields of the namedtuple
+        lead: tuple, tof_min and tof_max for the neutrons of the lead pulse
+        skip: tuple, tof_min and tof_max for the neutrons of the skip
+            pulse. `None` if the frame mode is 'non-skipping'
+    """
+    ch = EQSANSDiskChopperSet(ws)  # object representing the four choppers
+    bands = transmitted_bands(ws)
+    lead = (wlg.tof(bands.lead.min, sdd, ch.pulse_width),
+            wlg.tof(bands.lead.max, sdd))
+    skip = None if ch.frame_mode == FrameMode.not_skip else \
+        (wlg.tof(bands.skip.min, sdd, ch.pulse_width),
+         wlg.tof(bands.skip.max, sdd))
+    return dict(lead=lead, skip=skip)
+
+
+@namedtuplefy
+def transmitted_bands_clipped(ws, sdd, ltc, htc, interior_clip=False):
     r"""
     Wavelength bands of the lead and skipped pulses transmitted by
-    the choppers taking into account the TOF clipping cutoffs
+    the choppers taking into account the TOF clippings for neutrons
+    arriving at the detector, assuming a detector were placed at `distance`.
 
     Parameters
     ----------
@@ -64,17 +87,15 @@ def transmitted_bands_clipped(ws, ltc, htc, ltc2=None, htc2=None):
         Data workspace
     ltc: float
         trim neutrons of the leading pulse with a TOF smaller than the
-        minimal TOF plus this value
+        minimal TOF plus this value. Units in micro-seconds
+    sdd: float
+        Distance from source to detector, in meters
     htc: float
-        trim neutrons f the leading pulse with TOF bigger than the maxima
-         TOF minus this value.
-    ltc2: float
-        as `ltc` but for neutrons of the skipped pulse. If None, then we use
-        `ltc`
-    htc2: float
-        as `htc` but for neutrons of the skipped pulse. If None, then we use
-        `htc`
-
+        trim neutrons of the leading pulse with TOF bigger than the maximal
+        TOF minus this value.  Units in micro-seconds
+    interior_clip: False
+        If True, also trim slow neutrons from the lead pulse (using `htc`) and
+        fast neutrons from the skip pulse (using `ltc`)
     Returns
     -------
     namedtuple
@@ -83,32 +104,22 @@ def transmitted_bands_clipped(ws, ltc, htc, ltc2=None, htc2=None):
         - skipped, Wband for the skipped pulse. None if not operating in
             the skipped frame mode
     """
-    sdd = source_detector_distance(ws)
     ch = EQSANSDiskChopperSet(ws)  # object representing the four choppers
-
-    def clip_band(band, lc, hc):
-        tof_min = wlg.tof(band.min, sdd, ch.pulse_width) + lc
-        tof_max = wlg.tof(band.max, sdd) - hc
-        return wlg.Wband(wlg.from_tof(tof_min, sdd, ch.pulse_width),
-                         wlg.from_tof(tof_max, sdd))
-
+    lwc = wlg.from_tof(ltc, sdd, ch.pulse_width)  # low wavelength clip
+    hwc = wlg.from_tof(htc, sdd)  # high wavelength clip
     bands = transmitted_bands(ws)
-    lead_band = clip_band(bands.lead, ltc, htc)
-    if bands.skip is None:
-        skip_band = None
+    if ch.frame_mode == FrameMode.not_skip:
+        lead = wlg.Wband(bands.lead.min + lwc, bands.lead.max - hwc)
+        skip = None
     else:
-        if ltc2 is None:
-            ltc2 = ltc
-        if htc2 is None:
-            htc2 = htc
-        skip_band = clip_band(bands.skip, ltc2, htc2)
-
-    return dict(lead=lead_band, skip=skip_band)
+        l_i = bands.lead.max - hwc if interior_clip is True else bands.lead.max
+        l_a = bands.skip.min + lwc if interior_clip is True else bands.skip.min
+        lead = wlg.Wband(bands.lead.min + lwc, l_i)
+        skip = wlg.Wband(l_a, bands.skip.max - hwc)
+    return dict(lead=lead, skip=skip)
 
 
-@namedtuplefy
-def correct_frame(ws, ltc=0, htc=0, ltc2=None, htc2=None,
-                  component='detector1'):
+def correct_frame(ws, s2c):
     r"""
     Assign the correct TOF to each event.
 
@@ -119,21 +130,15 @@ def correct_frame(ws, ltc=0, htc=0, ltc2=None, htc2=None,
     ----------
     ws: EventsWorkspace
         Data workspace
-    component: str
-        Either 'detector1' or 'monitor1' to correct time of flights in the
-        detector bank or in the monitor
+    s2c: float
+        Distance from source to detecting component (detector1, monitor) in
+        meters
     ltc: float
         trim neutrons of the leading pulse with a TOF smaller than the
         minimal TOF plus this value
     htc: float
         trim neutrons f the leading pulse with TOF bigger than the maxima
          TOF minus this value.
-    ltc2: float
-        as `ltc` but for neutrons of the skipped pulse. If None, then we use
-        `ltc`
-    htc2: float
-        as `htc` but for neutrons of the skipped pulse. If None, then we use
-        `htc`
 
     Returns
     -------
@@ -144,80 +149,87 @@ def correct_frame(ws, ltc=0, htc=0, ltc2=None, htc2=None,
         - skip_band: WBand wavelength band for the skip pulse. `None` if not
             working in frame-skipping mode
     """
-    if ltc2 is None:
-        ltc2 = ltc
-    if htc2 is None:
-        htc2 = htc
-
     sl = SampleLogs(ws)
     pulse_period = 1.e6 / sl.frequency.value.mean()  # 10^6/60 micro-seconds
     ch = EQSANSDiskChopperSet(ws)  # object representing the four choppers
-
     # The TOF values recorded are never be bigger than the frame width,
     # which is also the choppers' rotational period.
-    first_chopper = ch[0]
-    frame_width = first_chopper.period
-
-    instrument = ws.getInstrument()
-    if component == 'detector1':  # Find distances from source to detectors
-        sample = instrument.getSample()
-        s2s = sample_source_distance(ws, units='m')
-        s2c_i = [s2s + instrument.getDetector(i).getDistance(sample)
-                 for i in range(ws.getNumberHistograms())]
-        s2c = min(s2c_i)  # nominal distance from source to component
-    elif component == 'monitor1':  # Find distance from source to monitor
-        source = instrument.getSource()
-        s2c = instrument.getDetector(-1).getDistance(source)
-        s2c_i = [s2c, ]
-
-    bands = transmitted_bands(ws)
-
-    def tof_min_max(band, distance):
-        return (wlg.tof(band.min, distance, ch.pulse_width),
-                wlg.tof(band.max, s2c))
+    frame_width = ch.period  # either 10^6/60 or 10^6/30 micro-seconds
 
     # Find how many frame widths elapsed from the time the neutrons of the
     # lead pulse were emitted and the time the neutrons arrived to the
     # detector bank. This time must be added to the stored TOF values
-    tof_min, tof_max = tof_min_max(bands.lead, s2c)
-    frames_offset_time = frame_width * int((tof_min + tof_max)
-                                           / (2 * frame_width))
-
+    tof_min, tof_max = limiting_tofs(ws, s2c).lead
+    frames_offset_time = frame_width * int(tof_min / frame_width)
     for i in range(ws.getNumberHistograms()):
         sp = ws.getSpectrum(i)
         tofs = sp.getTofs()
         if len(tofs) == 0:
             continue  # no events found in this detector
         tofs += frames_offset_time  # shift times to the correct frame
-        d = s2c_i[i]  # distance from source to detector
-        # TOF boundaries for the leading pulse
-        tof_min, tof_max = tof_min_max(bands.lead, d)
         # TOF values smaller than that of the fastest neutrons have been
         # 'folded' by the data acquisition system. They must be shifted
         tofs[np.where(tofs < tof_min)] += frame_width
+        # Events from the skipped pulse are delayed by one pulse period
         if ch.frame_mode == FrameMode.skip:
-            # Neutrons slower than the slowest neutron of the leading pulse
-            # originated in the skipped pulse. Their TOFs must be
-            # increased by a pulse period
-            tofs[np.where(tofs > tof_max)] += pulse_period
-        else:
-            # Neutrons stored as slower than the slowest neutron are actually
-            # fast neutrons whose TOFs have been shifted by a frame width
-            tofs[np.where(tofs > tof_max)] -= frame_width
-        # Discard events in the leading pulse outside the valid TOF range
-        valid_range = np.where((tofs > tof_min + ltc) &
-                               (tofs < tof_max - htc))[0]
-        if ch.frame_mode == FrameMode.skip:
-            # Discard events in the skipped pulse utside the valid TOF range
-            tof_min, tof_max = tof_min_max(bands.skip, d)
-            r12 = np.where((tofs > tof_min + ltc2) &
-                           (tofs < tof_max - htc2))[0]
-            valid_range = np.append(valid_range, r12)
-        # reset the event list with only the valid TOF's
-        valid_tofs = tofs[valid_range]
-        valid_pts = np.asarray(sp.getPulseTimes())[valid_range]
+            tofs[np.where(tofs > tof_min + pulse_period)] += pulse_period
+        pulse_times = list(sp.getPulseTimes())
         sp.clear(False)
-        for tof, pt in zip(valid_tofs, valid_pts):
+        for tof, pt in zip(tofs, pulse_times):
             sp.addEventQuickly(tof, pt)
+    # Amend the logs
+    sl.is_frame_skipping = True if ch.frame_mode == FrameMode.skip else False
 
-    return dict(ws=ws, lead_band=bands.lead, skip_band=bands.skip)
+
+def correct_detector_frame(ws):
+    correct_frame(ws, source_detector_distance(ws, units='m'))
+
+
+def convert_to_wavelength(ws, bands, bin_width, out_ws, events=False):
+    r"""
+    Convert a time-of-fligth events workspace to a wavelength workpsace
+
+    Parameters
+    ----------
+    ws: EventsWorkspace
+        Input workspace, after TOF frame correction
+    bands: namedtuple
+        Output of running `transmitted_bands_clipped` on the workspace
+    bin_width: float
+        Bin width in Angstroms
+    out_ws: str
+        Name of the output workspace
+    events: bool
+        Do we preserve events?
+    Returns
+    -------
+    MatrixWorspace
+        EventsWorkspace or Matrix2DWorkspace
+    """
+    fm = (EQSANSDiskChopperSet(ws).frame_mode == FrameMode.skip)  # frame skip?
+
+    # Convert to Wavelength and rebin
+    _ws = ConvertUnits(ws, Target='Wavelength', Emode='Elastic')
+    w_min = bands.lead.min
+    w_max = bands.lead.max if fm is False else bands.skip.max
+    _ws = Rebin(_ws, Params=[w_min, bin_width, w_max],
+                PreserveEvents=events, OutputWorkspace=out_ws)
+
+    # Discard neutrons in between bands.lead.max and bands.skip.min
+    if fm is True:
+        to_zero = np.where((_ws.dataX(0) > bands.lead.max) &
+                           (_ws.dataX(0) < bands.skip.min))[0]
+        for i in range(_ws.getNumberHistograms()):
+            _ws.dataY(i)[to_zero] = 0.0
+            _ws.dataE(i)[to_zero] = 0.0
+
+    # Insert bands information in the logs
+    sl = SampleLogs(_ws)
+    sl.wavelength_min, sl.wavelength_max = w_min, w_max
+    if fm is True:
+        sl.wavelength_lead_min = bands.lead.min
+        sl.wavelength_lead_max = bands.lead.max
+        sl.wavelength_skip_min = bands.skip.min
+        sl.wavelength_skip_max = bands.skip.max
+
+    return _ws
--- tests/unit/new/ornl/sans/sns/eqsans/test_correct_frame.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_correct_frame.py
@@ -1,10 +1,12 @@
 from __future__ import (absolute_import, division, print_function)
 
 import pytest
+from pytest import approx
 from numpy.testing import assert_almost_equal
 from mantid.simpleapi import Load
 from ornl.sans.sns.eqsans import correct_frame as cf
 from ornl.settings import amend_config
+from ornl.sans.geometry import source_detector_distance
 
 
 def test_transmitted_bands():
@@ -16,6 +18,35 @@ def test_transmitted_bands():
                             (2.48, 6.78), decimal=2)
         assert_almost_equal((bands.skip.min, bands.skip.max),
                             (10.90, 15.23), decimal=2)
+
+
+def test_transmitted_bands_clipped():
+    with amend_config({'instrumentName': 'EQSANS',
+                       'datasearch.searcharchive': 'on'}):
+        ws = Load(Filename='EQSANS_86217')
+        sdd = source_detector_distance(ws, units='m')
+        bands_0 = cf.transmitted_bands_clipped(ws, sdd, 0.0, 0.0)
+        lwc, hwc = (0.139, 0.560)  # expected clippings
+        # With no interior clipping
+        bands = cf.transmitted_bands_clipped(ws, sdd, 500, 2000,
+                                             interior_clip=False)
+        # Check clippings for the lead pulse
+        b1_0, b2_0 = bands_0.lead.min, bands_0.lead.max
+        b1, b2 = bands.lead.min, bands.lead.max
+        assert (b1, b2) == approx((b1_0 + lwc, b2_0), 0.01)
+        # Check clippings for the skip pulse
+        b1_0, b2_0 = bands_0.skip.min, bands_0.skip.max
+        b1, b2 = bands.skip.min, bands.skip.max
+        assert (b1, b2) == approx((b1_0, b2_0 - hwc), 0.01)
+        # With interior clipping
+        bands = cf.transmitted_bands_clipped(ws, sdd, 500, 2000,
+                                             interior_clip=True)
+        b1_0, b2_0 = bands_0.lead.min, bands_0.lead.max
+        b1, b2 = bands.lead.min, bands.lead.max
+        assert (b1, b2) == approx((b1_0 + lwc, b2_0 - hwc), 0.01)
+        b1_0, b2_0 = bands_0.skip.min, bands_0.skip.max
+        b1, b2 = bands.skip.min, bands.skip.max
+        assert (b1, b2) == approx((b1_0 + lwc, b2_0 - hwc), 0.01)
 
 
 if __name__ == '__main__':

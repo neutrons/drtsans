15e0e5d025d95e4d8bf575e84777c9ef7d97a707
Jose Borreguero <borreguero@gmail.com>
2018-12-05 19:21:31 -0500

    Refs #31 do not namedtuplefy the wss fixture
    Former-commit-id: cc265d634f6c24ecfcd0781d491d626561feec1e

--- tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
@@ -5,12 +5,11 @@ import pytest
 from pytest import approx
 from mantid.simpleapi import Load, SumSpectra, LoadNexus, CompareWorkspaces
 
-from ornl.settings import namedtuplefy, amend_config, uwn
+from ornl.settings import amend_config, uwn
 from ornl.sans.samplelogs import SampleLogs
 import ornl.sans.sns.eqsans.dark_current as dkc
 
 
-@namedtuplefy
 @pytest.fixture(scope='module')
 def wss(refd):
     with amend_config({'instrumentName': 'EQSANS',
@@ -22,17 +21,17 @@ def wss(refd):
 
 
 def test_duration(wss):
-    assert dkc.duration(wss.dark).value == approx(7200.0, abs=1.0)
+    assert dkc.duration(wss['dark']).value == approx(7200.0, abs=1.0)
 
 
 def test_counts_in_detector(wss):
-    y, e = dkc.counts_in_detector(wss.dark)
+    y, e = dkc.counts_in_detector(wss['dark'])
     assert max(y) == 67.0
     assert (y[0], e[0]) == (0.0, 1.0)
 
 
 def test_normalise_to_workspace(wss, refd):
-    _w0 = dkc.normalise_to_workspace(wss.dark, wss.data, uwn())
+    _w0 = dkc.normalise_to_workspace(wss['dark'], wss['data'], uwn())
     _w1 = SumSpectra(_w0, OutputWorkspace=uwn())
     assert SampleLogs(_w1).normalizing_duration.value == 'duration'
     name = pjn(refd.new.eqsans, 'test_dark_current', 'dark_norm_sum.nxs')
@@ -44,7 +43,7 @@ def test_normalise_to_workspace(wss, refd):
 def test_subtract_normalized_dark(wss, refd):
     name = pjn(refd.new.eqsans, 'test_dark_current', 'dark_norm_sum.nxs')
     _dark_normalized = LoadNexus(name, OutputWorkspace=uwn())
-    _w0 = dkc.subtract_normalized_dark(wss.data, _dark_normalized, uwn())
+    _w0 = dkc.subtract_normalized_dark(wss['data'], _dark_normalized, uwn())
     assert SampleLogs(_w0).normalizing_duration.value == 'duration'
     _w1 = SumSpectra(_w0, OutputWorkspace=uwn())
     name = pjn(refd.new.eqsans, 'test_dark_current', 'data_minus_dark.nxs')

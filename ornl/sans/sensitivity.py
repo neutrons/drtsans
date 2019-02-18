607e84a4a2bb945238b60608185f3fc4daf058b3
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-02-18 15:11:28 -0500

    Backup temp commit
    Former-commit-id: 8d56cda3c0111aa3a36590b243b304aa435153e2

--- ornl/sans/sensitivity.py
+++ ornl/sans/sensitivity.py
@@ -2,34 +2,38 @@ from __future__ import absolute_import, division, print_function
 
 import numpy as np
 
+from collections import OrderedDict
 from mantid.kernel import Property, logger
 from mantid.simpleapi import CloneWorkspace
-from ornl.settings import unique_workspace_dundername
+from ornl.settings import unique_workspace_name
 
 
-def _get_tube(component, tube_idx):
-    """Gets a given tube component
+class Detector(object):
 
-    Parameters
-    ----------
-    component : Mantid Component
-        Component where the tube is situated.
-    tube_idx : int
-        The tube index in the detector usually from 0 to 191
+    def __init__(self, workspace, component_name):
+        self._workspace = workspace
+        self._component_name = component_name
+        
+    
+    def _detector_details(self):
+        i = workspace.getInstrument()
+        d = i.getComponentByName(component_name)
+        n_tubes = d.nelements()
+        
+        if d[0].nelements() == 1:
+            # Handles EQSANS
+            n_pixels = component[0][0].nelements()
+            first_det_id = component[0][0][0].getID()
+            last_det_id = component[n_tubes-1][0][n_pixels-1].getID()
+        else:
+            # Handles BioSANS/GPSANS
+            n_pixels = component[0].nelements()
+            first_det_id = component[0][0].getID()
+            last_det_id = component[n_tubes-1][n_pixels-1].getID()
+        
+        
 
-    Returns
-    -------
-    Mantid Component
-        Tube component
-    """
 
-    if component[0].nelements() <= 1:
-        # Handles EQSANS
-        tube = component[tube_idx][0]
-    else:
-        # Handles BioSANS/GPSANS
-        tube = component[tube_idx]
-    return tube
 
 
 def _interpolate_tube(x, y, e, detectors_masked, detectors_inf,
@@ -74,9 +78,6 @@ def _interpolate_tube(x, y, e, detectors_masked, detectors_inf,
 def interpolate_mask(flood_ws, polynomial_degree=1,
                      component_name='detector1', min_detectors_per_tube=50):
     '''Interpolates over the mask (usually the beamstop and the tube ends)
-    Assumptions:
-    - Monitors are in the beginning of the workspace
-    - IDs of the detectors are contiguous
 
     Parameters
     ----------
@@ -84,7 +85,7 @@ def interpolate_mask(flood_ws, polynomial_degree=1,
         input workspace. Normally a sensitivity file.
     polynomial_degree : int, optional
         Polynomial degree for the interpolation (the default is 1, which is a
-        linear fitting). A value of 0 calculates de average.
+        linear fitting).
     component_name : str, optional
         Component name to  (the default is 'detector1', which is the main
         detector)
@@ -109,35 +110,20 @@ def interpolate_mask(flood_ws, polynomial_degree=1,
     if not component:
         logger.error("Component not valid! {}".format(component_name))
         return
-
-    # find out in which WS index starts the monitor
-    # there is a `MatrixWorkspace.getDetectorIDToWorkspaceIndexMap` but it's
-    # not exposed to python :(
-    spectrum_info = flood_ws.spectrumInfo()
-    ws_index = 0
-    while spectrum_info.isMonitor(ws_index):
-        ws_index += 1
-    first_detector_index = ws_index
+    
+    detector_id_to_index = _get_detector_id_to_ws_index(flood_ws)
+    detector_ids = list(detector_id_to_index.keys())
+    ws_indices = list(detector_id_to_index.values())
+    ws_index = ws_indices[0]
 
     # Put the data in numpy arrays
     data_y = flood_ws.extractY()
     data_e = flood_ws.extractE()
 
-    # numpy array with all the detector IDs
-    detector_ids = np.array(
-        [flood_ws.getSpectrum(index).getDetectorIDs()[0] for index in range(
-            first_detector_index, flood_ws.getNumberHistograms())])
-
-    # dict of detector_id -> ws index
-    detector_id_to_index = {
-        id: index for (id, index) in zip(
-            detector_ids, range(first_detector_index,
-                                flood_ws.getNumberHistograms()))}
-
     number_of_tubes = component.nelements()
     # Lets get the output workspace
     output_ws = CloneWorkspace(
-        flood_ws, OutputWorkspace=unique_workspace_dundername(
+        flood_ws, OutputWorkspace=unique_workspace_name(
             prefix="__sensitivity_"))
 
     detector_info_input_ws = flood_ws.detectorInfo()
@@ -204,6 +190,14 @@ def interpolate_mask(flood_ws, polynomial_degree=1,
         # Another iteration
         ws_index += num_detectors_in_tube
     return output_ws
+
+def _get_detector_id_to_ws_index(flood_ws):
+    spectrum_info = flood_ws.spectrumInfo()
+    detector_id_to_index = OrderedDict(
+        (flood_ws.getSpectrum(ws_index).getDetectorIDs()[0], ws_index)
+        for ws_index in range(flood_ws.getNumberHistograms())
+        if not spectrum_info.isMonitor(ws_index))
+    return detector_id_to_index
 
 
 def inf_value_to_mask(ws):

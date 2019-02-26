2f80c5363c0d3c91c8c8ed5e42c6d64f1bd1a905
Mathieu Doucet <doucetm@ornl.gov>
2019-02-26 11:59:45 -0500

    refactor common code
    Former-commit-id: 8f3dfc6fef0a571c77d18f9aa16b17dbb2653779

--- ornl/sans/hfir/resolution.py
+++ ornl/sans/hfir/resolution.py
@@ -2,11 +2,20 @@ from __future__ import (absolute_import, division, print_function)
 import numpy as np
 
 from ornl.sans.samplelogs import SampleLogs
-
+from ornl.sans.resolution import dq2_geometry, dq2_gravity
 
 def q_resolution(ws):
     r"""
     Compute q resolution for the given reduced workspace.
+
+    The resolution can be computed by giving either a 1D I(q) or
+    2D I(qx, qy) to this function:
+
+    dq = q_resolution(ws_1d)
+    dqx, dqy = q_resolution(ws_2d)
+
+    In both cases, the returned numpy arrays are of the same dimensions
+    as the input array.
 
     Parameters
     ----------
@@ -54,72 +63,6 @@ def q_resolution(ws):
             dqy[i] = np.sqrt(dqy2_hfir(qy_mid[i], L1, L2,
                                        R1, R2, wl, dwl, theta))
         return dqx, dqy
-
-
-def dq2_geometry(L1, L2, R1, R2, wl, theta, pixel_size=0.007):
-    r"""
-    Geometry term for both dqx and dqy.
-
-    Parameters
-    ----------
-    ws: MatrixWorkspace
-        Input workspace
-    L1: float
-        source-to-sample distance (m)
-    L2: float
-        sample-to-detector distance (m)
-    R1: float
-        source aperture radius (m)
-    R2: float
-        sample aperture radius (m)
-    wl: float
-        wavelength mid-point (Angstrom)
-    theta: float
-        scattering angle (rad)
-    pixel_size: float
-        dimension of the pixel (m)
-
-    Returns
-    ------
-    float
-    """
-    dq2 = 0.25 * (L2 / L1 * R1)**2 + 0.25 * ((L1 + L2) / L1 * R2)**2 \
-        + pixel_size**2 / 12.0
-    return dq2 * (2.0 * np.pi * np.cos(theta)
-                  * np.cos(2.0 * theta)**2 / wl / L2)**2
-
-
-def dq2_gravity(L1, L2, wl, dwl, theta):
-    r"""
-    Gravity term for dqy.
-
-    Parameters
-    ----------
-    L1: float
-        source-to-sample distance (m)
-    L2: float
-        sample-to-detector distance (m)
-    wl: float
-        wavelength mid-point (Angstrom)
-    dwl: float
-        wavelength-spread (Angstrom)
-    theta: float
-        scattering angle (rad)
-    pixel_size: float
-        dimension of the pixel (m)
-
-    Returns
-    ------
-    float
-    """
-    h = 6.626e-34    # m^2 kg s^-1
-    m_n = 1.675e-27  # kg
-    g = 9.8          # m s^-2
-    B = g * m_n**2 * L2 * (L1 + L2) / 2 / h**2
-    dq2 = 2 / 3 * B**2 * wl**2 * dwl**2
-    dq2 *= (2.0 * np.pi * np.cos(theta) * np.cos(2.0 * theta)**2 / wl / L2)**2
-    # Converting from A^2 / m^4 to 1 / A^2
-    return dq2 * 1.0e-40
 
 
 def dqx2_hfir(qx, L1, L2, R1, R2, wl, dwl, theta=None, pixel_size=0.011):
---
+++ ornl/sans/resolution.py
@@ -0,0 +1,71 @@
+"""
+    Resolution calculations common to all SANS
+"""
+from __future__ import (absolute_import, division, print_function)
+import numpy as np
+
+
+def dq2_geometry(L1, L2, R1, R2, wl, theta, pixel_size=0.007):
+    r"""
+    Geometry term for both dqx and dqy.
+
+    Parameters
+    ----------
+    ws: MatrixWorkspace
+        Input workspace
+    L1: float
+        source-to-sample distance (m)
+    L2: float
+        sample-to-detector distance (m)
+    R1: float
+        source aperture radius (m)
+    R2: float
+        sample aperture radius (m)
+    wl: float
+        wavelength mid-point (Angstrom)
+    theta: float
+        scattering angle (rad)
+    pixel_size: float
+        dimension of the pixel (m)
+
+    Returns
+    ------
+    float
+    """
+    dq2 = 0.25 * (L2 / L1 * R1)**2 + 0.25 * ((L1 + L2) / L1 * R2)**2 \
+        + pixel_size**2 / 12.0
+    return dq2 * (2.0 * np.pi * np.cos(theta)
+                  * np.cos(2.0 * theta)**2 / wl / L2)**2
+
+
+def dq2_gravity(L1, L2, wl, dwl, theta):
+    r"""
+    Gravity term for dqy.
+
+    Parameters
+    ----------
+    L1: float
+        source-to-sample distance (m)
+    L2: float
+        sample-to-detector distance (m)
+    wl: float
+        wavelength mid-point (Angstrom)
+    dwl: float
+        wavelength-spread (Angstrom)
+    theta: float
+        scattering angle (rad)
+    pixel_size: float
+        dimension of the pixel (m)
+
+    Returns
+    ------
+    float
+    """
+    h = 6.626e-34    # m^2 kg s^-1
+    m_n = 1.675e-27  # kg
+    g = 9.8          # m s^-2
+    B = g * m_n**2 * L2 * (L1 + L2) / 2 / h**2
+    dq2 = 2 / 3 * B**2 * wl**2 * dwl**2
+    dq2 *= (2.0 * np.pi * np.cos(theta) * np.cos(2.0 * theta)**2 / wl / L2)**2
+    # Converting from A^2 / m^4 to 1 / A^2
+    return dq2 * 1.0e-40

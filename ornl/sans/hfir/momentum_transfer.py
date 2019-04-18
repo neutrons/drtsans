6740f506601f9b5c4f4bf95b12e0599c46497934
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-04-18 13:25:16 -0400

    bin in Q2D misses testing
    Former-commit-id: 29ea3a30d3c0731206d59be0fbbe28cc15b4f366

--- ornl/sans/hfir/momentum_transfer.py
+++ ornl/sans/hfir/momentum_transfer.py
@@ -4,7 +4,7 @@ from mantid.simpleapi import CreateWorkspace, GroupWorkspaces
 import numpy as np
 
 
-def bin_into_q2d(ws):
+def bin_into_q2d(ws, out_ws_prefix="ws"):
 
     # TODO: get this somewhere
     X_SIZE_DET = 192
@@ -17,32 +17,80 @@ def bin_into_q2d(ws):
 
     # All returned arrays have the same shape. They are a list of spectrum = 1D
     qx, qy, dqx, dqy = resolution.q_resolution_per_pixel(ws)
+
+    # Get rid of the monitors
     qx, qy, dqx, dqy = qx[N_MONITORS:], qy[N_MONITORS:], dqx[N_MONITORS:], \
         dqy[N_MONITORS:]
+    i, i_sigma = i[N_MONITORS:], i_sigma[N_MONITORS:]
+    # get rid of the original bins, transform in 1D
+    i, i_sigma = i[:, 0], i_sigma[:, 0]
 
-    qx_bin_centers = np.linspace(np.abs(qx).min(), np.abs(qx).max(), X_SIZE_DET)
-    qy_bin_centers = np.linspace(np.abs(qy).min(), np.abs(qy).max(), Y_SIZE_DET)
+    # qx_bin_centers = np.linspace(np.abs(qx).min(), np.abs(qx).max(), X_SIZE_DET)
+    # qy_bin_centers = np.linspace(np.abs(qy).min(), np.abs(qy).max(), Y_SIZE_DET)
 
-    qx_bin_dist = abs(qx_bin_centers[1]-qx_bin_centers[0])
-    qx_bin_edges = qx_bin_centers - qx_bin_dist/2
-    qx_bin_edges = np.append(qx_bin_edges, qx_bin_centers[-1] + qx_bin_dist/2)
+    # qx_bin_dist = abs(qx_bin_centers[1]-qx_bin_centers[0])
+    # qx_bin_edges = qx_bin_centers - qx_bin_dist/2
+    # qx_bin_edges = np.append(qx_bin_edges, qx_bin_centers[-1] + qx_bin_dist/2)
 
-    qx_histo, _ = np.histogram(qx, bins=qx_bin_edges)
+    # Number of bins is the number of pixels in each diraction
+    counts, qx_bin_edges, qy_bin_edges = np.histogram2d(
+        qx, qy, bins=[X_SIZE_DET, Y_SIZE_DET], weights=i
+    )
+    counts, dqx_bin_edges, dqy_bin_edges = np.histogram2d(
+        dqx, dqy, bins=[X_SIZE_DET, Y_SIZE_DET], weights=i
+    )
 
-    iq, _ = np.histogram(q, bins=bins)
-    dqx, _ = np.histogram(q, bins=bins, weights=_dqx)
-    dqy, _ = np.histogram(q, bins=bins, weights=_dqy)
+    qy_bin_centers = (qy_bin_edges[1:] + qy_bin_edges[:-1]) / 2.0
+    dqy_bin_centers = (dqy_bin_edges[1:] + dqy_bin_edges[:-1]) / 2.0
 
+    # Grids for I, dqx, dqy
+    qx_bin_edges_grid, qy_bin_centers_grid = np.meshgrid(
+        qx_bin_edges, qy_bin_centers)
+    i_grid = i.reshape((X_SIZE_DET, Y_SIZE_DET))
+    i_sigma_grid = i_sigma.reshape((X_SIZE_DET, Y_SIZE_DET))
+
+    dqx_bin_centers_grid = np.tile(dqx_bin_edges, (len(dqy_bin_centers), 1))
+    dqy_bin_centers_grid = np.tile(np.array([dqy_bin_centers]).transpose(),
+                                   (1, len(dqx_bin_edges)))
     # Q WS
-    iqxqy_ws2d = CreateWorkspace(
-        DataX=qx_bin_grid,  # 2D
-        DataY=i,  # 2D
-        DataE=i_sigma,  # 2D
+    iqxqy_ws = CreateWorkspace(
+        DataX=qx_bin_edges_grid,  # 2D
+        DataY=i_grid,  # 2D
+        DataE=i_sigma_grid,  # 2D
         NSpec=256,
         UnitX='MomentumTransfer',
         VerticalAxisUnit='MomentumTransfer',
         VerticalAxisValues=qy_bin_centers,  # 1D
+        OutputWorkspace=out_ws_prefix+"_iqxqy"
     )
+
+    dqx_ws = CreateWorkspace(
+        DataX=qx_bin_edges_grid,  # 2D
+        DataY=dqx_bin_centers_grid,  # 2D
+        DataE=None,  # 2D
+        NSpec=256,
+        UnitX='MomentumTransfer',
+        VerticalAxisUnit='MomentumTransfer',
+        VerticalAxisValues=qy_bin_centers,  # 1D
+        OutputWorkspace=out_ws_prefix+"_dqx",
+    )
+
+    dqy_ws = CreateWorkspace(
+        DataX=qx_bin_edges_grid,  # 2D
+        DataY=dqy_bin_centers_grid,  # 2D
+        DataE=None,  # 2D
+        NSpec=256,
+        UnitX='MomentumTransfer',
+        VerticalAxisUnit='MomentumTransfer',
+        VerticalAxisValues=qy_bin_centers,  # 1D
+        OutputWorkspace=out_ws_prefix+"_dqy",
+    )
+
+    qxqy_wss_grouped = GroupWorkspaces(
+        InputWorkspaces=[iqxqy_ws, dqx_ws, dqy_ws],
+        OutputWorkspace=out_ws_prefix+"_qxqy")
+
+    return qxqy_wss_grouped
 
 
 def bin_into_q1d(ws):

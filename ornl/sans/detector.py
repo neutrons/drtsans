2b28cd4e4194ea20e988cfbdd1369c37ec143919
Ricardo <ricleal@gmail.com>
2019-05-09 18:56:11 -0400

    Added component info rather than detector'
    Former-commit-id: 8c0a05aa8e504992f4a61a00f92ed3c8723622c9

--- ornl/sans/detector.py
+++ ornl/sans/detector.py
@@ -1,68 +1,53 @@
+from __future__ import absolute_import, division, print_function
 
 
-class Detector(object):
+class Component:
+    '''
+    Stores information about the component
+    '''
 
-    _n_monitors = None
+    # class variables that will cache the component details
+    dim_x = -1
+    dim_y = -1
+    dims = -1
+    first_index = -1
 
-    _components_dimensions = {}
-
-    def __init__(self, workspace):
+    def __init__(self, workspace, component_name):
         self._workspace = workspace
-        self._instrument = workspace.getInstrument()
+        self._initialization(component_name)
 
-    def _get_detector_dimensions(self, component_name):
+    def _initialization(self, component_name):
+        first_det_id = self._detector_details(component_name)
+        self._detector_first_ws_index(first_det_id)
+
+    def _detector_details(self, component_name):
         """Private function that reads the instrument and get component_name
-        dimensions
+        dimensions and first detector id
         """
-
-        component = self._instrument.getComponentByName(component_name)
-        n_tubes = component.nelements()
+        instrument = self._workspace.getInstrument()
+        component = instrument.getComponentByName(component_name)
+        self.dim_x = component.nelements()
 
         if component[0].nelements() == 1:
             # Handles EQSANS
-            n_pixels_per_tube = component[0][0].nelements()
+            self.dim_y = component[0][0].nelements()
+            first_det_id = component[0][0][0].getID()
         else:
             # Handles BioSANS/GPSANS
-            n_pixels_per_tube = component[0].nelements()
-        return n_tubes, n_pixels_per_tube
+            self.dim_y = component[0].nelements()
+            first_det_id = component[0][0].getID()
+        self.dims = self.dim_x * self.dim_y
+        return first_det_id
 
-    def get_detector_dimensions(self, component_name):
-        """ Reads the cached dimensions
-        Parameters
-        ----------
-        component_name : string
-            Valid component name
-
-        Returns
-        -------
-        tuple
-            tuple of integers x,y
-        """
-
-        if component_name in self._components_dimensions:
-            return self._components_dimensions[component_name]
+    def _detector_first_ws_index(self, first_det_id):
+        ''' sets the first_index of this component
+        '''
+        for ws_index in range(self._workspace.getNumberHistograms()):
+            if self._workspace.getSpectrum(
+                    ws_index).getDetectorIDs()[0] == first_det_id:
+                self.first_index = ws_index
+                break
+                return
         else:
-            x, y = self._get_detector_dimensions(component_name)
-            self._components_dimensions[component_name] = (x, y)
-            return x, y
-
-    def get_number_of_monitors(self):
-        """Get the number of monitors for this workspace.
-        It is assumed that the monitors are in the beggining of the
-        spectra list.
-
-        Returns
-        -------
-        int
-            The number of monitors
-        """
-        if not self._n_monitors:
-            spectrum_info = self._workspace.spectrumInfo()
-            monitor_index_list = []
-            for ws_index in range(self._workspace.getNumberHistograms()):
-                if spectrum_info.isMonitor(ws_index):
-                    monitor_index_list.append(ws_index)
-                else:
-                    break
-            self._n_monitors = len(monitor_index_list)
-        return self._n_monitors
+            raise ValueError('Iterared WS and did not find first det id = '
+                             '{}'.format(first_det_id))
--- tests/unit/new/ornl/sans/test_detector.py
+++ tests/unit/new/ornl/sans/test_detector.py
@@ -1,62 +1,48 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from mantid.simpleapi import LoadHFIRSANS
-from ornl.sans.detector import Detector
+from mantid.simpleapi import LoadEmptyInstrument
+from ornl.sans.detector import Component
 from ornl.settings import unique_workspace_name
 
 
-def test_detector_biosans(biosans_f):
+def test_detector_biosans():
 
-    ws = LoadHFIRSANS(
-        Filename=biosans_f['beamcenter'],
-        OutputWorkspace=unique_workspace_name())
-    det = Detector(ws)
-    assert 2 == det.get_number_of_monitors()
-    assert (192, 256) == det.get_detector_dimensions("detector1")
-    assert (160, 256) == det.get_detector_dimensions("wing_detector")
+    ws = LoadEmptyInstrument(InstrumentName='biosans',
+                             OutputWorkspace=unique_workspace_name())
 
+    d = Component(ws, "detector1")
+    assert 192 == d.dim_x
+    assert 256 == d.dim_y
+    assert 192*256 == d.dims
+    assert 2 == d.first_index
 
-def test_detector_cache_biosans(biosans_f):
-
-    import time
-    import math
-
-    ws = LoadHFIRSANS(
-        Filename=biosans_f['beamcenter'],
-        OutputWorkspace=unique_workspace_name())
-
-    det = Detector(ws)
-
-    t_mons_1_start = time.time()
-    det.get_number_of_monitors()
-    t_mons_1_stop = time.time()
-    t_mons_1_elapsed = t_mons_1_stop - t_mons_1_start
-
-    t_mons_2_start = time.time()
-    det.get_number_of_monitors()
-    t_mons_2_stop = time.time()
-    t_mons_2_elapsed = t_mons_2_stop - t_mons_2_start
-
-    # The 2nd time should be at least 2 orders of magnitude faster
-    assert abs(int(math.log10(t_mons_1_elapsed - t_mons_2_elapsed))) >= 2
-
-    t_det_1_start = time.time()
-    det.get_detector_dimensions("detector1")
-    t_det_1_stop = time.time()
-
-    t_det_2_start = time.time()
-    det.get_detector_dimensions("detector1")
-    t_det_2_stop = time.time()
-    assert abs(int(math.log10(t_det_1_stop - t_det_1_start) -
-                             (t_det_2_stop - t_det_2_start))) >= 4
+    d = Component(ws, "wing_detector")
+    assert 160 == d.dim_x
+    assert 256 == d.dim_y
+    assert 160*256 == d.dims
+    assert 192*256 + 2 == d.first_index
 
 
-def test_detector_gpsans(gpsans_f):
+def test_detector_gpsans():
 
-    ws = LoadHFIRSANS(
-        Filename=gpsans_f['beamcenter'],
-        OutputWorkspace=unique_workspace_name())
-    det = Detector(ws)
-    assert 2 == det.get_number_of_monitors()
-    assert (192, 256) == det.get_detector_dimensions("detector1")
+    ws = LoadEmptyInstrument(InstrumentName='cg2',
+                             OutputWorkspace=unique_workspace_name())
+
+    d = Component(ws, "detector1")
+    assert 192 == d.dim_x
+    assert 256 == d.dim_y
+    assert 192*256 == d.dims
+    assert 2 == d.first_index
+
+
+def test_detector_eqsans():
+
+    ws = LoadEmptyInstrument(InstrumentName='eqsans',
+                             OutputWorkspace=unique_workspace_name())
+
+    d = Component(ws, "detector1")
+    assert 192 == d.dim_x
+    assert 256 == d.dim_y
+    assert 192*256 == d.dims
+    assert 1 == d.first_index

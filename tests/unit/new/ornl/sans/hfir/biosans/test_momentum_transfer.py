884697f21ed0c72319f31308000108183eda50ef
Ricardo <ricleal@gmail.com>
2019-05-09 21:16:50 -0400

    todo: finish parallel
    Former-commit-id: 20c9aee567f9113914b5af056d46a094439bdb32

--- tests/unit/new/ornl/sans/hfir/biosans/test_momentum_transfer.py
+++ tests/unit/new/ornl/sans/hfir/biosans/test_momentum_transfer.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from mantid.simpleapi import LoadHFIRSANS
 from ornl.settings import unique_workspace_name
 from ornl.sans.momentum_transfer import bin_into_q2d, bin_into_q1d
-
+from mantid import mtd
 
 def test_momentum_tranfer(biosans_sensitivity_dataset):
 
@@ -12,7 +12,7 @@ def test_momentum_tranfer(biosans_sensitivity_dataset):
         Filename=biosans_sensitivity_dataset['flood'],
         OutputWorkspace=unique_workspace_name())
 
-    qxqy_wss_grouped = bin_into_q2d(ws, "detector1")
+    qxqy_wss_grouped = bin_into_q2d(ws, "detector1", out_ws_prefix="main")
     assert len(qxqy_wss_grouped) == 3
 
     ws_iqxqy, ws_dqx, ws_dqy = qxqy_wss_grouped
@@ -23,7 +23,7 @@ def test_momentum_tranfer(biosans_sensitivity_dataset):
     assert ws_iq.extractY().shape == (1, 100)
     assert ws_iq.extractX().shape == (1, 101)
 
-    qxqy_wss_grouped = bin_into_q2d(ws, "wing_detector")
+    qxqy_wss_grouped = bin_into_q2d(ws, "wing_detector", out_ws_prefix="wing")
     assert len(qxqy_wss_grouped) == 3
 
     ws_iqxqy, ws_dqx, ws_dqy = qxqy_wss_grouped
@@ -33,3 +33,36 @@ def test_momentum_tranfer(biosans_sensitivity_dataset):
     ws_iq = bin_into_q1d(ws_iqxqy, ws_dqx, ws_dqy)
     assert ws_iq.extractY().shape == (1, 100)
     assert ws_iq.extractX().shape == (1, 101)
+
+
+
+def parallel_f(parameters):
+    ws_name, component_name, out_ws_prefix = parameters
+    ws = mtd[ws_name]
+    qxqy_wss_grouped = bin_into_q2d(ws, component_name, out_ws_prefix)
+    return [(ws.name, ws) for ws in qxqy_wss_grouped]
+
+def test_momentum_tranfer_parallel(biosans_sensitivity_dataset):
+
+    import multiprocessing
+    from mantid.simpleapi import AnalysisDataService
+
+    input_ws_name = unique_workspace_name()
+    LoadHFIRSANS(
+        Filename=biosans_sensitivity_dataset['flood'],
+        OutputWorkspace=input_ws_name)
+    
+    ws_names = [input_ws_name, input_ws_name]
+    components = ['detector1', 'wing_detector']
+    prefixes = ['main', 'wing']
+    parameters = [
+        (ws_name, component, prefix) for ws_name, component, prefix in
+        zip(ws_names, components, prefixes)
+    ]
+    with multiprocessing.Pool(processes=2) as pool:
+        results = pool.map(parallel_f, parameters)
+
+    [AnalysisDataService.add(name, ws) for name, ws in zip([
+        ('main_iqxqy', 'main_dqx', 'main_dqy'),
+        ('wing_iqxqy', 'wing_dqx', 'wing_dqy')], results)]
+

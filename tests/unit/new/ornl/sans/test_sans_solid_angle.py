0390f6a98f9549b21b42c26cfa06e12d390ad0cf
Steven Hahn <hahnse@ornl.gov>
2019-06-11 17:13:07 -0400

    add unit test.
    Former-commit-id: a700fc8547f866cb790060383338e3a0a6d01c7c

--- tests/unit/new/ornl/sans/test_sans_solid_angle.py
+++ tests/unit/new/ornl/sans/test_sans_solid_angle.py
@@ -1,29 +1,25 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from os.path import join
 import pytest
-from mantid.simpleapi import LoadEmptyInstrument, MoveInstrumentComponent
+from mantid.simpleapi import CompareWorkspaces, Load, LoadEmptyInstrument, MoveInstrumentComponent
+from ornl.sans import solid_angle_correction as sac
 
-from ornl.sans import solid_angle_correction
-
-
-@pytest.fixture(scope='module')
-def test_sans_solid_angle():
-    instrument = 'eqsans'
-
+def test_sans_solid_angle(refd):
     #Load empty instrument
-    wsInput = LoadEmptyInstrument(InstrumentName=instrument)
+    wsInput = LoadEmptyInstrument(InstrumentName='eqsans')
 
     #Move the detector 5m away from the sample
     MoveInstrumentComponent(Workspace=wsInput, ComponentName='detector1', RelativePosition='0', Z='5')
 
-    #Solid Angle correction
-    wsOutput = solid_angle_correction(wsInput)
+    #Apply solid angle correction
+    wsOutput = sac.solid_angle_correction(wsInput, detector_type='Normal')
 
     #Let's do some validation
     assert wsOutput.getNumberHistograms(), 49153
-    #SA is bigger in the center: 8.99095e-07 vs 9.4172e-07
-    correctionEdge = wsOutput.y(48896)[0];
-    correctionCenter = wsOutput.y(25984)[0];
-    assert correctionCenter > correctionEdge;
+    reference_workspace = Load(Filename=join(refd.new.eqsans, 'test_sans_solid_angle.nxs'))
+    assert CompareWorkspaces(wsOutput, reference_workspace)
 
+if __name__ == '__main__':
+    pytest.main()

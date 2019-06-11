9dcf7150ac6a545750c8646f4e5dbe11dd7607a9
Steven Hahn <hahnse@ornl.gov>
2019-06-11 17:19:45 -0400

    pylint
    Former-commit-id: bbe3eabf510ccf5d79a759ab58c0245f27325d12

--- tests/unit/new/ornl/sans/test_sans_solid_angle.py
+++ tests/unit/new/ornl/sans/test_sans_solid_angle.py
@@ -10,16 +10,17 @@ from ornl.sans import solid_angle_correction as sac
 
 
 def test_sans_solid_angle(refd):
-    #Load empty instrument
+    # Load empty instrument
     wsInput = LoadEmptyInstrument(InstrumentName='eqsans')
 
-    #Move the detector 5m away from the sample
-    MoveInstrumentComponent(Workspace=wsInput, ComponentName='detector1', RelativePosition='0', Z='5')
+    # Move the detector 5m away from the sample
+    MoveInstrumentComponent(Workspace=wsInput, ComponentName='detector1',
+                            RelativePosition='0', Z='5')
 
-    #Apply solid angle correction
+    # Apply solid angle correction
     wsOutput = sac.solid_angle_correction(wsInput, detector_type='Normal')
 
-    #Let's do some validation
+    # Let's do some validation
     assert wsOutput.getNumberHistograms(), 49153
     reference_workspace = Load(Filename=join(refd.new.eqsans, 'test_sans_solid_angle.nxs'))
     assert CompareWorkspaces(wsOutput, reference_workspace)

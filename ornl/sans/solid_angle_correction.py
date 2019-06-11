f1762c853e3a903064c77bc28511b355af14c475
Steven Hahn <hahnse@ornl.gov>
2019-06-11 14:00:30 -0400

    pylint
    Former-commit-id: 2567b1d915c3a764363bd8b8cab1af47ceb5c148

--- ornl/sans/solid_angle_correction.py
+++ ornl/sans/solid_angle_correction.py
@@ -1,9 +1,14 @@
-from __future__ import (absolute_import, division, print_function, unicode_literals)
+from __future__ import (absolute_import, division, print_function, 
+                                                 unicode_literals)
 
-from mantid.simpleapi import SANSSolidAngle, ReplaceSpecialValues 
+from mantid.simpleapi import SANSSolidAngle, ReplaceSpecialValues
+
 
 def solid_angle_correction(input_workspace, detector_type)
-    solid_angle_ws = SANSSolidAngle(InputWorkspace = input_workspace, Type = detector_type)
+    solid_angle_ws = SANSSolidAngle(InputWorkspace = input_workspace,
+                                    Type = detector_type)
     output_workpace = input_workspace / solid_angle_ws
-    ReplaceSpecialValues(InputWorkspace = output_workspace, OutputWorkspace=output_workspace NaNValue=0., InfinityValue=0.)
+    ReplaceSpecialValues(InputWorkspace = output_workspace, \
+                         OutputWorkspace=output_workspace NaNValue=0.,
+                         InfinityValue=0.)
     return output_workspace

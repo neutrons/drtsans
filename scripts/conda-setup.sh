3d86ebd2926f7cb38a5993ac7beb0f248ce768d8
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-13 14:40:44 -0400

    Update scripts/conda-setup.sh, scripts/sans-backend-run.sh files
    Former-commit-id: 25d2ca5d47b4be348fbc6a65c45bd87e2dbcd106

--- scripts/conda-setup.sh
+++ scripts/conda-setup.sh
@@ -2,11 +2,9 @@
 
 set -x
 
-#/opt/miniconda/bin/conda init bash
-#/opt/miniconda/bin/conda create --name mantid python=3 anaconda
-#conda activate mantid
 conda config --add channels conda-forge
 conda config --add channels mantid
 conda install --file requirements.txt
+conda install ipython pytest
 conda install -c mantid/label/nightly mantid-framework python=3
 

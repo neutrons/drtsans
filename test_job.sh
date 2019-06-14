5ab97a130c45c7cd5d45973cd8cd121248b7261f
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-14 14:28:03 -0400

    Update test_job.sh, .gitlab-ci.yml files
    Former-commit-id: a01a0524a591bc81a1010801a041296827143102

--- test_job.sh
+++ test_job.sh
@@ -5,14 +5,6 @@ TEST_SCOPE=$2  # 'unit' or 'integration'
 
 N_SUB=8  # number of python subprocesses
 
-module load mantid/ORNL_SANS_py${PYTHON_VERSION}
+#module load mantid/ORNL_SANS_py${PYTHON_VERSION}
 
-virtualenv -p python${PYTHON_VERSION} testenv${PYTHON_VERSION}
-source testenv${PYTHON_VERSION}/bin/activate
-
-pip install -r requirements_test.txt
-if [ ${PYTHON_VERSION} = "2.7" ]; then
-  pip install -r requirements_2.x.txt
-fi
-
-pytest -v -d --tx ${N_SUB}*popen//python=python${PYTHON_VERSION} tests/${TEST_SCOPE}
+pytest -v -d --tx ${N_SUB} tests/${TEST_SCOPE}

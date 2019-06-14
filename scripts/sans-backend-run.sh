04a2e0a6603089219a22f20e74fb53db0729d7c3
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-14 12:59:00 -0400

    Update scripts/sans-backend-run.sh
    Former-commit-id: affde57efd9b777747342e1d80881bffa58e7e20

--- scripts/sans-backend-run.sh
+++ scripts/sans-backend-run.sh
@@ -58,7 +58,7 @@ func_main() {
       else
         docker run -v "${VAR_TMP_DIR}":/tmp/input -t CONTAINER_URL bash -c 'find /tmp/input -iname "*.py" -execdir python {} +'
       fi
-      if "${VAR_TMP_DIR}"/*; do
+      if "${VAR_TMP_DIR}"/*; then
         cp -r "${VAR_TMP_DIR}"/* . 2>/dev/null
       fi
     else

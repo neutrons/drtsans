96ab1db5b325bddf5e207fd2475f6a07a468dd13
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-14 19:40:09 -0400

    Update scripts/conda-setup.sh
    Former-commit-id: 66d53294a7ea492f1127dc5d1126f70af73539ea

--- scripts/conda-setup.sh
+++ scripts/conda-setup.sh
@@ -3,12 +3,12 @@
 set -x
 
 conda install -q six==1.11.0 \
-                 configparser==3.5.0 \
-                 numpy==1.15.0 \
-                 matplotlib==2.2.2 \
-                 sortedcontainers==1.5.7 \
-                 scikit-image==0.14.0 \
-                 h5py==2.8.0 \
-                 pyyaml==3.13 \
+#                 configparser==3.5.0 \
+#                 numpy==1.15.0 \
+#                 matplotlib==2.2.2 \
+#                 sortedcontainers==1.5.7 \
+#                 scikit-image==0.14.0 \
+#                 h5py==2.8.0 \
+#                 pyyaml==3.13 \
                  pytest
 

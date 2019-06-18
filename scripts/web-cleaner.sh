d04bdb4870fa5bd35b0af4b52bc58bb1b5fc9667
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-18 09:56:25 -0400

    Update scripts/web-cleaner.sh
    Former-commit-id: c60cc0f631472a277bfcbde7cc3e80ce71c5be62

--- scripts/web-cleaner.sh
+++ scripts/web-cleaner.sh
@@ -4,13 +4,17 @@ declare CI_USER="${1}"
 declare CI_PASS="${2}"
 declare -a BRANCHES=( $(git ls-remote --heads https://${CI_USER}:${CI_PASS}@code.ornl.gov/sns-hfir-scse/sans/sans-backend.git | cut -f 3 -d '/') )
 declare HTTPD_PATH='/var/www/html/sans-backend'
+rm -f /tmp/web-cleaner.txt
+touch /tmp/web-cleaner.txt
+
+for BRANCH in "${BRANCHES[@]}"; do
+  printf "%s/%s\n" "${HTTPD_PATH}" "${BRANCH}" >> /tmp/web-cleaner.txt
+done
 
 for DIR in ${HTTPD_PATH}/*; do
-  for BRANCH in "${BRANCHES[@]}"; do
-    if [[ "${DIR}" != "${HTTPD_PATH}/${BRANCH}" ]]; then
-      printf "%s is OK!\n" "${DIR}"
-    else
-      printf "%s is NOT OK!\n" "${DIR}"
-    fi
-  done
+  if ! $(grep "${HTTPD_PATH}/${BRANCH}" /tmp/web-cleaner.txt); then
+    rm -rf "${HTTPD_PATH}/${BRANCH}"
+  fi
 done
+
+rm -f /tmp/web-cleaner.txt

9ab8be2d21c13e365edd498c407e3a13edb6c420
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-18 07:22:05 -0400

    Update scripts/web-cleaner.sh
    Former-commit-id: 8856f01c1281a1f5fbffdbc5e7b63b9ce7c74f53

--- scripts/web-cleaner.sh
+++ scripts/web-cleaner.sh
@@ -5,8 +5,8 @@ declare CI_PASS="${2}"
 declare -a BRANCHES=( $(git ls-remote --heads https://${CI_USER}:${CI_PASS}@code.ornl.gov/sns-hfir-scse/sans/sans-backend.git | cut -f 3 -d '/') )
 declare HTTPD_PATH='/var/www/html/sans-backend'
 
-for DIR in ${HTTP_PATH}/*; do
-  if [[ ! "${DIR}" =~ ${HTTPD_PATH}/${DIR} ]]; then
+for DIR in ${HTTPD_PATH}/*; do
+  if [[ ! "${DIR}" =~ ${HTTPD_PATH}/* ]]; then
     printf "%s is OK!\n" "${DIR}"
   else
     printf "%s is NOT OK!\n" "${DIR}"

5e636b3385f149df0ca5ac98b1373a65a7c81520
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-20 15:33:32 -0400

    Update test_job.sh
    Former-commit-id: 3f9b0119fb53efce4055518be712efba8f661d82

--- test_job.sh
+++ test_job.sh
@@ -5,5 +5,5 @@ TEST_SCOPE=$1  # 'unit' or 'integration'
 N_SUB=8  # number of python subprocesses
 #source /etc/profiles.d/modules.sh
 #module load mantid/ORNL_SANS_py3.6
-conda activate mantid
+source activate mantid
 pytest -v ${N_SUB} tests/${TEST_SCOPE}

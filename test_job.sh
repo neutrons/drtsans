def500a2f2765edf7dc41cfe8b23a0d846cfc784
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-20 16:27:38 -0400

    Update test_job.sh, .gitlab-ci.yml files
    Former-commit-id: eeee162ebd5ce8c174736d0056a5afe26c1d8001

--- test_job.sh
+++ test_job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -x
+
 TEST_SCOPE=$1  # 'unit' or 'integration'
 
 N_SUB=8  # number of python subprocesses
 #source /etc/profiles.d/modules.sh
 #module load mantid/ORNL_SANS_py3.6
 source activate mantid
-pytest -v ${N_SUB} /opt/sans-backend/tests/${TEST_SCOPE}
+pytest -v /opt/sans-backend/tests/${TEST_SCOPE}

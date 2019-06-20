c3c4be5a8fb37097324e374b144e9a8cc4eede39
Borreguero Calvo, Jose M <borreguerojm@ornl.gov>
2019-06-20 15:54:28 -0400

    Update test_job.sh
    Former-commit-id: bf36771596382f8036929ff5998af15d764eabbc

--- test_job.sh
+++ test_job.sh
@@ -6,4 +6,4 @@ N_SUB=8  # number of python subprocesses
 #source /etc/profiles.d/modules.sh
 #module load mantid/ORNL_SANS_py3.6
 source activate mantid
-pytest -v ${N_SUB} tests/${TEST_SCOPE}
+pytest -v ${N_SUB} /opt/sans-backend/tests/${TEST_SCOPE}

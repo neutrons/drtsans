9495646adc780f93c7802466f47b4a9d4e304faa
Steven Hahn <hahnse@ornl.gov>
2019-06-21 18:27:02 -0400

    add output_workspace decorator
    Former-commit-id: 2f84b23ae1bb8e76003adced0b6a6e3c4dee58fc

--- ornl/sans/solid_angle_correction.py
+++ ornl/sans/solid_angle_correction.py
@@ -2,9 +2,10 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from mantid.simpleapi import SolidAngle, ReplaceSpecialValues
+from ornl.settings import optional_output_workspace
 
-
-def solid_angle_correction(input_workspace, detector_type):
+@optional_output_workspace
+def solid_angle_correction(input_workspace, detector_type = 'Rectangle'):
     solid_angle_ws = SolidAngle(InputWorkspace=input_workspace,
                                 Method=detector_type)
     output_workspace = input_workspace / solid_angle_ws

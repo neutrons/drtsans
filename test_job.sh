9c91e28f6af10ecdfeebd80949196f95d945d6a7
Cooper, Caleb D <coopercd@ornl.gov>
2019-06-21 08:06:30 -0400

    Update test_job.sh
    Former-commit-id: 0548c18b9fd162b8fe1956f2f0716888f165d91e

--- test_job.sh
+++ test_job.sh
@@ -8,4 +8,5 @@ N_SUB=8  # number of python subprocesses
 #source /etc/profiles.d/modules.sh
 #module load mantid/ORNL_SANS_py3.6
 source activate mantid
+cd /opt/sans-backend
 pytest -v /opt/sans-backend/tests/${TEST_SCOPE}

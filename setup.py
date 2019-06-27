be259d22c8c7daabc93cd7e9a4b1e2a2aa973c34
Pete Peterson <petersonpf@ornl.gov>
2019-06-27 11:47:32 -0400

    Add options to run pytest from setup.py
    This includes an alias to run it as `python setup.py test`. This was
    happily taken from [pytest's good practices](https://github.com/pytest-dev/pytest/blob/master/doc/en/goodpractices.rst).
    
    
    Former-commit-id: 4e0a2de3d74eecf622d341aadf5e8413a64687b8

--- setup.py
+++ setup.py
@@ -3,11 +3,33 @@
 """
 from __future__ import absolute_import, division, print_function
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 import versioneer
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        import shlex
+        import sys
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+commands = versioneer.get_cmdclass()
+commands['pytest'] = PyTest  # add the custom pytest command
 
 setup(name="ornl",
       version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
+      cmdclass=commands,
       description="ORNL SANS reduction",
       url="https://http://www.mantidproject.org",
       long_description="""ORNL SANS reduction""",

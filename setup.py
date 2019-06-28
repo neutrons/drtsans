b84ad5bd3760ed0a840189e9c658742fc0d24fc1
Pete Peterson <petersonpf@ornl.gov>
2019-06-28 12:18:07 -0400

    Use the Command provided by pytest
    Former-commit-id: 66aa117b1e3ffa4b587039373515b7d28cc62afc

--- setup.py
+++ setup.py
@@ -1,35 +1,13 @@
 """
-    Setup script for ORNL SANS reduction
+Setup script for ORNL SANS reduction
 """
 from __future__ import absolute_import, division, print_function
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 import versioneer
-
-
-class PyTest(TestCommand):
-    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ""
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        import shlex
-        import sys
-
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
-
-commands = versioneer.get_cmdclass()
-commands['pytest'] = PyTest  # add the custom pytest command
 
 setup(name="ornl",
       version=versioneer.get_version(),
-      cmdclass=commands,
+      cmdclass=versioneer.get_cmdclass(),
       description="ORNL SANS reduction",
       url="https://http://www.mantidproject.org",
       long_description="""ORNL SANS reduction""",
@@ -39,5 +17,6 @@ setup(name="ornl",
       package_dir={},
       package_data={},
       install_requires=[],
-      setup_requires=[],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-xdist'],
       )

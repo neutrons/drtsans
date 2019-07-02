ad744080ac27d74b578c42500729658594673366
Jose Borreguero <borreguero@gmail.com>
2019-07-02 19:50:07 -0400

    Refs #100 expose dark current to public api
    Former-commit-id: 8d13f9d85c75c03b2d19ecf8540c0bc6a1b475fd

--- ornl/sans/sns/eqsans/__init__.py
+++ ornl/sans/sns/eqsans/__init__.py
@@ -1,7 +1,8 @@
 from .load import *   # noqa: F403
 from .geometry import *  # noqa: F403
 from .correct_frame import *  # noqa: F403
-
+from .dark_current import  *  # noqa: F403
 from .api import *  # noqa: F403, F401
 
-__all__ = load.__all__ + geometry.__all__ + correct_frame.__all__  # noqa: F405
+__all__ = load.__all__ + geometry.__all__ + correct_frame.__all__ +\
+          dark_current.__all__# noqa: F405

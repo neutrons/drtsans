d4f66bc9f51eb1266f5e00d04f9789b495dba276
Jose Borreguero <borreguero@gmail.com>
2019-07-02 19:51:15 -0400

    Refs #100 add api subtract_dark_current function
    Former-commit-id: 1378dfcca3665b7419857677003b3d6afd8e06e7

--- tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
@@ -47,8 +47,8 @@ def test_normalise_to_workspace(wss, refd):
 def test_subtract_normalised_dark(wss, refd):
     name = pjn(refd.new.eqsans, 'test_dark_current', 'dark_norm_sum.nxs')
     _dark_normalised = LoadNexus(name, OutputWorkspace=unique_workspace_name())
-    _w0 = dkc.subtract_normalised_dark(wss['data'], _dark_normalised,
-                                       unique_workspace_name())
+    _w0 = dkc.subtract_normalized_dark_current(wss['data'], _dark_normalised,
+                                               unique_workspace_name())
     assert SampleLogs(_w0).normalizing_duration.value == 'duration'
     _w1 = SumSpectra(_w0, OutputWorkspace=unique_workspace_name())
     name = pjn(refd.new.eqsans, 'test_dark_current', 'data_minus_dark.nxs')

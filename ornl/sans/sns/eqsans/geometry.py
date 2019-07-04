08ac8950ae63c2d2458a62dc193a1b0baa7571e7
Jose Borreguero <borreguero@gmail.com>
2019-07-03 20:00:45 -0400

    Refs #103 amend aperture functions and add unit tests
    Former-commit-id: 0faa25709aa762919d6e195fecf02ed8562e2236

--- ornl/sans/sns/eqsans/geometry.py
+++ ornl/sans/sns/eqsans/geometry.py
@@ -11,7 +11,8 @@ from ornl.sans.geometry import (sample_source_distance,
 
 __all__ = ['detector_z_log',
            'translate_sample_by_z', 'translate_detector_by_z',
-           'center_detector']
+           'center_detector', 'sample_aperture_diameter',
+           'source_aperture_diameter']
 
 detector_z_log = 'detectorZ'
 
@@ -108,29 +109,38 @@ def center_detector(ws, x, y, units='m', relative=False):
     return np.array(det.getPos())
 
 
-def sample_aperture_diameter(other, unit='mm'):
+def sample_aperture_diameter(run, unit='mm'):
     r"""
-    Find the sample aperture diameter
+    Find the sample aperture diameter from the logs.
+
+    Log keys searched are 'sample-aperture-diameter' and 'beamslit4'.
 
     Parameters
     ----------
-    other: Run, MatrixWorkspace, file name, run number
+    run: Mantid Run instance, MatrixWorkspace, file name, run number
+        Input from which to find the aperture
     unit: str
-        Length unit, either 'm' or 'mm'
+        return aperture in requested length unit, either 'm' or 'mm'
+
     Returns
     -------
     float
         Sample aperture diameter, in requested units
     """
-    sl = SampleLogs(other)
-    sad = sl.single_value('beamslit4')
+    sl = SampleLogs(run)
+    for log_key in ('sample-aperture-diameter', 'beamslit4'):
+        if log_key in sl.keys():
+            sad = sl.single_value(log_key)
+            break
+    if 'sample-aperture-diameter' not in sl.keys():
+        sl.insert('sample-aperture-diameter', sad, unit='mm')
     if unit == 'm':
         sad /= 1000.0
     return sad
 
 
 @namedtuplefy
-def source_aperture(other, unit='mm'):
+def source_aperture(other, unit='mm', search_logs=True):
     r"""
     Find the source aperture diameter and position
 
@@ -150,6 +160,7 @@ def source_aperture(other, unit='mm'):
     other: Run, MatrixWorkspace, file name, run number
     unit: str
         Length unit, either 'm' or 'mm'
+
     Returns
     -------
     namedtuple
@@ -182,9 +193,11 @@ def source_aperture(other, unit='mm'):
     return dict(diameter=diameter, distance_to_sample=asd, unit=unit)
 
 
-def source_aperture_diameter(other, unit='mm'):
+def source_aperture_diameter(run, unit='mm', search_logs=True):
     r"""
     Find the source aperture diameter
+
+    Either report log vale or compute this quantity.
 
     After the moderator (source) there are three consecutive discs
     (termed wheels), each with eight holes in them (eight slits).
@@ -197,17 +210,32 @@ def source_aperture_diameter(other, unit='mm'):
     rotation angle for each wheel in order to align the appropriate slit
     with the neutron beam. These angles are not used in reduction.
 
+    If the aperture is computed, then the value is stored
+    in log key "source-aperture-diameter", with mili meter units
+
     Parameters
     ----------
-    other: Run, MatrixWorkspace, file name, run number
+    run: Mantid Run instance, MatrixWorkspace, file name, run number
     unit: str
         Length unit, either 'm' or 'mm'
+    search_logs: Bool
+        Report log value in "source-aperture-diameter" key
+
     Returns
     -------
     float
         Source aperture diameter, in requested units
     """
-    return source_aperture(other, unit=unit).diameter
+    log_key = 'source-aperture-diameter'
+    sl = SampleLogs(run)
+    if search_logs is True:
+        sad = sl.single_value(log_key)  # units are 'mm'
+    else:
+        sad = source_aperture(run, unit='mm').diameter
+        sl.insert(log_key, sad, unit='mm')
+    if unit == 'm':
+        sad /= 1000.0
+    return sad
 
 
 def insert_aperture_logs(ws):
--- tests/unit/new/ornl/sans/sns/eqsans/test_geometry.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_geometry.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pytest
 from pytest import approx
-from mantid.simpleapi import (Load, MoveInstrumentComponent)
+from mantid.simpleapi import (LoadEventNexus, MoveInstrumentComponent)
 from ornl.sans.geometry import detector_name
-from ornl.sans.sns.eqsans.geometry import center_detector
+from ornl.sans.sns.eqsans.geometry import (center_detector,
+                                           sample_aperture_diameter,
+                                           source_aperture_diameter)
 from ornl.sans.samplelogs import SampleLogs
 
 
 def test_center_detector(eqsans_f):
-    w = Load(eqsans_f['data'])
+    w = LoadEventNexus(eqsans_f['data'])
     r_0 = [0.1, 0.2, 0.3]
     MoveInstrumentComponent(w, X=r_0[0], Y=r_0[1], Z=r_0[2],
                             ComponentName=detector_name(w),
@@ -22,6 +24,24 @@ def test_center_detector(eqsans_f):
     assert r == approx(np.array([.016, 0.021, r_0[2]]))
     sdd = SampleLogs(w)['sample-detector-distance'].value
     assert sdd == approx(1e3 * np.linalg.norm(r))
+
+
+def test_sample_aperture_diameter():
+    ws = LoadEventNexus('EQSANS_92353')
+    sad = sample_aperture_diameter(ws)
+    assert sad == approx(10)
+    sad = SampleLogs(ws).single_value('sample-aperture-diameter')
+    assert sad == approx(10)
+
+
+def test_source_aperture_diameter():
+    ws = LoadEventNexus('EQSANS_92353')
+    sad = source_aperture_diameter(ws, search_logs=False)
+    assert sad == approx(20)
+    sad = SampleLogs(ws).single_value('source-aperture-diameter')
+    assert sad == approx(20)
+    sad = source_aperture_diameter(ws, search_logs=True)
+    assert sad == approx(20)
 
 
 if __name__ == '__main__':

a12d84e04a3e7041e15f6647c3aec951b60e16ad
Jose Borreguero <borreguero@gmail.com>
2019-07-07 15:07:09 -0400

    Refs #111 function to apply mask
    Former-commit-id: 7be15a71d07e082434d9efc4fbe5e87152431088

--- ornl/sans/mask_utils.py
+++ ornl/sans/mask_utils.py
@@ -1,6 +1,9 @@
 from __future__ import (absolute_import, division, print_function)
 
 import numpy as np
+from mantid.dataobjects import MaskWorkspace
+from mantid.simpleapi import (LoadMask, MaskDetectors, MaskBTP)
+from ornl.settings import unique_workspace_dundername as uwd
 
 
 def mask_as_numpy_array(w, invert=False):
@@ -23,3 +26,37 @@ def masked_indexes(w, invert=False):
     """
     mask = mask_as_numpy_array(w, invert=invert)
     return np.where(mask)[0]
+
+
+def apply_mask(w, mask=None, **btp):
+    r"""
+    Apply a mask or region-of-interest to a workspace.
+
+    The function accepts a path to a mask file, a MaskWorkspace, or options
+    to algorithm MaskBTP.
+
+    Parameters
+    ----------
+    w: Workspace
+        Workspace to be masked
+    mask: mask file path, MaskWorkspace
+        Mask to be applied. If `None`, it is expected that `maskbtp`
+        is not empty
+    btp: dict
+        Options to Mantid algorithm MaskBTP. Will be used if `mask=None`
+    """
+    instrument = w.getInstrument().name()
+    if mask is None:
+        if bool(btp):
+            MaskBTP(w, instrument=instrument, **btp)
+            return
+        else:
+            raise RuntimeError('maskbtp expected if mask is None')
+    elif isinstance(mask, str):
+        wm = LoadMask(Instrument=instrument, InputFile=mask, RefWorkspace=w,
+                      OutputWorkspace=uwd())
+    elif isinstance(mask, MaskWorkspace):
+        wm = mask
+    else:
+        raise RuntimeError('mask not understood')
+    MaskDetectors(Workspace=w, MaskedWorkspace=wm)

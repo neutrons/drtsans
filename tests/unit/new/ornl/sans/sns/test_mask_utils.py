548bd2309ed952aaa375b59c538a10a55e3bf527
Jose Borreguero <borreguero@gmail.com>
2019-07-07 16:53:56 -0400

    Refs #111 test stub
    Former-commit-id: 5d284aee27ac046f9dd18e0e71275a51a90422ef

---
+++ tests/unit/new/ornl/sans/sns/test_mask_utils.py
@@ -0,0 +1,13 @@
+import pytest
+from pytest import approx
+
+
+def test_apply_mask():
+    # test mask file
+    # test MaskWorkspace
+    # test combination
+    assert 42 == approx(42)
+
+
+if __name__ == '__main__':
+    pytest.main()

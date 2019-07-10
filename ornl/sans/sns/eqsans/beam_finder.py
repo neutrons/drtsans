98085dbbc958b6c14b019f0da88619a8c0d66ef6
Jose Borreguero <borreguero@gmail.com>
2019-07-10 15:22:07 -0400

    Refs #105 includind direct beam center
    Former-commit-id: 0ef073351441ad7478b184af195f1b467c50948b

--- ornl/sans/sns/eqsans/beam_finder.py
+++ ornl/sans/sns/eqsans/beam_finder.py
@@ -1,15 +1,14 @@
-from __future__ import (absolute_import, division, print_function)
-
 from mantid.simpleapi import (Integration, FindCenterOfMassPosition)
+from ornl.settings import unique_workspace_dundername as uwd
 
 
-def direct_beam_center(input_ws, method='center_of_mass'):
+def direct_beam_center(ws, method='center_of_mass'):
     r"""
     Calculate absolute coordinates of beam impinging on the detector
 
     Parameters
     ----------
-    input_ws: EventsWorkspace
+    ws: EventsWorkspace
         Workspace for the direct beam run (no sample and no sample holder)
     method: str
         Method to calculate the beam center( only 'center_of_mass' is
@@ -21,5 +20,5 @@ def direct_beam_center(input_ws, method='center_of_mass'):
         (X, Y) coordinates of the beam center (units in meters)
     """
     method_to_alg = dict(center_of_mass=FindCenterOfMassPosition)
-    ws_flattened = Integration(InputWorkspace=input_ws)
+    ws_flattened = Integration(InputWorkspace=ws, OutputWorkspace=uwd())
     return method_to_alg[method](InputWorkspace=ws_flattened)
--- ornl/sans/sns/eqsans/geometry.py
+++ ornl/sans/sns/eqsans/geometry.py
@@ -1,9 +1,12 @@
 from __future__ import (absolute_import, division, print_function)
 
 import numpy as np
-from mantid.simpleapi import mtd, MoveInstrumentComponent
+from mantid.simpleapi import (MoveInstrumentComponent, Integration,
+                              FindCenterOfMassPosition, LoadMask,
+                              MaskDetectors)
 
-from ornl.settings import namedtuplefy
+from ornl.settings import (namedtuplefy,
+                           unique_workspace_dundername as uwd)
 from ornl.sans.samplelogs import SampleLogs
 from ornl.sans.geometry import (sample_source_distance,
                                 sample_detector_distance,
@@ -11,7 +14,7 @@ from ornl.sans.geometry import (sample_source_distance,
 
 __all__ = ['detector_z_log',
            'translate_sample_by_z', 'translate_detector_by_z',
-           'center_detector', 'sample_aperture_diameter',
+           'center_detector', 'direct_beam_center', 'sample_aperture_diameter',
            'source_aperture_diameter']
 
 detector_z_log = 'detectorZ'
@@ -64,48 +67,108 @@ def translate_detector_by_z(ws, z, **kwargs):
     return translate_detector_z(ws, z=z, relative=True, **kwargs)
 
 
-def center_detector(ws, x, y, units='m', relative=False):
+def center_detector(ws, method='center_of_mass',
+                    x=None, y=None, units='m', relative=False,
+                    move_detector=True):
     r"""
-    Move the detector on the XY plane.
+    Move the detector on the XY plane to center the beam location
 
-    Usually `x` and `y` will be the absolute coordinates of the beam
-    impinging on the detector.
+    An estimation of the center of the detector is carried out
+    according to `method`, unless `x` and `y` absolute coordinates
+    are provided (`relative=False`) in which case `x` and `y` are used.
+    If `x` and `y` are a translation (`relative=False`) then both
+    `method` and the translation will be applied.
+
+    case 1: position detector after a center of mass estimation
+        center_detector(ws)
+    case 2: position detector at absolute coordinates (x0,y0)
+        center_detector(ws, x=x0, y=y0)
+    case 3: translate the detector by (x0, y0)
+        enter_detector(ws, method=None, x=x0, y=y0, relative=True)
+    case 4: position detector after a center of mass estimation followed
+            by a translation (x0, y0)
+         center_detector(ws, x=x0, y=y0, relative=True)
 
     Parameters
     ----------
     ws: Workspace
         Input workspace containing the instrument
+    method: str
+        Method to estimate the center of the beam. `None` for no method
     x: float
         Final position or translation along the X-axis
     y: float
         Final position or translation along the Y-axis
     units: str
-        Either meters 'm' or mili-meters 'mm'
+        units of `x` and `y`. Either meters 'm' or mili-meters 'mm'
     relative: Bool
-        Apply translation if True, otherwise `x` and `y` are final coordinates
+        Values of `x` and `y` are either absolute coordinates or a
+        translation.
+    move_detector: bool
+        Only calculate the final position if this is False
 
     Returns
     =======
     numpy.ndarray
         Detector vector position
     """
-    t_x = x if units == 'm' else x / 1.e3
-    t_y = y if units == 'm' else y / 1.e3
-    t_z = 0.0
-    if relative is False:
-        i = mtd[str(ws)].getInstrument()
-        # preserve the Z coordinate value
-        t_z = i.getComponentByName(detector_name(i)).getPos()[-1]
-    MoveInstrumentComponent(ws, X=t_x, Y=t_y, Z=t_z,
-                            ComponentName=detector_name(ws),
-                            RelativePosition=relative)
+    i = ws.getInstrument()
+    rs = i.getComponentByName(detector_name(i)).getPos()
+    rf = np.zeros(3)
+
+    # Use `method` when we are not passing absolute coordinates
+    abs_xy = x is not None and y is not None and relative is False
+    if method is not None and abs_xy is False:
+        method_to_alg = dict(center_of_mass=FindCenterOfMassPosition)
+        ws_flattened = Integration(InputWorkspace=ws, OutputWorkspace=uwd())
+        t_x, t_y = list(method_to_alg[method](InputWorkspace=ws_flattened))
+        rs = np.array([t_x, t_y, rs[-1]])
+        rf = rs
+
+    if x is not None and y is not None:
+        t_x = x if units == 'm' else x / 1.e3
+        t_y = y if units == 'm' else y / 1.e3
+        if relative is True:
+            rf = rs + np.array([t_x, t_y, 0.0])
+        else:
+            rf = np.array([t_x, t_y, rs[-1]])
 
     # Recalculate distance from sample to detector
-    sdd = sample_detector_distance(ws, units='mm', search_logs=False)
-    SampleLogs(ws).insert('sample-detector-distance', sdd, unit='mm')
-    instrument = mtd[str(ws)].getInstrument()
-    det = instrument.getComponentByName(detector_name(instrument))
-    return np.array(det.getPos())
+    if move_detector is True:
+        MoveInstrumentComponent(ws, X=rf[0], Y=rf[1], Z=rf[2],
+                                ComponentName=detector_name(ws),
+                                RelativePosition=False)
+        sdd = sample_detector_distance(ws, units='mm', search_logs=False)
+        SampleLogs(ws).insert('sample-detector-distance', sdd, unit='mm')
+
+    return rf
+
+
+def find_beam_center(input_workspace, method='center_of_mass', mask=None):
+    r"""
+    Calculate absolute coordinates of beam impinging on the detector.
+    Usually employed for a direct beam run (no sample and not sample holder).
+
+    Parameters
+    ----------
+    input_workspace: Workspace
+    method: str
+        Method to calculate the beam center( only 'center_of_mass' is
+        implemented)
+    mask: str, MaskWorkspace
+        Path to mask file, or MaskWorkspace object
+
+    Returns
+    -------
+    tuple
+        (X, Y) coordinates of the beam center (units in meters)
+    """
+    if isinstance(mask, str):
+        mask = LoadMask(mask, OutputWorkspace=uwd())
+    if mask is not None:
+        w = MaskDetectors(input_workspace, OutputWorkspace=uwd())
+    r = center_detector(input_workspace, method=method, move_detector=False)
+    return (r[0], r[1])
 
 
 def sample_aperture_diameter(run, unit='mm'):

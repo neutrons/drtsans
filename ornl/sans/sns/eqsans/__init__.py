93cf4f2aef5878fce6d2bfab14ee9961b1b4f611
Jose Borreguero <borreguero@gmail.com>
2019-07-12 16:39:11 -0400

    Refs #121 bit of cleanin of API
    Former-commit-id: 2df4bc92b4ee98e91971291c0185cb6f0c300f16

--- ornl/sans/sns/eqsans/__init__.py
+++ ornl/sans/sns/eqsans/__init__.py
@@ -8,5 +8,5 @@ from .beam_finder import *  # noqa: F403
 from .api import *  # noqa: F403, F401
 
 mods = (load, geometry, correct_frame, dark_current,  # noqa: F405
-        mask, normalisation, beam_finder)  # noqa: F405
+        mask, normalisation, beam_finder, api)  # noqa: F405
 __all__ = [s for m in mods for s in m.__all__]

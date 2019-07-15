c29d564d283de0f2d77a08ee7d32edf95d045fa9
Steven Hahn <hahnse@ornl.gov>
2019-07-15 13:17:24 -0400

    fix flake8 warnings.
    Former-commit-id: c1dcaa12bbc3eb44e47ca600bde65cd71536468b

--- tests/integration/new/ornl/sans/sns/eqsans/test_save_ascii.py
+++ tests/integration/new/ornl/sans/sns/eqsans/test_save_ascii.py
@@ -3,12 +3,12 @@ from __future__ import print_function
 
 import tempfile
 from os.path import join
-from ornl.sans.momentum_transfer import bin_into_q1d, bin_into_q2d
-from ornl.sans.save_ascii import save_ascii_1D, save_ascii_2D, save_xml_1D
+from ornl.sans.save_ascii import save_ascii_1D, save_xml_1D
 
 from mantid.simpleapi import Load
 import numpy as np
-import  xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
+
 
 def test_save_ascii_1d(refd):
     '''
@@ -18,18 +18,21 @@ def test_save_ascii_1d(refd):
         test_momentum_tranfer_parallel
     '''
 
-    ws = Load(Filename=join(refd.new.eqsans,'test_save_output/EQSANS_68200_iq.nxs'))
+    ws = Load(Filename=join(refd.new.eqsans,
+                            'test_save_output/EQSANS_68200_iq.nxs'))
 
     with tempfile.NamedTemporaryFile('r+') as tmp:
         save_ascii_1D(ws, 'test_reduction_log.hdf', tmp.name)
-        output = np.loadtxt(tmp.name, dtype={'names':('Q', 'I', 'dI', 'dQ'),'formats':('f', 'f', 'f', 'f')})
-        reference = np.loadtxt(join(refd.new.eqsans,'test_save_output/EQSANS_68200_Iq.txt'),
-                                    dtype={'names':('Q', 'I', 'dI', 'dQ'),'formats':('f', 'f', 'f', 'f')})
+        output = np.loadtxt(tmp.name, dtype={'names': ('Q', 'I', 'dI', 'dQ'),
+                                             'formats': ('f', 'f', 'f', 'f')})
+        reference = np.loadtxt(join(refd.new.eqsans,
+                                    'test_save_output/EQSANS_68200_Iq.txt'),
+                               dtype={'names': ('Q', 'I', 'dI', 'dQ'),
+                                      'formats': ('f', 'f', 'f', 'f')})
         assert np.allclose(output['Q'], reference['Q'], atol=1e-6)
         assert np.allclose(output['I'], reference['I'], atol=1e-6)
         assert np.allclose(output['dI'], reference['dI'], atol=1e-6)
         assert np.allclose(output['dQ'], reference['dQ'], atol=1e-6)
-
 
     with tempfile.NamedTemporaryFile('r+') as tmp:
         save_xml_1D(ws, 'test_reduction_log.hdf', tmp.name)
@@ -37,12 +40,15 @@ def test_save_ascii_1d(refd):
         tree = ET.parse(tmp.name)
         root = tree.getroot()
         for node in root[0][2]:
-            output.append([float(node[0].text), float(node[1].text), float(node[2].text), float(node[3].text)])
+            output.append([float(node[0].text), float(node[1].text),
+                           float(node[2].text), float(node[3].text)])
         reference = []
-        tree = ET.parse(join(refd.new.eqsans,'test_save_output/EQSANS_68200_Iq.xml'))
+        tree = ET.parse(join(refd.new.eqsans,
+                             'test_save_output/EQSANS_68200_Iq.xml'))
         root = tree.getroot()
         for node in root[0][2]:
-            reference.append([float(node[0].text), float(node[1].text), float(node[2].text), float(node[3].text)])
+            reference.append([float(node[0].text), float(node[1].text),
+                              float(node[2].text), float(node[3].text)])
         assert np.allclose(output, reference, atol=1e-6)
 
 

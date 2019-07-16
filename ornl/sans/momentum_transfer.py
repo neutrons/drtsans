b9d9c88384baa742c9f3f4f74595756a8e827909
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-07-16 14:56:07 -0400

    Tests for GPSANS pass with new MomentumTransfer class
    Former-commit-id: f6af1c529ca1b83346543ad772e11ac1562dced0

--- ornl/sans/momentum_transfer.py
+++ ornl/sans/momentum_transfer.py
@@ -3,9 +3,11 @@ from __future__ import print_function
 import numpy as np
 from scipy import stats
 
-from mantid.simpleapi import CreateWorkspace, GroupWorkspaces, CreateEmptyTableWorkspace
+from mantid.simpleapi import (CreateWorkspace,
+                              CreateEmptyTableWorkspace)
 from ornl.sans.detector import Component
 from ornl.sans.hfir import resolution
+
 
 class MomentumTransfer:
 
@@ -15,20 +17,20 @@ class MomentumTransfer:
 
     def __init__(self, input_workspace, component_name="detector1",
                  out_ws_prefix="ws"):
-        self.prexix = out_ws_prefix
+        self.prefix = out_ws_prefix
         self.component = Component(input_workspace, component_name)
         self._initialize_qs(input_workspace)
 
     def _initialize_qs(self, input_workspace):
-        
+
         assert input_workspace.blocksize() == 1  # sanity check: only 1 bin
 
         masked_pixels = self.component.masked_ws_indices()
 
         # 1D arrays
-        self.qx, self.qy, self.dqx, self.dqy = resolution.q_resolution_per_pixel(
-            input_workspace)
-        self.i = input_workspace.extractY().ravel().ravel()
+        self.qx, self.qy, self.dqx, self.dqy = \
+            resolution.q_resolution_per_pixel(input_workspace)
+        self.i = input_workspace.extractY().ravel()
         self.i_sigma = input_workspace.extractE().ravel()
 
         # Get rid of the monitors; from 49154 to 49152 spectra
@@ -41,22 +43,25 @@ class MomentumTransfer:
             self._mask_pixels(d, masked_pixels) for d in [
                 self.qx, self.qy, self.dqx, self.dqy, self.i, self.i_sigma]]
 
-    def q2d(self):
-        self._create_table_ws()
+    def __iadd__(self, other):
+        """This is an overload for `+=` operator.
+        Usefull for EQSANS when calculating I(qx,qy) or I(q) from several bins.
 
+        Parameters
+        ----------
+        other : MomentumTransfer class
 
-    def _remove_monitors(self, data):
-        return data[self.component.first_index:
-            self.component.first_index + self.component.dims]
-
-
-    def _mask_pixels(self, data, masked_pixels):
-        return np.ma.MaskedArray(data, masked_pixels, dtype=np.float,
-                                fill_value=np.nan)
-
+        """
+        self.qx = np.concatenate(self.qx, other.qx)
+        self.qy = np.concatenate(self.qy, other.qy)
+        self.dqx = np.concatenate(self.dqx, other.dqx)
+        self.dqy = np.concatenate(self.dqy, other.dqy)
+        self.i = np.concatenate(self.i, other.i)
+        self.i_sigma = np.concatenate(self.i_sigma, other.i_sigma)
 
     def _create_table_ws(self):
-        table_iq = CreateEmptyTableWorkspace(OutputWorkspace=self.prefix+"_table")
+        table_iq = CreateEmptyTableWorkspace(
+            OutputWorkspace=self.prefix+"_iqxqy_table")
         table_iq.addColumn(type="float", name="Qx")
         table_iq.addColumn(type="float", name="Qy")
         table_iq.addColumn(type="float", name="dQx")
@@ -65,7 +70,7 @@ class MomentumTransfer:
         table_iq.addColumn(type="float", name="Sigma(I)")
 
         for qx_i, qy_i, dqx_i, dqy_i, i_i, i_sigma_i in zip(
-            self.qx, self.qy, self.dqx, self.dqy, self.i, self.i_sigma):
+                self.qx, self.qy, self.dqx, self.dqy, self.i, self.i_sigma):
             nextRow = {
                 'Qx': qx_i,
                 'Qy': qy_i,
@@ -77,16 +82,27 @@ class MomentumTransfer:
             table_iq.addRow(nextRow)
         return table_iq
 
+    def q2d(self):
+        return self._create_table_ws()
+
+    def _remove_monitors(self, data):
+        return data[self.component.first_index:
+                    self.component.first_index + self.component.dims]
+
+    def _mask_pixels(self, data, masked_pixels):
+        return np.ma.MaskedArray(data, masked_pixels, dtype=np.float,
+                                 fill_value=np.nan)
 
     def bin_into_q2d(self, bins=None):
-        """Bin the data into Q 2D
+        """Bin the data into Q 2D for visualization only! No dqx/y data!!!
 
         Parameters
         ----------
 
         bins : int or array_like or [int, int] or [array, array], optional
             The bin specification:
-                - If int, the number of bins for the two dimensions (nx=ny=bins).
+                - If int, the number of bins for the two dimensions
+                (nx=ny=bins).
                 - If array_like, the bin edges for the two dimensions
                 (x_edges=y_edges=bins).
                 - If [int, int], the number of bins in each dimension
@@ -100,17 +116,13 @@ class MomentumTransfer:
 
         Returns
         -------
-        WorkSpaceGroup
-            A WorkSpaceGroup containing tuples (ws name, ws) of 3 workspaces.
-            To date Mantid has no option to add uncertainties to the axis.
-            The resolution dx,dy is set in separated workspaces suffixed by
-            `_dqx` and `_dqy`.
+        string, Workspace2D
 
         """
-        
+
         if bins is None:
             bins = [self.component.dim_x, self.component.dim_y]
-        
+
         # Number of bins in Qx Qy is the number of pixels in X and Y
         counts_qx_qy_weights, qx_bin_edges, qy_bin_edges = np.histogram2d(
             self.qx, self.qy, bins=bins)
@@ -118,7 +130,7 @@ class MomentumTransfer:
             self.qx, self.qy, bins=bins, weights=self.i)
         counts_qx_qy /= counts_qx_qy_weights
 
-        # TODO:,Error propagation is wrong
+        # TODO: Error propagation is probably wrong
         counts_dqx_dqy_weights, _, _ = np.histogram2d(
             self.dqx, self.dqy, bins=bins)
         counts_dqx_dqy, _, _ = np.histogram2d(
@@ -127,7 +139,7 @@ class MomentumTransfer:
 
         qy_bin_centers = (qy_bin_edges[1:] + qy_bin_edges[:-1]) / 2.0
 
-        # Q WS
+        # When doing histogram2d the masks are gone
         iqxqy_ws = CreateWorkspace(
             DataX=np.tile(qx_bin_edges, len(qy_bin_centers)),
             DataY=counts_qx_qy.T,
@@ -141,7 +153,6 @@ class MomentumTransfer:
 
         return iqxqy_ws.name(), iqxqy_ws
 
-
     def bin_into_q1d(self, bins=100, statistic='mean'):
         """ Calculates: I(Q) and Dq
         The ws_* input parameters are the output workspaces from bin_into_q2d
@@ -153,13 +164,13 @@ class MomentumTransfer:
 
         bins : int or sequence of scalars, optional
             See `scipy.stats.binned_statistic`.
-            If `bins` is an int, it defines the number of equal-width bins in the
-            given range (10 by default).  If `bins` is a sequence, it defines the
-            bin edges, including the rightmost edge, allowing for non-uniform bin
-            widths.  Values in `x` that are smaller than lowest bin edge are
-            assigned to bin number 0, values beyond the highest bin are assigned to
-            ``bins[-1]``.  If the bin edges are specified, the number of bins will
-            be, (nx = len(bins)-1).
+            If `bins` is an int, it defines the number of equal-width bins in
+            the given range (10 by default).  If `bins` is a sequence, it
+            defines the bin edges, including the rightmost edge, allowing for
+            non-uniform bin widths.  Values in `x` that are smaller than lowest
+            bin edge areassigned to bin number 0, values beyond the highest bin
+            are assigned to ``bins[-1]``.  If the bin edges are specified,
+            the number of bins will be, (nx = len(bins)-1).
         statistic : str, optional
             See `scipy.stats.binned_statistic`.
             The statistic to compute, by default 'mean'
@@ -223,187 +234,114 @@ class MomentumTransfer:
 
         return iq.name(), iq
 
+    def bin_wedge_into_q1d(self, phi_0=0, phi_aperture=30,
+                           bins=100, statistic='mean'):
+        """
+        Wedge calculation and integration
 
-def bin_wedge_into_q1d(ws_iqxqy, ws_dqx, ws_dqy, phi_0=0, phi_aperture=30,
-                       bins=100, statistic='mean', out_ws_prefix="ws_wedge"):
-    """
-    Wedge calculation and integration
+        Calculates: I(Q) and Dq
+        The ws_* input parameters are the output workspaces from bin_into_q2d
 
-    Calculates: I(Q) and Dq
-    The ws_* input parameters are the output workspaces from bin_into_q2d
+        Parameters
+        ----------
+        phi_0 : int, optional
+            Where to start the wedge, by default 0
+        phi_aperture : int, optional
+            Aperture of the wedge, by default 30
+        bins : int or sequence of scalars, optional
+            See `scipy.stats.binned_statistic`.
+            If `bins` is an int, it defines the number of equal-width bins in
+            the given range (10 by default).  If `bins` is a sequence, it
+            defines the bin edges, including the rightmost edge, allowing for
+            non-uniform bin widths.  Values in `x` that are smaller than lowest
+            bin edge areassigned to bin number 0, values beyond the highest bin
+            are assigned to ``bins[-1]``.  If the bin edges are specified,
+            the number of bins will be, (nx = len(bins)-1).
+        statistic : str, optional
+            See `scipy.stats.binned_statistic`.
+            The statistic to compute, by default 'mean'
+            The following statistics are available:
+            * 'mean' : compute the mean of values for points within each bin.
+                Empty bins will be represented by NaN.
+            * 'std' : compute the standard deviation within each bin. This
+                is implicitly calculated with ddof=0.
+            * 'median' : compute the median of values for points within each
+                bin. Empty bins will be represented by NaN.
+            * 'count' : compute the count of points within each bin.  This is
+                identical to an unweighted histogram.  `values` array is not
+                referenced.
+            * 'sum' : compute the sum of values for points within each bin.
+                This is identical to a weighted histogram.
+            * 'min' : compute the minimum of values for points within each bin.
+                Empty bins will be represented by NaN.
+            * 'max' : compute the maximum of values for point within each bin.
+                Empty bins will be represented by NaN.
+            * function : a user-defined function which takes a 1D array of
+                values, and outputs a single numerical statistic. This function
+                will be called on the values in each bin.  Empty bins will be
+                represented by function([]), or NaN if this returns an error.
+        out_ws_prefix : str, optional
+            The prefix of the workspace created in Mantid, by default "ws"
 
-    TODO:
-    The code is almost copy paste bin_into_q1d :(
-    Ideally one would call inside this function bin_into_q1d but mantid
-    does not allowing masking without going through a for cycle and mask
-    by workspace index!
+        Returns
+        -------
+        tuple
+            tuple (workspace name, workspace)
 
-    Parameters
-    ----------
-    ws_iqxqy : Workspace2D
-        I(qx, qy)
-    ws_dqx : Workspace2D
-        dqx(qx, qy)
-    ws_dqy : Workspace2D
-        dqy(qx, qy)
-    phi_0 : int, optional
-        Where to start the wedge, by default 0
-    phi_aperture : int, optional
-        Aperture of the wedge, by default 30
-    bins : int or sequence of scalars, optional
-        See `scipy.stats.binned_statistic`.
-        If `bins` is an int, it defines the number of equal-width bins in the
-        given range (10 by default).  If `bins` is a sequence, it defines the
-        bin edges, including the rightmost edge, allowing for non-uniform bin
-        widths.  Values in `x` that are smaller than lowest bin edge are
-        assigned to bin number 0, values beyond the highest bin are assigned to
-        ``bins[-1]``.  If the bin edges are specified, the number of bins will
-        be, (nx = len(bins)-1).
-    statistic : str, optional
-        See `scipy.stats.binned_statistic`.
-        The statistic to compute, by default 'mean'
-        The following statistics are available:
-          * 'mean' : compute the mean of values for points within each bin.
-            Empty bins will be represented by NaN.
-          * 'std' : compute the standard deviation within each bin. This
-            is implicitly calculated with ddof=0.
-          * 'median' : compute the median of values for points within each
-            bin. Empty bins will be represented by NaN.
-          * 'count' : compute the count of points within each bin.  This is
-            identical to an unweighted histogram.  `values` array is not
-            referenced.
-          * 'sum' : compute the sum of values for points within each bin.
-            This is identical to a weighted histogram.
-          * 'min' : compute the minimum of values for points within each bin.
-            Empty bins will be represented by NaN.
-          * 'max' : compute the maximum of values for point within each bin.
-            Empty bins will be represented by NaN.
-          * function : a user-defined function which takes a 1D array of
-            values, and outputs a single numerical statistic. This function
-            will be called on the values in each bin.  Empty bins will be
-            represented by function([]), or NaN if this returns an error.
-    out_ws_prefix : str, optional
-        The prefix of the workspace created in Mantid, by default "ws_wedge"
+        """
 
-    Returns
-    -------
-    tuple
-         tuple (workspace name, workspace)
+        angle = np.arctan2(self.qy, self.qx)
 
-    """
+        # Let's work in radians
+        phi_0 = np.deg2rad(phi_0)
+        phi_aperture = np.deg2rad(phi_aperture)
 
-    # Calculate Q: qy_bin_centers.shape == (256,)
-    qx_bin_edges_grid = ws_iqxqy.extractX()
-    qy_bin_centers = ws_iqxqy.getAxis(1).extractValues()
+        phi_aperture_min = phi_0 - phi_aperture/2
+        phi_aperture_max = phi_0 + phi_aperture/2
+        # opposite 180 degrees apart
+        phi_aperture_min_pi = phi_aperture_min + np.pi
+        phi_aperture_max_pi = phi_aperture_max + np.pi
 
-    # Qx: qx_bin_centers_grid.shape == (256, 192)
-    qx_bin_centers_grid = (
-        qx_bin_edges_grid[:, 1:] + qx_bin_edges_grid[:, :-1]) / 2.0
+        condition1 = (angle > phi_aperture_min) & \
+            (angle < phi_aperture_max)
+        # make angle > np.pi varying between np.pi and 2*np.pi, rather than the
+        # initial -np.pi to np.pi
+        angle[angle < 0] = 2*np.pi + angle[angle < 0]
+        condition2 = (angle > phi_aperture_min_pi) & \
+            (angle < phi_aperture_max_pi)
+        # True where the wedge is located, otherwise false
+        condition = condition1 | condition2
 
-    # Qy: qy_bin_centers_t_grid.shape == (256, 192)
-    qy_bin_centers_t = qy_bin_centers[np.newaxis].T
-    qy_bin_centers_t_grid = np.tile(
-        qy_bin_centers_t, qx_bin_edges_grid.shape[1]-1)
+        q = np.sqrt(np.square(self.qx) + np.square(self.qy))
+        q = q[condition]
+        i = self.i[condition]
+        i_sigma = self.i_sigma[condition]
 
-    # Q
-    q_bin_centers_grid = np.sqrt(
-        np.square(qx_bin_centers_grid) + np.square(qy_bin_centers_t_grid))
+        intensity_statistic, q_bin_edges, _ = stats.binned_statistic(
+            q, i, statistic=statistic, bins=bins)
 
-    # Angle
-    angle_grid = np.arctan2(qy_bin_centers_t_grid, qx_bin_centers_grid)
+        sigma_statistic, q_bin_edges, _ = stats.binned_statistic(
+            q, i_sigma, statistic=lambda array_1d: np.sqrt(
+                np.sum(np.square(array_1d))) / len(array_1d), bins=bins)
 
-    # This is just to show the angle
-    CreateWorkspace(
-        DataX=qx_bin_edges_grid,
-        DataY=angle_grid,
-        DataE=np.sqrt(angle_grid),
-        NSpec=256,
-        UnitX='MomentumTransfer',
-        VerticalAxisUnit='MomentumTransfer',
-        VerticalAxisValues=qy_bin_centers,
-        OutputWorkspace=out_ws_prefix+"_angle",
-    )
+        # Bin centres in y edges in x
+        dq = np.sqrt(np.square(self.dqx) + np.square(self.dqy))
+        dq = dq[condition]
 
-    # Let's work in radians
-    phi_0 = np.deg2rad(phi_0)
-    phi_aperture = np.deg2rad(phi_aperture)
+        _, dq_bin_edges, _ = stats.binned_statistic(
+            dq, i, statistic=statistic, bins=bins)
 
-    phi_aperture_min = phi_0 - phi_aperture/2
-    phi_aperture_max = phi_0 + phi_aperture/2
-    # opposite 180 degrees apart
-    phi_aperture_min_pi = phi_aperture_min + np.pi
-    phi_aperture_max_pi = phi_aperture_max + np.pi
+        dq_bin_centers = (dq_bin_edges[1:] + dq_bin_edges[:-1]) / 2.0
 
-    condition1 = (angle_grid > phi_aperture_min) & \
-        (angle_grid < phi_aperture_max)
-    # make angle > np.pi varying between np.pi and 2*np.pi, rather than the
-    # initial -np.pi to np.pi
-    angle_grid[angle_grid < 0] = 2*np.pi + angle_grid[angle_grid < 0]
-    condition2 = (angle_grid > phi_aperture_min_pi) & \
-        (angle_grid < phi_aperture_max_pi)
-    # 2D Array: True where the wedge is, otherwise false
-    condition = condition1 | condition2
+        iq = CreateWorkspace(
+            DataX=np.array([q_bin_edges]),
+            DataY=np.array([intensity_statistic]),
+            DataE=np.array([sigma_statistic]),
+            Dx=dq_bin_centers,  # bin centers!!
+            NSpec=1,
+            UnitX='MomentumTransfer',
+            YUnitLabel='Counts',
+            OutputWorkspace=self.prefix+"_wedge_iq"
+        )
 
-    # This is just to show the condition
-    CreateWorkspace(
-        DataX=qx_bin_edges_grid,
-        DataY=condition,
-        DataE=np.sqrt(angle_grid),
-        NSpec=256,
-        UnitX='MomentumTransfer',
-        VerticalAxisUnit='MomentumTransfer',
-        VerticalAxisValues=qy_bin_centers,
-        OutputWorkspace=out_ws_prefix+"_condition",
-    )
-
-    # This transforms a 2D into 1D array
-    q_bin_centers_grid = q_bin_centers_grid[condition]
-
-    # Calculate I(Q) and error(I(Q))
-    i = ws_iqxqy.extractY()
-    sigma_i = ws_iqxqy.extractE()
-
-    i = i[condition]
-    sigma_i = sigma_i[condition]
-
-    assert(q_bin_centers_grid.shape == i.shape == sigma_i.shape)
-
-    intensity_statistic, q_bin_edges, _ = stats.binned_statistic(
-        q_bin_centers_grid, i, statistic=statistic, bins=bins)
-
-    sigma_statistic, q_bin_edges, _ = stats.binned_statistic(
-        q_bin_centers_grid, sigma_i,
-        statistic=lambda array_1d: np.sqrt(
-            np.sum(np.square(array_1d))) / len(array_1d), bins=bins)
-
-    # Calculate dq from dqx dqy
-    dqx_bin_centers_grid = ws_dqx.extractY()
-    dqy_bin_centers_grid = ws_dqy.extractY()
-
-    # Bin centres in y edges in x
-    dq_bin_centers_grid = np.sqrt(
-        np.square(dqx_bin_centers_grid) + np.square(dqy_bin_centers_grid))
-    # get all to centres
-    dq_bin_centers_grid_all = (
-        dq_bin_centers_grid[:, 1:] + dq_bin_centers_grid[:, :-1]) / 2.0
-
-    dq_bin_centers_grid_all = dq_bin_centers_grid_all[condition]
-
-    _, dq_bin_edges, _ = \
-        stats.binned_statistic(dq_bin_centers_grid_all, i,
-                               statistic=statistic, bins=bins)
-
-    dq_bin_centers = (dq_bin_edges[1:] + dq_bin_edges[:-1]) / 2.0
-
-    iq = CreateWorkspace(
-        DataX=np.array([q_bin_edges]),
-        DataY=np.array([intensity_statistic]),
-        DataE=np.array([sigma_statistic]),
-        Dx=dq_bin_centers,  # bin centers!!
-        NSpec=1,
-        UnitX='MomentumTransfer',
-        YUnitLabel='Counts',
-        OutputWorkspace=out_ws_prefix+"_iq"
-    )
-    return iq.name(), iq
-
+        return iq.name(), iq
--- tests/unit/new/ornl/sans/hfir/gpsans/test_momentum_transfer.py
+++ tests/unit/new/ornl/sans/hfir/gpsans/test_momentum_transfer.py
@@ -5,10 +5,10 @@ import tempfile
 
 import numpy as np
 
+import mantid
 from mantid import mtd
-from mantid.simpleapi import LoadHFIRSANS, MaskBTP
-from ornl.sans.momentum_transfer import (bin_into_q1d, bin_into_q2d,
-                                         bin_wedge_into_q1d)
+from mantid.simpleapi import LoadHFIRSANS, MaskBTP, CloneWorkspace
+from ornl.sans.momentum_transfer import MomentumTransfer
 from reduction_workflow.command_interface import AppendDataFile, Reduce
 from reduction_workflow.instruments.sans.hfir_command_interface import (
     GPSANS, AzimuthalAverage, IQxQy, OutputPath, SetBeamCenter)
@@ -25,30 +25,32 @@ def test_momentum_tranfer_wedge_anisotropic(gpsans_f):
         Filename=gpsans_f['anisotropic'],
         OutputWorkspace='aniso_raw')
 
-    wss_name_ws = bin_into_q2d(ws, out_ws_prefix='aniso')
-    assert len(wss_name_ws) == 3
+    mt = MomentumTransfer(ws)
+    assert mt.qx.shape == mt.qy.shape == mt.dqx.shape == mt.dqy.shape == \
+        mt.i.shape == mt.i_sigma.shape == (256*192, )
 
-    ws_iqxqy, ws_dqx, ws_dqy = [ws[1] for ws in wss_name_ws]
-    assert ws_iqxqy.extractY().shape == (256, 192)
-    assert ws_iqxqy.extractX().shape == (256, 193)
+    table_iq = mt.q2d()
+    assert type(table_iq) == mantid.dataobjects.TableWorkspace
 
-    _, ws_iq = bin_into_q1d(ws_iqxqy, ws_dqx, ws_dqy, out_ws_prefix='aniso')
-    assert ws_iq.extractY().shape == (1, 100)
-    assert ws_iq.extractX().shape == (1, 101)
+    ws_name, ws = mt.bin_into_q2d()
+    assert ws.extractY().shape == (256, 192)
+    assert ws.extractX().shape == (256, 193)
 
-    _, ws_iq_feature = bin_wedge_into_q1d(ws_iqxqy, ws_dqx, ws_dqy,
-                                          phi_0=55, phi_aperture=30,
-                                          out_ws_prefix='ws_feature')
+    ws_name, ws = mt.bin_into_q1d()
+    assert ws.extractY().shape == (1, 100)
+    assert ws.extractX().shape == (1, 101)
+
+    _, ws_iq_feature = mt.bin_wedge_into_q1d(phi_0=55, phi_aperture=30)
     ws_iq_feature_i = ws_iq_feature.extractY()
     assert ws_iq_feature_i.shape == (1, 100)
     assert ws_iq_feature.extractX().shape == (1, 101)
+    ws_iq_feature = CloneWorkspace(ws_iq_feature)
 
-    _, ws_iq_non_feature = bin_wedge_into_q1d(ws_iqxqy, ws_dqx, ws_dqy,
-                                              phi_0=55+90, phi_aperture=30,
-                                              out_ws_prefix='ws_non_feature')
+    _, ws_iq_non_feature = mt.bin_wedge_into_q1d(phi_0=55+90, phi_aperture=30)
     ws_iq_non_feature_i = ws_iq_non_feature.extractY()
     assert ws_iq_non_feature_i.shape == (1, 100)
     assert ws_iq_non_feature.extractX().shape == (1, 101)
+    ws_iq_non_feature = CloneWorkspace(ws_iq_non_feature)
 
     # Wedge with feature has more counts than that without it
     assert (ws_iq_feature_i.sum() - ws_iq_non_feature_i.sum()) > 1000
@@ -61,7 +63,9 @@ def test_momentum_tranfer_cross_check(gpsans_f):
 
     GPSANS()
     SetBeamCenter(92, 123)
-    AppendDataFile(gpsans_f['sample_scattering_2'], workspace='ws_legacy')
+    AppendDataFile(
+        gpsans_f['sample_scattering_2'],
+        workspace='ws_legacy')
     AzimuthalAverage(binning="0.01,0.001,0.11")
     OutputPath(tempfile.gettempdir())
     IQxQy(nbins=110, log_binning=False)
@@ -70,12 +74,9 @@ def test_momentum_tranfer_cross_check(gpsans_f):
     ws_legacy = mtd['ws_legacy']
     ws_legacy_iq = mtd['ws_legacy_iq']
 
-    wss_name_ws = bin_into_q2d(ws_legacy, out_ws_prefix='ws_new')
-    ws_new_iqxqy, ws_new_dqx, ws_new_dqy = [ws[1] for ws in wss_name_ws]
+    mt = MomentumTransfer(ws_legacy, out_ws_prefix='ws_new')
 
-    _, ws_new_iq = bin_into_q1d(ws_new_iqxqy, ws_new_dqx, ws_new_dqy,
-                                bins=np.linspace(0.01, 0.11, 101),
-                                out_ws_prefix='ws_new')
+    _, ws_new_iq = mt.bin_into_q1d(bins=np.linspace(0.01, 0.11, 101))
 
     legacy_iq = ws_legacy_iq.extractY()
     new_iq = ws_new_iq.extractY()
@@ -83,35 +84,42 @@ def test_momentum_tranfer_cross_check(gpsans_f):
     legacy_iq = np.nan_to_num(legacy_iq[0])
     new_iq = np.nan_to_num(new_iq[0])
 
-    assert np.allclose(legacy_iq, new_iq, atol=1e-01)
+    assert np.allclose(legacy_iq, new_iq)
 
 
-def test_momentum_tranfer_with_mask(gpsans_f):
+def test_momentum_tranfer_with_and_without_mask(gpsans_f):
     '''
     Test Iq, Iqxqy with the ends of the detector tubes masked
     '''
     filename = gpsans_f['sample_scattering_2']
-    ws = LoadHFIRSANS(Filename=filename, OutputWorkspace='mask_raw')
+    ws_mask = LoadHFIRSANS(Filename=filename, OutputWorkspace='mask_raw')
+    ws_no_mask = LoadHFIRSANS(Filename=filename, OutputWorkspace='no_mask_raw')
 
     # Let's mask the detector ends: 20 + 20
-    MaskBTP(ws, Components='detector1', Pixel='0-19,236-255')
+    MaskBTP(ws_mask, Components='detector1', Pixel='0-19,236-255')
 
-    wss_name_ws = bin_into_q2d(ws, out_ws_prefix='mask')
-    assert len(wss_name_ws) == 3
+    mt_mask = MomentumTransfer(ws_mask, out_ws_prefix='mask')
+    mt_no_mask = MomentumTransfer(ws_no_mask, out_ws_prefix='no_mask')
 
-    ws_iqxqy, ws_dqx, ws_dqy = [ws[1] for ws in wss_name_ws]
-    assert ws_iqxqy.extractY().shape == (256, 192)
-    assert ws_iqxqy.extractX().shape == (256, 193)
+    _, ws_mask = mt_mask.bin_into_q2d()
+    _, ws_no_mask = mt_no_mask.bin_into_q2d()
+
+    # ws_no_mask has more Q range because it was not masked
+    for i in range(10, 19):
+        assert ws_mask.readY(i).any() != ws_no_mask.readY(i).any()
 
     # Let's make sure the I(qx,qy) has masked the values as the detector
-    i = ws_iqxqy.extractY()
+    i = mt_mask.i.filled()
     nans = np.isnan(i)
     assert np.any(nans)
     assert np.count_nonzero(nans) == 192 * (20 + 20)  # 192 tubes
 
-    _, ws_iq = bin_into_q1d(ws_iqxqy, ws_dqx, ws_dqy, out_ws_prefix='mask')
-    assert ws_iq.extractY().shape == (1, 100)
-    assert ws_iq.extractX().shape == (1, 101)
-    # Check if the last I of I(q) are nan
-    ws_iq_end = np.ravel(ws_iq.extractY())[-20:]
-    assert np.all(np.isnan(ws_iq_end))
+    _, ws_mask_iq = mt_mask.bin_into_q1d()
+    assert ws_mask_iq.extractY().shape == (1, 100)
+    assert ws_mask_iq.extractX().shape == (1, 101)
+
+    _, ws_non_mask_iq = mt_no_mask.bin_into_q1d()
+    assert ws_non_mask_iq.extractY().shape == (1, 100)
+    assert ws_non_mask_iq.extractX().shape == (1, 101)
+    # Q range in non mask is higher
+    assert ws_non_mask_iq.extractX().max() > ws_mask_iq.extractX().max()

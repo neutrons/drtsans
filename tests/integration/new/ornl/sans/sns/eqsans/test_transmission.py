5969d6323cd6b1b6733c1dce5abfced585fa8f9c
Jose Borreguero <borreguero@gmail.com>
2019-07-18 18:19:58 -0400

    Refs #55 partial integration test
    Former-commit-id: 47659b9186ed67954253528c2244edb468dd3365

--- tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
+++ tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
@@ -3,42 +3,54 @@ from __future__ import (absolute_import, division, print_function)
 import pytest
 from numpy.testing import assert_almost_equal
 from os.path import join as pjn
-from mantid.simpleapi import Load, CompareWorkspaces
-from ornl.settings import amend_config
-from ornl.sans.transmission import (calculate_transmission,
-                                    _apply_transmission_mantid)
-from ornl.sans.sns.eqsans.transmission import beam_radius, fit_raw
+
+from mantid.simpleapi import LoadNexus, CompareWorkspaces, SaveNexus
+
+from ornl.settings import namedtuplefy, unique_workspace_dundername as uwd
 from ornl.sans.sns.eqsans.geometry import insert_aperture_logs
+from ornl.sans.sns.eqsans import (calculate_transmission,
+                                  apply_transmission_correction)
 
 
-def test_transmission(refd):
+@pytest.fixture(scope='module')
+@namedtuplefy
+def trans_fix(refd):
     data_dir = pjn(refd.new.eqsans, 'test_transmission')
     cmp_dir = pjn(data_dir, 'compare')
 
     def quick_compare(tentative, asset):
-        _ws = Load(pjn(cmp_dir, asset))
-        return CompareWorkspaces(tentative, _ws)
+        r"""asset: str, name of golden standard nexus file"""
+        ws = LoadNexus(pjn(cmp_dir, asset), OutputWorkspace=uwd())
+        return CompareWorkspaces(tentative, ws).Result
 
-    with amend_config({'instrumentName': 'EQSANS'}):
-        # Prepare data
-        sample = Load(pjn(data_dir, 'sample.nxs'))
-        insert_aperture_logs(sample)  # source and sample aperture diameters
-        reference = Load(pjn(data_dir, 'direct_beam.nxs'))
+    a = LoadNexus(pjn(data_dir, 'sample.nxs'))
+    insert_aperture_logs(a)  # source and sample aperture diameters
+    b = LoadNexus(pjn(data_dir, 'direct_beam.nxs'))
+    c = LoadNexus(pjn(data_dir, 'sample_skip.nxs'))
+    d = LoadNexus(pjn(data_dir, 'direct_beam_skip.nxs'))
+    return dict(data_dir=data_dir, sample=a, reference=b, sample_skip=c,
+                reference_skip=d, compare=quick_compare)
 
-        # Calculate raw transmission
-        raw_transmission = calculate_transmission(sample, reference,
-                                                  beam_radius(sample))
-        quick_compare(raw_transmission, 'raw_transmission.nxs')
 
-        # Fit the raw transmission
-        fitted = fit_raw(raw_transmission, 'fitted_transm')
-        quick_compare(fitted.transmission, 'fitted_transmission.nxs')
-        assert_almost_equal(fitted.lead_mfit.OutputChi2overDoF, 1.1, decimal=1)
+def test_calculate_transmission(trans_fix):
+    # Raw transmission values
+    raw = calculate_transmission(trans_fix.sample_skip,
+                                 trans_fix.reference_skip, radius=50,
+                                 fit_func=None)
+    SaveNexus(raw, '/tmp/raw_transmission_skip.nxs')
+    raw = calculate_transmission(trans_fix.sample, trans_fix.reference,
+                                 fit_func=None)
+    assert trans_fix.compare(raw, 'raw_transmission.nxs')
+    # Fitted transmission values
+    fitted = calculate_transmission(trans_fix.sample, trans_fix.reference)
+    trans_fix.compare(fitted, 'fitted_transmission.nxs')
+    assert_almost_equal(fitted.lead_mfit.OutputChi2overDoF, 1.1, decimal=1)
 
-        # Apply the fitted transmission
-        corr = _apply_transmission_mantid(sample, trans_ws=fitted.transmission,
-                                          theta_dependent=True)
-        quick_compare(corr, 'sample_corrected.nxs')
+
+def test_apply_transmission(trans_fix):
+    corr = apply_transmission_correction(trans_fix.sample, trans_fix.reference,
+                                         output_workspace=uwd())
+    trans_fix.compare(corr, 'sample_corrected.nxs')
 
 
 if __name__ == '__main__':

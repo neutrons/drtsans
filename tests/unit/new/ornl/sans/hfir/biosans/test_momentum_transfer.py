d3d553adba08ba239792d29e09afe6c1b5f9b18c
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-07-19 13:26:15 -0400

    Working in parallel with pathos
    Former-commit-id: a01f45e4d18dd3c037ddb1a61ef5dc39746d9f7a

--- tests/unit/new/ornl/sans/hfir/biosans/test_momentum_transfer.py
+++ tests/unit/new/ornl/sans/hfir/biosans/test_momentum_transfer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from multiprocessing import Pool
-
-import pytest
+from pathos.multiprocessing import ProcessingPool
 
 import mantid
 from mantid import mtd
@@ -53,6 +51,19 @@ def test_momentum_tranfer_serial(biosans_f):
 
 
 def bin_in_parallel(params):
+    """Runs mantid algorithms in parallel
+
+    Parameters
+    ----------
+    params : list
+        The parameters of this function have to be passed in a list.
+        `map` does not support multiple algorithms
+
+    Returns
+    -------
+    list
+        Mantid loses `ws.name()`. We passing a list of ws_name amd WSs.
+    """
 
     component_name, out_ws_prefix = params
     ws = mtd["ws_data_raw"]
@@ -60,55 +71,44 @@ def bin_in_parallel(params):
                           out_ws_prefix=out_ws_prefix)
 
     table_iq = mt.q2d()
+    ws_q2d_name, ws_q2d = mt.bin_into_q2d()
+    ws_q1d_name, ws_q1d = mt.bin_into_q1d()
 
-    ws_q2d_name, ws = mt.bin_into_q2d()
-
-    ws_q1d_name, ws = mt.bin_into_q1d()
-
-    logger.notice("Returning from the parallel calculation: {} {} {}".format(
-        table_iq.name(), ws_q2d_name, ws_q1d_name))
-    logger.notice("Workspaces names in the parallel function: {}".format(
-        mtd.getObjectNames()))
-
-    return table_iq.name(), ws_q2d_name, ws_q1d_name
+    return ((table_iq.name(), table_iq), (ws_q2d_name, ws_q2d),
+            (ws_q1d_name, ws_q1d))
 
 
-@pytest.mark.skip(reason="Does not create the WSs in parallel."
-                  "Waiting for the mantid team solve the bug")
 def test_momentum_tranfer_parallel(biosans_f):
     '''
-    For now let's skip this test. it does not run as part of multiple tests
-    It runs as a single test though:
-    pytest tests/unit/new/ornl/sans/hfir/biosans/test_momentum_transfer.py::\
-        test_momentum_tranfer_parallel
+    Note that we are using `pathos`. That's the only way to serialize
+    Mantid WSs back to python.
     '''
 
     ws = LoadHFIRSANS(
         # Filename='/SNS/EQSANS/shared/sans-backend/data/new/ornl/sans/hfir/'
-        #     'biosans/BioSANS_exp440_scan0022_0006.xml',
+        # 'biosans/BioSANS_exp440_scan0022_0006.xml',
         Filename=biosans_f['anisotropic'],
         OutputWorkspace="ws_data_raw")
 
     parameters = [('detector1', 'main'), ('wing_detector', 'wing')]
-    with Pool(processes=2) as pool:
+
+    with ProcessingPool(nodes=2) as pool:
         return_list = pool.map(bin_in_parallel, parameters)
 
-    logger.notice("After launching return_list: {}".format(return_list))
-    logger.notice("After launching we have the WSs: {}".format(
-        mtd.getObjectNames()))
-
     for ret in return_list:
-        if ret[0].startswith('wing'):
-            y_shape = (256, 160)
-            x_shape = (256, 161)
-        else:
-            y_shape = (256, 192)
-            x_shape = (256, 193)
+        for ws_name, ws in ret:
+            logger.notice("Testing WS: {}".format(ws_name))
+            if ws_name.startswith('wing'):
+                y_shape = (256, 160)
+                x_shape = (256, 161)
+            else:
+                y_shape = (256, 192)
+                x_shape = (256, 193)
 
-        ws = mtd[ret[1]]
-        assert ws.extractY().shape == y_shape
-        assert ws.extractX().shape == x_shape
+            if ws_name.endswith('qxqy'):
+                assert ws.extractY().shape == y_shape
+                assert ws.extractX().shape == x_shape
 
-        ws = mtd[ret[2]]
-        assert ws.extractY().shape == (1, 100)
-        assert ws.extractX().shape == (1, 101)
+            if ws_name.endswith('iq'):
+                assert ws.extractY().shape == (1, 100)
+                assert ws.extractX().shape == (1, 101)

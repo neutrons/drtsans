4d99139f76ffafa1b85ce86f1a3b6f40385dbc5d
Jose Borreguero <borreguero@gmail.com>
2019-07-22 17:29:23 -0400

    Refs #55 flaky flake8
    Former-commit-id: 93131d0584016298f5ba539d847f32c47b43816e

--- tests/unit/new/ornl/sans/hfir/biosans/test_save_ascii.py
+++ tests/unit/new/ornl/sans/hfir/biosans/test_save_ascii.py
@@ -34,7 +34,7 @@ def test_save_ascii(biosans_sensitivity_dataset):
     with tempfile.NamedTemporaryFile('r+') as tmp:
         save_ascii_1D(ws_iq, 'Test BioSANS', tmp.name)
         output_lines = tmp.readlines()
-        numbers =(0.112749, 107368.964140, 2815.765996, 0.021596)
+        numbers = (0.112749, 107368.964140, 2815.765996, 0.021596)
         assert numbers_in_line(output_lines[101], numbers) is True
 
     with tempfile.NamedTemporaryFile('r+') as tmp:

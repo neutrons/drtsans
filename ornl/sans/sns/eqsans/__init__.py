caf79ca407e2c49883d1c9818e1ff72ece2caf13
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2019-07-24 09:31:11 -0400

    fixed flake8
    Former-commit-id: c22a23cc301d247eb7f248aad5ff5326512c3c23

--- ornl/sans/sns/eqsans/__init__.py
+++ ornl/sans/sns/eqsans/__init__.py
@@ -1,7 +1,7 @@
 from ornl.sans.sns.eqsans.momentum_transfer import (prepare_momentum_transfer,
                                                     iq, iqxqy)
 from ornl.sans.transmission import apply_transmission_correction
-from .load import *   # noqa: F403
+from .load import *  # noqa: F403
 from .geometry import *  # noqa: F403
 from .correct_frame import *  # noqa: F403
 from .dark_current import *  # noqa: F403
@@ -11,6 +11,23 @@ from .beam_finder import *  # noqa: F403
 from .transmission import *  # noqa: F403
 from .api import *  # noqa: F403, F401
 
-mods = (load, geometry, correct_frame, dark_current,  # noqa: F405
-        mask, normalisation, beam_finder, transmission, api)  # noqa: F405)
+mods = (
+    load,  # noqa: F405
+    geometry,  # noqa: F405
+    correct_frame,  # noqa: F405
+    dark_current,  # noqa: F405
+    mask,  # noqa: F405
+    normalisation,  # noqa: F405
+    beam_finder,  # noqa: F405
+    transmission,  # noqa: F405
+    api)  # noqa: F405)
+
 __all__ = [s for m in mods for s in m.__all__]
+
+# Flake8: when a function is imported it needs to be used
+__all__.extend([
+    str(prepare_momentum_transfer),
+    str(iq),
+    str(iqxqy),
+    str(apply_transmission_correction)
+])

d2d4e249e8a8161eacdfb307c3b8e0bb91125efb
Jose Borreguero <borreguero@gmail.com>
2019-08-03 20:19:37 -0400

    Refs #129 add fixture serve_events_workspace
    Former-commit-id: b158530370a11405b1d12ff9e5f49b92a2ac8b3a

--- tests/unit/new/ornl/sans/sns/eqsans/test_geometry.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_geometry.py
@@ -1,43 +1,29 @@
 import pytest
 from pytest import approx
-from mantid.simpleapi import LoadEventNexus, CloneWorkspace
-from ornl.settings import amend_config, unique_workspace_dundername as uwd
 from ornl.sans.sns.eqsans.geometry import (sample_aperture_diameter,
                                            source_aperture_diameter,
                                            source_monitor_distance)
 from ornl.sans.samplelogs import SampleLogs
 
 
-@pytest.fixture(scope='function')
-def ws(refd):
-    if ws._original is None:
-        with amend_config(data_dir=refd.new.eqsans):
-            ws._original = LoadEventNexus('EQSANS_92353', OutputWorkspace=uwd())
-            ws._all_ws.append(ws._original)
-    clone = CloneWorkspace(ws._original, OutputWorkspace=uwd())
-    ws._all_ws.append(clone)
-    yield CloneWorkspace(ws._original, OutputWorkspace=uwd())
-    [w.delete() for w in ws._all_ws]
-
-
-ws._original, ws._all_ws = None, list()
-
-
-def test_sample_aperture_diameter(ws):
+def test_sample_aperture_diameter(serve_events_workspace):
+    ws = serve_events_workspace('EQSANS_92353')
     sad = sample_aperture_diameter(ws)
     assert sad == approx(10)
     sad = SampleLogs(ws).single_value('sample-aperture-diameter')
     assert sad == approx(10)
 
 
-def test_source_aperture_diameter(ws):
+def test_source_aperture_diameter(serve_events_workspace):
+    ws = serve_events_workspace('EQSANS_92353')
     sad = source_aperture_diameter(ws)
     assert sad == approx(20)
     sad = SampleLogs(ws).single_value('source-aperture-diameter')
     assert sad == approx(20)
 
 
-def test_source_monitor_distance(ws):
+def test_source_monitor_distance(serve_events_workspace):
+    ws = serve_events_workspace('EQSANS_92353')
     smd = source_monitor_distance(ws, unit='m')
     assert smd == approx(10.122, abs=0.001)
     smd = SampleLogs(ws).single_value('source-monitor-distance')

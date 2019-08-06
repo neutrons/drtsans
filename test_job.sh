0d80ec141b7d7c8123ecf73e6519f8db3d7256b2
Pete Peterson <petersonpf@ornl.gov>
2019-08-06 16:08:41 -0400

    Add junit results from the tests
    Former-commit-id: 6e93349bbc2e248b8d0fe45d189920954a24eb97

--- test_job.sh
+++ test_job.sh
@@ -6,4 +6,5 @@ TEST_SCOPE=$1  # 'unit' or 'integration'
 
 source activate mantid
 cd /opt/sans-backend
-pytest -v /opt/sans-backend/tests/${TEST_SCOPE}
+echo "Writing tests results to /tmp/sans-backend/${TEST_SCOPE}_test_results.xml"
+pytest -v /opt/sans-backend/tests/${TEST_SCOPE} --junitxml=/tmp/${TEST_SCOPE}_test_results.xml

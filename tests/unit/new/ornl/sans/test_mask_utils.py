5b8752445b656f303534bf0d23bc29ab746a55bb
Jose Borreguero <borreguero@gmail.com>
2019-08-14 12:18:51 -0400

    Refs #167 test apply mask to simple histograms
    Former-commit-id: 8f161b0c55ea9c3cd65d9e6a15e57fe19015bc9d

--- tests/unit/new/ornl/sans/test_mask_utils.py
+++ tests/unit/new/ornl/sans/test_mask_utils.py
@@ -25,25 +25,26 @@ def test_apply_mask_single_bin(generate_sans_generic_IDF):
                          OutputWorkspace=uwd())
     LoadInstrument(Workspace=ws, InstrumentXML=generate_sans_generic_IDF,
                    RewriteSpectraMap=True, InstrumentName='GenericSANS')
+
     # detectors to be masked with detector ID's 0 and 3
     masked_detectors = [3, 0]
-    intensities_after_mask = np.copy(intensities)
-    intensities_after_mask[masked_detectors] = 0
+    apply_mask(ws, mask=masked_detectors)
+
+    # check mask-flags
+    si = ws.spectrumInfo()
+    mask_flags = [False]*9
+    for i in masked_detectors:
+        mask_flags[i] = True
+    assert [si.isMasked(i) for i in range(9)] == mask_flags
 
     # check masked data
-    apply_mask(ws, mask=masked_detectors)
+    intensities_after_mask = np.copy(intensities)
+    intensities_after_mask[masked_detectors] = 0
     assert ws.extractY().flatten() == approx(intensities_after_mask, abs=0.1)
 
     # errors also set to zero for the masked spectra
     errors_after_mask = np.sqrt(intensities_after_mask)
     assert ws.extractE().flatten() == approx(errors_after_mask, abs=1e-9)
-    si = ws.spectrumInfo()
-
-    # check mask-flags
-    mask_flags = [False]*9
-    for i in masked_detectors:
-        mask_flags[i] = True
-    assert [si.isMasked(i) for i in range(9)] == mask_flags
 
     # check the value of a masked detector is irrelevant when doing
     # operations on the whole workspace
@@ -61,25 +62,63 @@ def test_apply_mask_single_bin(generate_sans_generic_IDF):
     intensities_after_mask[trap_detector_id] = 0
     assert ws.extractY().flatten() == approx(intensities_after_mask, abs=0.1)
 
+    # Clean up
+    ws.delete()
+    ws_sum.delete()
+
 
 @pytest.mark.parametrize('generate_sans_generic_IDF',
                          [{'Nx': 2, 'Ny': 2, 'dx': 0.00425,
                            'dy': 0.0055, 'xc': 0.32, 'yc': -0.16}],
                          indirect=True)
-def test_apply_mask_simple(generate_sans_generic_IDF):
+def test_apply_mask_simple_histogram(generate_sans_generic_IDF):
+    r"""Four histograms, each containing three bins"""
     wavelength = np.array([1., 2., 3., 4.] * 4)
-    intensities = np.array([9, 10, 11, 3],
-                           [8, 12, 4, 14],
-                           [11, 15, 3, 16]).transpose()
-    ws = CreateWorkspace(DataX=wavelength,
-                         DataY=intensities,
+    intensities = np.array([[9, 10, 11, 3],
+                            [8, 12, 4, 14],
+                            [11, 15, 3, 16]]).transpose()
+    ws = CreateWorkspace(DataX=wavelength, DataY=intensities,
                          DataE=np.sqrt(intensities),
-                         Nspec=4)
+                         Nspec=4, OutputWorkspace=uwd())
     LoadInstrument(Workspace=ws, InstrumentXML=generate_sans_generic_IDF,
                    RewriteSpectraMap=True, InstrumentName='GenericSANS')
+
+    # detector to be masked with detector ID 2
     masked_detectors = [2]
     apply_mask(ws, mask=masked_detectors)
-    assert ws.readY(masked_detectors) == approx(np.zeros(3), abs=1e-6)
+
+    # check mask-flags
+    si = ws.spectrumInfo()
+    mask_flags = [False, False, True, False]
+    assert [si.isMasked(i) for i in range(4)] == mask_flags
+
+    # check masked data
+    intensities_after_mask = np.copy(intensities)
+    intensities_after_mask[masked_detectors] = 0
+    assert ws.extractY() == approx(intensities_after_mask, abs=0.1)
+
+    # errors also set to zero for the masked spectra
+    errors_after_mask = np.sqrt(intensities_after_mask)
+    assert ws.extractE() == approx(errors_after_mask, abs=1e-6)
+
+    # check the value of a masked detector is irrelevant when doing
+    # operations on the whole workspace
+    big_value = 1.0e6
+    ws.setY(2, [big_value]*3)
+    ws_sum = SumSpectra(ws, OutputWorkspace=uwd())
+    assert ws_sum.dataY(0) == approx(np.sum(intensities_after_mask, axis=0))
+    intensities_after_mask[2] = big_value
+
+    # Now apply an additional mask mimicking the trap beam
+    x_c, y_c = (1, 1)  # location of the trap beam, in pixel coordinates
+    pixels_per_tube = 2
+    trap_detector_id = x_c * pixels_per_tube + y_c
+    apply_mask(ws, mask=[trap_detector_id])
+    intensities_after_mask[trap_detector_id] = 0
+    assert ws.extractY() == approx(intensities_after_mask, abs=0.1)
+
+    # Clean up
+    ws.delete()
 
 
 def test_apply_mask():

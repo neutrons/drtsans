f09561418d23781a20eaa44bc672d9d953e5ab5e
Steven Hahn <hahnse@ornl.gov>
2019-08-21 16:30:04 -0400

    Separate into reusable and instrument-specific pieces
    Former-commit-id: 58af55f3a5808d7b452a1d672d4f33a9cc3ad719

--- ornl/sans/sns/eqsans/dark_current.py
+++ ornl/sans/sns/eqsans/dark_current.py
@@ -8,81 +8,11 @@ from mantid.simpleapi import (mtd, Integration, Transpose, RebinToWorkspace,
 from ornl.settings import (namedtuplefy, amend_config,
                            unique_workspace_dundername as uwd)
 from ornl.path import exists, registered_workspace
+from ornl.sans.dark_current import duration, counts_in_detector
 from ornl.sans.samplelogs import SampleLogs
 from ornl.sans.sns.eqsans import correct_frame as cf
 
 __all__ = ['subtract_dark_current', ]
-
-
-@namedtuplefy
-def duration(input_workspace, log_key=None):
-    """
-    Compute the duration of the workspace by iteratively searching the logs for
-    keys 'duration', 'start_time/end_time', 'proton_charge', and 'timer'.
-
-    Parameters
-    ----------
-    input_workspace: str, MatrixWorkspace
-        Usually the dark current workspace
-    log_key: str
-        If a log entry is passed, only the contents under this log entry are
-        searched. No iterative search over the default values is performed.
-
-    Returns
-    -------
-    namedtuple
-        Fields of the namedtuple:
-        - value: float, contents under the log
-        - log_key: str, log used to return the duration
-
-    """
-    ws = mtd[str(input_workspace)]
-    log_keys = ('duration', 'start_time', 'proton_charge', 'timer') if \
-        log_key is None else (log_key, )
-    sl = SampleLogs(ws)
-
-    def from_start_time(lk):
-        st = parse_date(sl[lk].value)
-        et = parse_date(sl['end_time'].value)
-        return (et - st).total_seconds()
-
-    def from_proton_charge(lk):
-        return sl[lk].getStatistics().duration
-
-    calc = dict(start_time=from_start_time, proton_charge=from_proton_charge)
-
-    for lk in log_keys:
-        try:
-            return dict(value=calc.get(lk, sl.single_value)(lk), log_key=lk)
-        except RuntimeError:
-            continue
-    raise AttributeError("Could not determine the duration of the run")
-
-
-def counts_in_detector(input_workspace):
-    r"""
-    Fin the total number of neutron counts in each detector pixel.
-    By definition, error=1 when zero counts in the detector.
-
-    Parameters
-    ----------
-    input_workspace: str, EventsWorkspace
-        Usually the dark current workspace
-
-    Returns
-    -------
-    tuple
-        Two elements in the tuple: (1)numpy.ndarray: counts;
-        (2) numpy.ndarray: error in the counts
-    """
-    ws = mtd[str(input_workspace)]
-    _wnc = Integration(ws, OutputWorkspace=uwd())
-    _wnc = Transpose(_wnc, OutputWorkspace=_wnc.name())
-    y = np.copy(_wnc.dataY(0))  # counts
-    e = np.copy(_wnc.dataE(0))  # errors
-    _wnc.delete()
-    e[y < 1] = 1.0  # convention of error=1 if no counts present
-    return y, e
 
 
 def normalise_to_workspace(dark_ws, data_ws, output_workspace=None):
--- tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_dark_current.py
@@ -21,19 +21,6 @@ def wss(refd):
         return dict(data=data, dark=dark)
 
 
-def test_duration(wss):
-    for lk in ('start_time', 'proton_charge'):
-        assert dkc.duration(wss['dark'], lk).value == approx(7200.0, abs=1.0)
-    with pytest.raises(AttributeError):
-        dkc.duration(wss['dark'], log_key='timer')
-
-
-def test_counts_in_detector(wss):
-    y, e = dkc.counts_in_detector(wss['dark'])
-    assert max(y) == 67.0
-    assert (y[0], e[0]) == (0.0, 1.0)
-
-
 def test_normalise_to_workspace(wss, refd):
     _w0 = dkc.normalise_to_workspace(wss['dark'], wss['data'],
                                      output_workspace=uwd())

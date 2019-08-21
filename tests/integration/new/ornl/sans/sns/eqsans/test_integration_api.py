391ca8d0e1963a7ef4ea74f031bc0bc65aae2e34
Jose Borreguero <borreguero@gmail.com>
2019-08-21 06:44:16 -0400

    Refs #188 adjust tolerance parameter
    Former-commit-id: 27fd388768be66c5f3a83c4050db1566271f82ff

--- tests/integration/new/ornl/sans/sns/eqsans/test_integration_api.py
+++ tests/integration/new/ornl/sans/sns/eqsans/test_integration_api.py
@@ -9,7 +9,7 @@ from mantid.dataobjects import EventWorkspace
 # https://docs.mantidproject.org/nightly/algorithms/CompareWorkspaces-v1.html
 # https://docs.mantidproject.org/nightly/algorithms/LoadNexus-v1.html
 # https://docs.mantidproject.org/nightly/algorithms/SumSpectra-v1.html
-from mantid.simpleapi import SumSpectra, mtd, LoadNexus, CompareWorkspaces
+from mantid.simpleapi import SumSpectra, mtd, LoadNexus, CompareWorkspaces, SaveNexus
 
 # public API
 from ornl.sans.sns import eqsans
@@ -140,7 +140,8 @@ def test_prepare_monitors(refd):
         assert sl.wavelength_min.value == approx(9.9, abs=0.1)
         assert sl.wavelength_max.value == approx(13.6, abs=0.1)
         v = LoadNexus('EQSANS_88565_monitors_wav.nxs', OutputWorkspace=uwd())
-        assert CompareWorkspaces(w, v).Result is True
+        assert CompareWorkspaces(w, v, Tolerance=1e-3,
+                                 ToleranceRelErr=True).Result is True
 
 
 @pytest.mark.skip(reason="prepare data not yet completed")

eda04d52d598857ee79cd634e330d78ebf7274e0
Wenduo Zhou <zhouw@ornl.gov>
2019-08-24 23:45:31 -0400

    Refs #163. Added unit test for initial uncertainties.
    Former-commit-id: 78d4027be484f509f2a4df95b56d884c7537a13f

---
+++ tests/unit/new/ornl/sans/test_initial_uncertainty.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+# https://docs.mantidproject.org/nightly/algorithms/LoadEmptyInstrument-v1.html
+from mantid.simpleapi import LoadEmptyInstrument
+from ornl.sans.process_uncertainties import set_init_uncertainties
+
+
+# This implements Issue #163
+# in master document section 3.3
+# dev - Pete Peterson <petersonpf@ornl.gov>
+# SME - William Heller <hellerwt@ornl.gov>
+@pytest.mark.parametrize('generate_sans_generic_IDF',
+                         [{'Nx': 2, 'Ny': 2,
+                           'dx': 0.005, 'dy': 0.004, 'zc': 2.5}],
+                         indirect=True)
+def test_initial_uncertainty(generate_sans_generic_IDF):
+    """
+    Test initial uncertainty after histogram data is converted to unit
+    wavelength for a TOF instrument
+    :param generate_sans_generic_IDF: IDF to generate
+    :return:i
+    """
+    # Range of TOF
+    TOF = np.array([12345., 12346.])  # microseconds
+    intensity = np.array([0., 187., 1., np.nan])
+    init_delta_intensity = np.random.randn(intensity.shape[0], )
+    gold_delta_intensity = np.array([1., np.sqrt(187.), 1., np.nan])
+
+    # Generate a generic SANS instrument with a pixel of
+    # the size and position specified in
+    # sans-backend/documents/Master_document_022219.pdf
+    with open(r'/tmp/GenericSANS_Definition.xml', 'w') as tmp:
+        tmp.write(generate_sans_generic_IDF)
+        tmp.close()
+    ws = LoadEmptyInstrument(Filename=tmp.name, InstrumentName='GenericSANS')
+    ws.getAxis(0).setUnit('TOF')
+    # assume that the TOF is already frame corrected
+    for i in range(4):
+        ws.dataX(i)[:] = TOF  # microseconds
+        ws.dataY(i)[0] = intensity[i]
+        ws.dataE(i)[0] = init_delta_intensity[i]
+    # #### ABOVE THIS POINT WILL BE A TEST FIXTURE
+
+    # Set uncertainties
+    ws = set_init_uncertainties(ws)
+
+    for ws_index in range(ws.getNumberHistograms()):
+        if np.isnan(gold_delta_intensity[ws_index]):
+            assert np.isnan(ws.dataE(ws_index)[0])
+        else:
+            assert abs(ws.dataE(ws_index)[0] -
+                       gold_delta_intensity[ws_index]) < 1.E-10
+
+
+if __name__ == '__main__':
+    pytest.main()

1d9db4c0408ef31c61dc9d5f00efe47ec54c25ec
Steven Hahn <hahnse@ornl.gov>
2019-08-26 17:00:46 -0400

    Refs #195 fix up formatting
    Former-commit-id: 9f95061e710c16b4d1b6fee3ec2e267d8bf29a76

--- tests/integration/new/ornl/sans/hfir/test_normalization.py
+++ tests/integration/new/ornl/sans/hfir/test_normalization.py
@@ -1,34 +1,36 @@
 import pytest
 import numpy as np
 
+from ornl.sans.samplelogs import SampleLogs
+from ornl.sans.hfir.normalisation import time, monitor
 
 def test_normalization_by_time():
-    x,y = np.meshgrid(np.linspace(-1, 1, 5), np.linspace(-1,1, 5))
-    z = ( x*0 + y*0  + 1 ) # constant image
-    sigma, mu = 0.3, 0 # gaussian image
-    d=np.sqrt(x*x + y*y)
-    g=np.exp(-((d-mu)**2/(2 * sigma**2)))
-    z = (x+5*y)*10 + 100
+    x, y = np.meshgrid(np.linspace(-1, 1, 5), np.linspace(-1, 1, 5))
+    z = (x*0 + y*0 + 1)  # constant image
+    sigma, mu = 0.3, 0  # gaussian image
+    d = np.sqrt(x*x + y*y)
+    g = np.exp(-((d-mu)**2/(2 * sigma**2)))
+    z = (x + 5 * y) * 10 + 100
 
-    I_sam = z #choose sample data from g or z
+    I_sam = z  # choose sample data from g or z
     I_sam_err = np.sqrt(z)
 
-    t_sam = 5 # any value
-    t_sam_err = 0.2 # 2 percent error
+    t_sam = 5  # any value
+    t_sam_err = 0.2  # 2 percent error
 
     I_samnorm = I_sam / t_sam
-    I_norm_err = np.sqrt((I_sam_err/t_sam)**2 + (I_sam * t_sam_err / t_sam**2 )**2)
+    I_norm_err = np.sqrt((I_sam_err / t_sam)**2 + (I_sam * t_sam_err / t_sam**2)**2)
 
 
 def test_normalization_by_monitor():
-    x,y = np.meshgrid(np.linspace(-1, 1, 5), np.linspace(-1,1, 5))
-    z = ( x*0 + y*0  + 1 ) # constant image
-    sigma, mu = 0.3, 0 # gaussian image
-    d=np.sqrt(x*x + y*y)
-    g=np.exp(-((d-mu)**2/(2 * sigma**2)))
-    z = (x+5*y)*10 + 100
+    x, y = np.meshgrid(np.linspace(-1, 1, 5), np.linspace(-1, 1, 5))
+    z = (x*0 + y*0 + 1)  # constant image
+    sigma, mu = 0.3, 0  # gaussian image
+    d = np.sqrt(x*x + y*y)
+    g = np.exp(-((d-mu)**2/(2 * sigma**2)))
+    z = (x + 5 * y) * 10 + 100
 
-    I_sam = z #choose sample data from g or z
+    I_sam = z  # choose sample data from g or z
     
     factor_is = 10**8
     flux_sam = 5 * 10**8

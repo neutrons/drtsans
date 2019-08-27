e46521c39624a46041eeeb43226f8e4a61f5e00e
Pete Peterson <petersonpf@ornl.gov>
2019-08-27 16:52:50 -0400

    Filled in William's test
    Former-commit-id: acd163851328a8570f41c635845ed3880909ddc3

--- tests/unit/new/ornl/sans/sns/eqsans/test_convert_tof_to_wavelength.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_convert_tof_to_wavelength.py
@@ -4,34 +4,39 @@ from ornl.sans.samplelogs import SampleLogs
 from ornl.sans.sns.eqsans.correct_frame import convert_to_wavelength
 
 
-'''
+def add_frame_skipping_log(ws):
+    samplelog = SampleLogs(ws)
+    samplelog.insert('is_frame_skipping', False)
+
+
 # TODO expand on the next statement
 # in master document section 3.3
 # dev - Pete Peterson <petersonpf@ornl.gov>
 # SME - William Heller <hellerwt@ornl.gov>
-@pytest.mark.parametrize('generate_sans_generic_IDF',
-                         [{'Nx': 2, 'Ny': 2}],
+@pytest.mark.parametrize('generic_workspace',
+                         [{'dx': .005, 'dy': .004,
+                           'zc': 5., 'l1': 14.,
+                           'axis_units': 'tof',
+                           'axis_values': [15432.]}],
                          indirect=True)
-@pytest.mark.parametrize('I, dI',
-                         [(187., np.sqrt(187.)),
-                          (0., 1.),
-                          (np.nan, np.nan)],
-                         ids=('I=187', 'I=0', 'I=NaN'))
-def test_william(generate_sans_generic_IDF, I, dI):
-    # generate a generic SANS instrument with a pixel of
-    # the size and position specified in
-    # sans-backend/documents/Master_document_022219.pdf
-    with open(r'/tmp/GenericSANS_Definition.xml', 'w') as tmp:
-        tmp.write(generate_sans_generic_IDF)
-        tmp.close()
-    ws = LoadEmptyInstrument(Filename=tmp.name, InstrumentName='GenericSANS')
+def test_william(generic_workspace):
+    ws = generic_workspace  # friendly name
+    add_frame_skipping_log(ws)
 
-    ws.dataY(1)[0] = I
-    # TODO set uncertainty using non-existant function
+    ws = convert_to_wavelength(input_workspace=generic_workspace)
 
-    assert ws.dataY(1)[0] == I
-    assert ws.dataE(1)[0] == dI
-'''
+    specInfo = ws.spectrumInfo()
+    source_sample = specInfo.l1()  # in meters
+
+    # make sure the unit is wavelength
+    assert ws.getAxis(0).getUnit().caption() == 'Wavelength'
+    # verify the individual wavelength values
+    for i in range(4):
+        sample_detector = specInfo.l2(i)  # to detector pixel in meters
+        # equation taken
+        lambda_exp = 3.9560346e-3 * np.array([15432.]) / (source_sample + sample_detector)
+        assert ws.dataX(i)[0] == pytest.approx(lambda_exp[0])
+        assert ws.dataX(i)[0] == pytest.approx(3.2131329446)
 
 
 TOF = [12345., 12346.]
@@ -43,15 +48,13 @@ TOF = [12345., 12346.]
 # SME - Shuo Qian
 @pytest.mark.parametrize('generic_workspace',
                          [{'dx': .005, 'dy': .004,
-                           'zc': 2.5, 'L1': 10.1,
+                           'zc': 2.5, 'l1': 10.1,
                            'axis_units': 'tof',
-                           'axis_values': TOF,
-                           'intensities': [[1., 1.], [1., 1.]]}],
+                           'axis_values': TOF}],
                          indirect=True)
 def test_shuo(generic_workspace):
     ws = generic_workspace  # friendly name
-    samplelog = SampleLogs(ws)
-    samplelog.insert('is_frame_skipping', False)
+    add_frame_skipping_log(ws)
 
     ws = convert_to_wavelength(input_workspace=generic_workspace)
 
@@ -67,7 +70,7 @@ def test_shuo(generic_workspace):
         lambda_exp = 3.9560346e-3 * np.array(TOF) / (source_sample + sample_detector)
         assert ws.dataX(i)[0] == pytest.approx(lambda_exp[0])
         assert ws.dataX(i)[1] == pytest.approx(lambda_exp[1])
-        assert ws.dataX(i)[0] == pytest.approx(3.8760)
+        assert ws.dataX(i)[0] == pytest.approx(3.875969)  # Shuo asked for 3.8760
 
 
 if __name__ == '__main__':

ddd62c69e290fcbb756f4df3e2dcee7051136e96
Jose Borreguero <borreguero@gmail.com>
2019-08-29 18:34:09 -0400

    Refs #175 pseudo code written
    Former-commit-id: f5fb4457f72edd21607c4abab6a3e0c37acb75d1

--- tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
+++ tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
@@ -9,7 +9,8 @@ from ornl.settings import (amend_config, namedtuplefy,
 from ornl.sans.sns.eqsans.geometry import insert_aperture_logs
 from ornl.sans.sns.eqsans.api import prepare_data
 from ornl.sans.sns.eqsans import (calculate_transmission,
-                                  apply_transmission_correction)
+                                  apply_transmission_correction,
+                                  find_beam_center)
 
 
 @pytest.fixture(scope='module')
@@ -53,6 +54,19 @@ def test_calculate_raw_transmission(trans_fix):
                                  trans_fix.reference_skip, radius=50,
                                  fit_func=None)
     assert trans_fix.compare(raw, 'raw_transmission_skip.nxs')
+
+
+@pytest.mark.skip(reason="pseudo-code")
+@pytest.mark.parametrize('workspace_with_instrument', [dict(Nx=10, Ny=10, dx=1e-3, dy=1e-3)], indirect=True)
+def test_calculate_transmission_single_bin(workspace_with_instrument):
+    reference_counts = None   # load from file in database
+    reference_workspace = workspace_with_instrument(axis_values=[2.0, 4.0], intensities=reference_counts)
+    beam_center = find_beam_center(reference_workspace)
+    assert beam_center == pytest.approx([5.5761, 5.6547], abs=1e-4)
+    sample_counts = None
+    sample_workspace = workspace_with_instrument(axis_values=[2.0, 4.0], intensities=sample_counts)
+    calculate_transmission(sample_workspace, reference_workspace, radius=2.0, fit_func=None)
+    assert calculate_transmission.readY(0)[0] == pytest.approx(0.7888, abs=1e-04)
 
 
 def test_calculate_fitted_transmission(trans_fix):

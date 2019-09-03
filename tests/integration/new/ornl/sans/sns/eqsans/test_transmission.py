25c50992ed61a0cb9a5ef2e371e500fe97332a5e
Jose Borreguero <borreguero@gmail.com>
2019-09-03 14:33:04 -0400

    Refs #175 implement passing test
    Former-commit-id: aa82b804b67a04bfb55e34e3417afc724ec11ad0

--- tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
+++ tests/integration/new/ornl/sans/sns/eqsans/test_transmission.py
@@ -1,6 +1,9 @@
 from __future__ import (absolute_import, division, print_function)
 
 import pytest
+import json
+import numpy as np
+from pathlib import Path
 from os.path import join as pjn
 from mantid.simpleapi import LoadNexus, CompareWorkspaces, SumSpectra
 
@@ -8,9 +11,8 @@ from ornl.settings import (amend_config, namedtuplefy,
                            unique_workspace_dundername as uwd)
 from ornl.sans.sns.eqsans.geometry import insert_aperture_logs
 from ornl.sans.sns.eqsans.api import prepare_data
-from ornl.sans.sns.eqsans import (calculate_transmission,
-                                  apply_transmission_correction,
-                                  find_beam_center)
+from ornl.sans.sns.eqsans import (calculate_transmission, apply_transmission_correction, find_beam_center,
+                                  center_detector)
 
 
 @pytest.fixture(scope='module')
@@ -56,17 +58,38 @@ def test_calculate_raw_transmission(trans_fix):
     assert trans_fix.compare(raw, 'raw_transmission_skip.nxs')
 
 
-@pytest.mark.skip(reason="pseudo-code")
-@pytest.mark.parametrize('workspace_with_instrument', [dict(Nx=10, Ny=10, dx=1e-3, dy=1e-3)], indirect=True)
-def test_calculate_transmission_single_bin(workspace_with_instrument):
-    reference_counts = None   # load from file in database
-    reference_workspace = workspace_with_instrument(axis_values=[2.0, 4.0], intensities=reference_counts)
+@pytest.mark.parametrize('workspace_with_instrument',
+                         [dict(name='EQSANS', Nx=10, Ny=10, dx=1e-3, dy=1e-3, zc=0.01)], indirect=True)
+def test_calculate_transmission_single_bin(reference_dir, workspace_with_instrument):
+    r"""
+    This implements Issue #175, addressing master document, section 7.3
+    dev - Jose Borreguero <borreguerojm@ornl.gov>
+    SME - Changwoo Do <doc1@ornl.gov>
+    - List of Mantid algorithms employed:
+        FindCenterOfMassPosition <https://docs.mantidproject.org/nightly/algorithms/FindCenterOfMassPosition-v2.html>
+    """
+    # Gather reference and sample data, then check
+    wavelength_bin = [2.0, 5.0]  # some arbitrary wavelength bin
+    with open(Path(reference_dir.new.eqsans) / 'test_transmission' / 'transmission_single_bin.json') as json_file:
+        data = json.load(json_file)
+    reference_counts = np.array(data['reference_data_block'], dtype=float).reshape((10, 10, 1))
+    reference_workspace = workspace_with_instrument(axis_values=wavelength_bin, intensities=reference_counts,
+                                                    view='array')
+    assert reference_workspace.readY(42)[0] == 9
+    sample_counts = np.array(data['sample_data_block'], dtype=float).reshape((10, 10, 1))
+    sample_workspace = workspace_with_instrument(axis_values=wavelength_bin, intensities=sample_counts, view='array')
+    assert sample_workspace.readY(42)[0] == 8
+
+    # Find the beam center and then center reference and sample
     beam_center = find_beam_center(reference_workspace)
-    assert beam_center == pytest.approx([5.5761, 5.6547], abs=1e-4)
-    sample_counts = None
-    sample_workspace = workspace_with_instrument(axis_values=[2.0, 4.0], intensities=sample_counts)
-    calculate_transmission(sample_workspace, reference_workspace, radius=2.0, fit_func=None)
-    assert calculate_transmission.readY(0)[0] == pytest.approx(0.7888, abs=1e-04)
+    assert beam_center == pytest.approx([-0.00007, 0.00015], abs=1e-5)
+    center_detector(reference_workspace, x=-beam_center[0], y=-beam_center[1], unit='m')
+    center_detector(sample_workspace, x=-beam_center[0], y=-beam_center[1], unit='m')
+
+    # Calculate transmission, then check
+    transmission = calculate_transmission(sample_workspace, reference_workspace,
+                                          radius=2.0, radius_unit='mm', fit_func=None)
+    assert transmission.readY(0)[0] == pytest.approx(0.7888, abs=1e-04)
 
 
 def test_calculate_fitted_transmission(trans_fix):

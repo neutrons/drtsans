5c1d127885c382977b58ff4a17a1f1d18b50a4ae
Jiao Lin <linjiao@ornl.gov>
2019-09-06 08:15:46 -0400

    Refs #168. makes flake happy
    Former-commit-id: 324c116d66e673b9d21bc2d6ceda8bdd9e7cdf14

--- tests/unit/new/ornl/sans/sns/eqsans/test_resolution.py
+++ tests/unit/new/ornl/sans/sns/eqsans/test_resolution.py
@@ -66,10 +66,10 @@ class EQSANSResolution(unittest.TestCase):
     def test_moderator_time_error(self):
         from ornl.sans.sns.eqsans.resolution import _moderator_time_error
         out = _moderator_time_error(np.array([1.5]))
-        self.assertTrue(np.isclose( out[0], 214.74671875 ))
+        self.assertTrue(np.isclose(out[0], 214.74671875))
         out = _moderator_time_error(np.array([3.0]))
         wl = 3.
-        self.assertTrue(np.isclose( out[0], 0.0148*wl**3-0.5233*wl**2+6.4797*wl+231.99 ))
+        self.assertTrue(np.isclose(out[0], 0.0148*wl**3-0.5233*wl**2+6.4797*wl+231.99))
         return
 
 

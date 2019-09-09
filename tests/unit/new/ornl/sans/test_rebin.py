1eb78eb4c4bedac8e5af553d07c2e14e4e0a1459
Jiao Lin <linjiao@ornl.gov>
2019-09-09 07:55:20 -0400

    Refs #208. created fixture for making workspace with given events
    Former-commit-id: 9309398ac13fd3a6f434ecefbc10b9a4762ab8c4

---
+++ tests/unit/new/ornl/sans/test_rebin.py
@@ -0,0 +1,36 @@
+from __future__ import (absolute_import, division, print_function)
+
+import pytest
+
+@pytest.fixture
+def fake_events():
+    def _(wavelengths):
+        from mantid.simpleapi import CreateSampleWorkspace
+        import mantid.kernel
+        ws=CreateSampleWorkspace(
+            WorkspaceType='Event',
+            Function='Flat background',
+            NumBanks=1,
+            BankPixelWidth=1,
+            NumEvents=0,
+            XUnit='Wavelength',
+            XMax=10,
+            BinWidth=1)
+        sp0=ws.getSpectrum(0)
+        date=mantid.kernel.DateAndTime("2019-09-09T00:00")
+        for lam in wavelengths:
+            sp0.addEventQuickly(lam,date)
+        return ws
+    return _
+
+
+
+def test_linear(fake_events):
+    ws = fake_events([1., 2.])
+    from mantid.simpleapi import Rebin
+    ws=Rebin(InputWorkspace=ws, Params='.1')
+    return
+
+
+if __name__ == '__main__':
+    pytest.main()

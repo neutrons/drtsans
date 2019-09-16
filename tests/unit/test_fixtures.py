0b51a0ee55b2d21c467e16f4517a39a62164642f
Jose Borreguero <borreguero@gmail.com>
2019-09-16 11:54:33 -0400

    Refs #175 Fix unit test for instrument generation
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 94fe2b1580549fe637012140cb7dc1cf08b7dc39

--- tests/unit/test_fixtures.py
+++ tests/unit/test_fixtures.py
@@ -63,7 +63,7 @@ def test_generate_IDF(generic_IDF):
     <component type="panel" idstart="0" idfillbyfirst="y" idstepbyrow="4">
         <location x="0.0" y="0.0" z="5.0"
             name="detector1"
-            rot="0.0" axis-x="0" axis-y="1" axis-z="0">
+            rot="180.0" axis-x="0" axis-y="1" axis-z="0">
         </location>
     </component>
 
@@ -133,7 +133,7 @@ def test_generate_IDF_defaults(generic_IDF):
     <component type="panel" idstart="0" idfillbyfirst="y" idstepbyrow="3">
         <location x="0.0" y="0.0" z="5.0"
             name="detector1"
-            rot="0.0" axis-x="0" axis-y="1" axis-z="0">
+            rot="180.0" axis-x="0" axis-y="1" axis-z="0">
         </location>
     </component>
 
@@ -216,7 +216,7 @@ def test_generate_workspace_monochromatic(generic_workspace):
     assert ws.readY(1) == 4.
     assert ws.readY(3) == 16.
     specInfo = ws.spectrumInfo()
-    assert specInfo.position(0) == V3D(-1., -.5, 5.)  # row=0, col=0
+    assert specInfo.position(0) == V3D(1., -.5, 5.)  # row=0, col=0
     assert specInfo.position(3) == V3D(0., .5, 5.)    # row=1, col=0
 
 
@@ -267,7 +267,7 @@ class TestWorkspaceWithInstrument(object):
             assert ws.readE(i).tolist() == [1.]  # SANS default
 
         x, y = np.arange(9).reshape((3, 3)), np.abs(np.random.random((3, 3)))
-        ws2 = workspace_with_instrument(axis_values=x, intensities=y)
+        ws2 = workspace_with_instrument(axis_values=x, intensities=y, view='pixel')
         assert ws != ws2
         x, y = x.flatten(), y.flatten()
         for i in range(ws.getNumberHistograms()):
@@ -301,7 +301,7 @@ class TestWorkspaceWithInstrument(object):
         assert ws2.readY(1) == 4.
         assert ws2.readY(3) == 16.
         spectum_info = ws.spectrumInfo()
-        assert spectum_info.position(0) == V3D(-1., -.5, 5.)  # row=0, col=0
+        assert spectum_info.position(0) == V3D(1., -.5, 5.)  # row=0, col=0
         assert spectum_info.position(3) == V3D(0., .5, 5.)  # row=1, col=0
 
     @pytest.mark.parametrize('workspace_with_instrument', [{'Nx': 3, 'Ny': 2}], indirect=True)
@@ -323,7 +323,7 @@ class TestWorkspaceWithInstrument(object):
         assert ws.readY(1).tolist() == [4., 4.]
         assert ws.readY(3).tolist() == [16., 16.]
         spectrum_info = ws.spectrumInfo()
-        assert spectrum_info.position(0) == V3D(-1., -.5, 5.)  # row=0, col=0
+        assert spectrum_info.position(0) == V3D(1., -.5, 5.)  # row=0, col=0
         assert spectrum_info.position(3) == V3D(0., .5, 5.)  # row=1, col=0
 
     @pytest.mark.parametrize('workspace_with_instrument', [{'Nx': 3, 'Ny': 4}], indirect=True)

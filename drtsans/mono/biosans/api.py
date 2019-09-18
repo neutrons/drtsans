a876784873a33a81ad1eaa372297046312ca7f44
Steven Hahn <hahnse@ornl.gov>
2019-09-18 18:00:11 -0400

    Refs #195. Add normalization to api
    Former-commit-id: bff49db92b48faa63d9ea6ef6e7e56ae962592bf

--- drtsans/mono/biosans/api.py
+++ drtsans/mono/biosans/api.py
@@ -1,19 +1,6 @@
-from drtsans import solid_angle_correction
 from drtsans.mono.normalisation import (time, monitor)
-from drtsans.process_uncertainties import set_init_uncertainties  # noqa: F401
-
 # Functions exposed to the general user (public) API
 __all__ = ['apply_solid_angle_correction', 'normalize']
-
-
-def apply_solid_angle_correction(ws):
-    """
-        Apply solid angle correction
-    """
-    # correct implementation requires BOTH wing and tube detectors
-    # see https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/issues/107
-    raise NotImplementedError()
-    return solid_angle_correction(ws, detector_type='VerticalTube')
 
 
 def normalize(ws, normalization_type):

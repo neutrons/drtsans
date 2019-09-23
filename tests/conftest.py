265c29be715cf56015277184e755e8a209aeeea7
Steven Hahn <hahnse@ornl.gov>
2019-09-23 11:58:05 -0400

    Refs #195. Remove print statements
    Former-commit-id: a0271786e565be6cd54ad4d49f7098c19f507b01

--- tests/conftest.py
+++ tests/conftest.py
@@ -609,7 +609,6 @@ def generic_workspace(generic_IDF, request):
         x = np.array(x).ravel()
     else:
         x = np.zeros(Nx * Ny, dtype=float)
-    # print(x.shape, y.shape, e.shape)
     wksp = CreateWorkspace(DataX=x,
                            DataY=y,
                            DataE=e,

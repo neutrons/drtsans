Corrected the Qx (not Qx) and Qy (not Qy) calculation and test.
Former-commit-id: b8c541dfde79d26f2f1bb631145ded13f07191d5

drtsans/momentum_transfer.py
@@ -105,14 +105,13 @@ class InstrumentSetupParameters(object):
 
 
 def calculate_momentum_transfer(ws):
-    """Calculate momentum transfer in vector by operating on source, sample and detector positions,
-    i.e., vec{q} = 2 pi (vec{k_out} - vec{k_in}) / lambda
+    """Calculate momentum transfer including scaler momentum transfer Q, azimuthal projection (Qx) and Qy
 
     Note:
     - N: number of spectra
     - M: number of wave length bins
-    - Qx: Q component at X-Z plane
-    - Qy: Q component at Y plane
+    - Qx: Azimuthal projection, as Q's projection to detector plane and then to X-axis (Q cos(phi))
+    - Qy: Orthogonal to azimuthal projection, i.e., Q sin (phi)
 
     Parameters
     ----------
@@ -120,8 +119,9 @@ def calculate_momentum_transfer(ws):
         Wavelength workspace to calculate momentum transfer from
     Returns
     -------
-    ndarray, ndarray, ndarray, ndarray
-        N x M matrix for Q, N x M matrix for Qx, N x M matrix for Qy, N array for 2theta,
+    ndarray, ndarray, ndarray, ndarray, ndarray
+        N x M matrix for Q, N x M matrix for Qx, N x M matrix for Qy, (N, 1) array for 2theta,
+        (N, 1) for sample pixel distance
 
     """
     # Check inputs and unit
@@ -140,43 +140,34 @@ def calculate_momentum_transfer(ws):
     spec_info = ws.spectrumInfo()
     num_spec = spec_info.size()
     two_theta_array = np.zeros((spec_info.size(), 1))  # 2-theta array is 2D (N x 1)
+    phi_array = np.zeros((spec_info.size(), 1))  # phi array is 2D (N x 1)
+    sample_pixel_distance_array = np.zeros((spec_info.size(), 1))  # s2p array is 2D (N x 1)
 
-    # sample and moderator information: get K_i
-    sample_pos = ws.getInstrument().getSample().getPos()
-    source_pos = ws.getInstrument().getSource().getPos()
-    k_in = sample_pos - source_pos
-    k_in /= linalg.norm(k_in)
-
-    unit_q_vector = np.zeros(shape=(num_spec, 3), dtype='float')
-
-    # TODO - k_out can be calculated in the vector operation by recording det_i_pos, the l2 is almost "free"
-    for iws in range(num_spec):
-        if spec_info.hasDetectors(iws) and not spec_info.isMonitor(iws):
-            # calculate Qx, Qy, Qz
-            det_i_pos = ws.getDetector(iws).getPos()
-            k_out = det_i_pos - sample_pos
-            k_out /= linalg.norm(k_out)
-            unit_q_vector[iws] = k_out - k_in
-
-            # 2theta
-            two_theta_array[iws][0] = spec_info.twoTheta(iws)
-        # otherwise, unit_q_vector[iws] is zero
+    # Get instrument geometry information
+    for i in range(spec_info.size()):
+        if spec_info.hasDetectors(i) and not spec_info.isMonitor(i):
+            sample_pixel_distance_array[i] = spec_info.l2(i)
+            two_theta_array[i] = spec_info.twoTheta(i)
+            phi_array[i] = spec_info.azimuthal(i)
+        else:
+            two_theta_array[i] = np.nan
     # END-FOR
 
-    # Calculate Q from unit Q vector and 2pi/lambda
-    qx_matrix = 2.*np.pi*unit_q_vector[:, 0].reshape((num_spec, 1)) / wavelength_bin_center_matrix
-    qy_matrix = 2.*np.pi*unit_q_vector[:, 1].reshape((num_spec, 1)) / wavelength_bin_center_matrix
-    qz_matrix = 2.*np.pi*unit_q_vector[:, 2].reshape((num_spec, 1)) / wavelength_bin_center_matrix
+    # Calculate momentum transfer
+    # Get mask
+    mask = np.isnan(two_theta_array)
+    q_array = 4.0 * np.pi * np.sin(0.5 * two_theta_array) / wavelength_bin_center_matrix
 
-    q_matrix = np.sqrt(qx_matrix**2 + qy_matrix**2 + qz_matrix**2)
-    q_theta_matrix = np.sqrt(qx_matrix**2 + qz_matrix**2)
+    q_array[mask] = 0.
 
-    # print('[DEBUG] Q  matrix: shape={}\n{}'.format(q_matrix.shape, q_matrix))
-    # print('[DEBUG] Qx matrix: shape={}\n{}'.format(qx_matrix.shape, qx_matrix))
-    # print('[DEBUG] Qy matrix: shape={}\n{}'.format(qy_matrix.shape, qy_matrix))
-    # print('[DEBUG] Qz matrix: shape={}\n{}'.format(qz_matrix.shape, qz_matrix))
+    # Calculate Qx and Qy
+    qx_array = np.cos(phi_array) * q_array
+    qy_array = np.sin(phi_array) * q_array
 
-    return q_matrix, q_theta_matrix, qy_matrix, two_theta_array
+    # Clean memory
+    del mask, phi_array
+
+    return q_array, qx_array, qy_array, two_theta_array, sample_pixel_distance_array
 
 
 def dq2_geometry(L1, L2, R1, R2, wl, theta, pixel_size=0.007):

tests/unit/new/drtsans/test_momentum_transfer.py
@@ -41,13 +41,13 @@ def test_calculate_momentum_transfer(generic_IDF):
         ws.dataE(i)[0] = init_delta_intensity[i]
 
     # TODO FIXME - Need to review because Qx and Qy are redefined
-    q, qx, qy, qz = calculate_momentum_transfer(ws)
+    q, qx, qy, two_theta, s2p = calculate_momentum_transfer(ws)
     verify_q(ws, q, qx, qy)
 
     # Test for multiple bins (wave lengths)
     ws = Rebin(InputWorkspace=ws, Params='2.5, 0.5, 4.5')
 
-    q, qx, qy, qz = calculate_momentum_transfer(ws)
+    q, qx, qy, qz, s2p = calculate_momentum_transfer(ws)
     verify_q(ws, q, qx, qy)
 
     return
@@ -63,49 +63,15 @@ def verify_q(ws, q_matrix, qx_matrix, qy_matrix):
     :param qy_matrix: N x M array (N is number of histogram, M is number of Q values)
     :return:
     """
-    # Obtain wave length
-    wavelength_bin_boundary_matrix = ws.extractX()
-    wavelength_bin_center_matrix = (wavelength_bin_boundary_matrix[:, 1:] +
-                                    wavelength_bin_boundary_matrix[:, :-1]) / 2.0
+    # Check dimension
+    num_spec = ws.getNumberHistograms()
+    num_bins = ws.readY(0)[0]
 
-    # Get instrument information
-    spec_info = ws.spectrumInfo()
-    num_spec = spec_info.size()
+    assert q_matrix.shape == (num_spec, num_bins)
+    assert qx_matrix.shape == (num_spec, num_bins)
+    assert qy_matrix.shape == (num_spec, num_bins)
 
-    # Arrays
-    ratio_x_vec = np.ndarray(shape=(num_spec, 1), dtype='float')
-    ratio_y_vec = np.ndarray(shape=(num_spec, 1), dtype='float')
-    ratio_z_vec = np.ndarray(shape=(num_spec, 1), dtype='float')
-    pixel_2theta_vec = np.ndarray(shape=(num_spec, 1), dtype='float')
-
-    # sample and moderator information: get K_i
-    sample_pos = ws.getInstrument().getSample().getPos()
-    source_pos = ws.getInstrument().getSource().getPos()
-    k_in = sample_pos - source_pos
-    k_in /= linalg.norm(k_in)
-
-    for iws in range(num_spec):
-        # get 2theta
-        pixel_2theta_vec[iws, 0] = spec_info.twoTheta(iws)  # unit = radians
-
-        # calculate Qx, Qy, Qz
-        det_i_pos = ws.getDetector(iws).getPos()
-        k_out = det_i_pos - sample_pos
-        k_out /= linalg.norm(k_out)
-
-        unit_q = k_out - k_in
-        ratio_x_vec[iws] = unit_q[0] / linalg.norm(unit_q)
-        ratio_y_vec[iws] = unit_q[1] / linalg.norm(unit_q)
-        ratio_z_vec[iws] = unit_q[2] / linalg.norm(unit_q)
-    # END
-
-    # Calculate Q = 4 pi sin(theta)/lambda
-    prove_q_matrix = 4.0 * np.pi * np.sin(0.5 * pixel_2theta_vec) / wavelength_bin_center_matrix
-    # Note that Qx (used by SANS) is actually Q's component in X-Z plane
-    q_theta_matrix = np.sqrt((q_matrix * ratio_x_vec)**2 + (q_matrix * ratio_z_vec)**2)
-
-    assert np.allclose(q_matrix, prove_q_matrix, rtol=1.E-10)
-    assert np.allclose(q_theta_matrix, qx_matrix, rtol=1.E-10)
-    assert np.allclose(q_matrix * ratio_y_vec, qy_matrix, rtol=1.E-10)
+    # Test value
+    assert np.allclose(q_matrix**2, qx_matrix**2 + qy_matrix**2, rtol=1.E-10)
 
     return

tests/unit/new/drtsans/tof/eqsans/test_momentum_transfer.py
@@ -1,10 +1,14 @@
 import numpy as np
+from drtsans.momentum_transfer import dq2_geometry, dq2_gravity
+from drtsans import geometry as sans_geometry
+from drtsans.tof.eqsans.geometry import source_aperture_diameter, sample_aperture_diameter, source_sample_distance
 import pytest
 # https://docs.mantidproject.org/nightly/algorithms/LoadEmptyInstrument-v1.html
 from mantid.simpleapi import LoadEmptyInstrument, AddTimeSeriesLog, Rebin, ConvertUnits
 from drtsans.tof.eqsans.momentum_transfer import calculate_q_dq, calculate_pixel_positions,\
     retrieve_instrument_setup
-from drtsans.tof.eqsans.momentum_transfer import calculate_q_resolution, InstrumentSetupParameters
+from drtsans.tof.eqsans.momentum_transfer import calculate_q_resolution, InstrumentSetupParameters,\
+    moderator_time_uncertainty
 from drtsans.tof.eqsans import load_events
 
 
@@ -368,3 +372,85 @@ def q_resolution_per_pixel_to_mod(ws):
     dqy = np.sqrt(_dqy2(qy, L1, L2, R1, R2, wl, dwl, theta, s2p, dtof=dtof))
     return qx, qy, dqx, dqy
 
+
+def _dqx2(qx, L1, L2, R1, R2, wl, dwl, theta, s2p, pixel_size=0.0055, dtof=0.):
+    """
+    Q resolution in the horizontal direction.
+
+    Parameters
+    ----------
+    qx: float
+        value of q_x (1/Angstrom)
+    L1: float
+        source-to-sample distance (m)
+    L2: float
+        sample-to-detector distance (m)
+    R1: float
+        source aperture radius (m)
+    R2: float
+        sample aperture radius (m)
+    wl: float
+        wavelength mid-point (Angstrom)
+    dwl: float
+        wavelength-spread (Angstrom)
+    theta: float
+        scattering angle (rad)
+    s2p: float
+        sample-to-pixel (m)
+    pixel_size: float
+        dimension of the pixel (m)
+
+    Returns
+    ------
+    float
+    """
+    # If theta is not supplied, compute it from qx
+    # This simplifies the calculation for I(Q) in 1D.
+    if theta is None:
+        theta = 2.0 * np.arcsin(wl * np.fabs(qx) / 4.0 / np.pi)
+    dq2_geo = dq2_geometry(L1, L2, R1, R2, wl, theta, pixel_size)
+    dq_tof_term = (3.9560 * dtof / 1000.0 / wl / (L1 + s2p))**2
+    return dq2_geo + qx**2 * (dq_tof_term + (dwl / wl)**2) / 12.0
+
+
+def _dqy2(qy, L1, L2, R1, R2, wl, dwl, theta, s2p, pixel_size=0.0043, dtof=0.):
+    """
+    Q resolution in vertical direction.
+
+    Parameters
+    ----------
+    qy: float
+        value of q_y (1/Angstrom)
+    L1: float
+        source-to-sample distance (m)
+    L2: float
+        sample-to-detector distance (m)
+    R1: float
+        source aperture radius (m)
+    R2: float
+        sample aperture radius (m)
+    wl: float
+        wavelength mid-point (Angstrom)
+    dwl: float
+        wavelength-spread (Angstrom)
+    theta: float
+        scattering angle (rad)
+    s2p: float
+        sample-to-pixel (m)
+    pixel_size: float
+        dimension of the pixel (m)
+
+    Returns
+    ------
+    float
+    """
+    # If theta is not supplied, compute it from qx
+    # This simplifies the calculation for I(Q) in 1D.
+    if theta is None:
+        theta = 2.0 * np.arcsin(wl * np.fabs(qy) / 4.0 / np.pi)
+    dq2_geo = dq2_geometry(L1, L2, R1, R2, wl, theta, pixel_size)
+    dq_tof_term = (3.9560 * dtof / 1000.0 / wl / (L1 + s2p))**2
+    dq2_grav = dq2_gravity(L1, L2, wl, dwl, theta)
+    dq2 = dq2_geo + dq2_grav
+    dq2 += qy**2 * (dq_tof_term + (dwl / wl)**2) / 12.0
+    return dq2
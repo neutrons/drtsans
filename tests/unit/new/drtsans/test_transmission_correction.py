7cf1566038017849467aeccd83c00e4e42743bd4
Joe Osborn <osbornjd@ornl.gov>
2019-09-24 13:53:29 -0400

    fixed up test, final assertion fails
    Former-commit-id: 544d1cb23afd0b32000270cf71609565cdc4d88b

--- tests/unit/new/drtsans/test_transmission_correction.py
+++ tests/unit/new/drtsans/test_transmission_correction.py
@@ -1,24 +1,33 @@
 from drtsans.transmission import apply_transmission_correction
 import numpy as np
-import matplotlib.pyplot as plt
 import pytest
 
-I_sam = np.array([[ 60,  65,  70,  75,  80],
-                  [ 75,  80,  85,  90,  95],
-                  [ 90,  95, 100, 105, 110],
+# Make an example array of intensity counts
+I_sam = np.array( [[60,  65,  70,  75,  80],
+                   [75,  80,  85,  90,  95],
+                   [90,  95, 100, 105, 110],
                   [105, 110, 115, 120, 125],
-                  [120, 125, 130, 135, 140]])
+                  [120, 125, 130, 135, 140]], dtype=np.float)
 
+# Poisson uncertainties on counts
+I_sam_err = np.sqrt(I_sam)
+
+# Make an example array of transmission coefficients
 transmission = np.array( [[0.6, 0.7, 0.7, 0.7, 0.6],
                           [0.7, 0.7, 0.8, 0.7, 0.7],
                           [0.7, 0.8, 0.9, 0.8, 0.7],
                           [0.7, 0.7, 0.8, 0.7, 0.7],
-                          [0.6, 0.7, 0.7, 0.7, 0.6]] )
+                          [0.6, 0.7, 0.7, 0.7, 0.6]]) #the first entry in this list for some reason is taken as the constant transmission value
 
 # Give the transmission coefficients 5% uncertainties
 transmission_err = np.array(transmission * 0.05)
     
 pixel_size = 0.005 # meters
+
+# Make a mantid workspace for the transmission coefficients
+@pytest.mark.parametrize('workspace_with_instrument',
+                         [dict(name='EQSANS', Nx=10, Ny=10, dx=1e-3, dy=1e-3, zc=0.01)],
+                         indirect = True)
 
 # Make a mantid workspace for the intensities
 @pytest.mark.parametrize('generic_workspace',
@@ -26,46 +35,47 @@ pixel_size = 0.005 # meters
                            'dx': pixel_size, 'dy': pixel_size,
                            'yc': pixel_size / 2.,
                            'axis_values': [5.95,6.075],
-                            'intensities': I_sam}],
+                           'intensities': I_sam}],
                          indirect = True)
-
-# Make a mantid workspace for the transmission coefficients
-@pytest.mark.parametrize('generic_workspace',
-                          [{'name': 'transmission',
-                            'intensities' : transmission,
-                            'uncertainties' : transmission_err
-                            }],
-                          indirect=True)
-
-# I can get a single workspace through to the function, but when I try to use
-# two workspaces I run into issues
-def test_transmission_correction(generic_workspace, transmission_workspace):
+def test_transmission_correction(generic_workspace, workspace_with_instrument):
     '''
     Test the calculation of the detector transmission correction and error propagation
     given in Eq. 7.7 and 7.8 in the master document
     dev - Joe Osborn <osbornjd@ornl.gov>
     '''
-    
+
+    # Create the workspaces
     I_sam_wksp = generic_workspace
-    transmission_wksp = transmission_workspace;
-    
-    # Calculate the result with drtsans framework
-    result = apply_transmission_correction(I_sam_wksp, transmission_wksp, 1, 0.05)
+    transmission_wksp = workspace_with_instrument( axis_values = [5.95,6.075],
+                                                   intensities = transmission,
+                                                   uncertainties = transmission_err)
+
+    # Check that they were created successfully
+    assert I_sam_wksp.extractY().sum() ==  pytest.approx(np.sum(I_sam))
+    assert transmission_wksp.extractY().sum() == pytest.approx(np.sum(transmission))
+    assert I_sam_wksp.extractE().sum() == pytest.approx(np.sum(I_sam_err))
+    assert transmission_wksp.extractE().sum() == pytest.approx(np.sum(transmission_err))
 
     # Calculate the result by hand to compare to drtsans
-    # Poisson uncertainties on counts
-    I_sam_err = np.sqrt(I_sam)
-
     # Calculate transmission corrected intensity
     I_tsam = I_sam / transmission
 
     # Propagate uncertainties to transmission corrected intensity
-    I_tsam_err = I_sam / transmission * np.sqrt( (I_sam_err/I_sam)**2 + (transmission_err/transmission)**2 )
+    I_tsam_err = I_tsam * np.sqrt( (I_sam_err/I_sam)**2 + (transmission_err/transmission)**2 )
 
+    # Calculate the result with drtsans framework
+    result = apply_transmission_correction(I_sam_wksp, transmission_wksp)
+    
     # Compare the result from drtsans and the result calcualted by hand
-    assert result.extractY()[0][0] == I_tsam[0][0]
-    assert result.extractE()[0][0] == I_tsam_err[0][0]
- 
+
+    #print the drtsans result
+    print(result.extractY())
+
+    #print the by-hand calculation
+    print(I_tsam)
+
+    np.testing.assert_equal(result.extractY(), I_tsam)
+    
 if __name__ == '__main__':
     pytest.main()
-  
+    

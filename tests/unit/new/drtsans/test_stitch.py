75bb7066c2d1bbcb15910e7ce8fd3321ab3f2e0c
Jiao Lin <linjiao@ornl.gov>
2019-09-27 12:08:26 -0400

    Refs #218. don't plot when running headless
    Former-commit-id: f28710a52cb28eb4a0a829a8c84abf4c1ee70b08

--- tests/unit/new/drtsans/test_stitch.py
+++ tests/unit/new/drtsans/test_stitch.py
@@ -3,6 +3,8 @@ import pytest
 from mantid.simpleapi import Load, CreateWorkspace, WorkspaceFactory, Stitch1D
 from drtsans.stitch import stitch
 
+headless = 'DISPLAY' not in os.environ or not os.environ['DISPLAY']
+
 
 def test_stitch(reference_dir):
     # function to load test data
@@ -18,16 +20,17 @@ def test_stitch(reference_dir):
     startoverlap, stopoverlap = 0.04, 0.08
     # stitch
     xout, yout, erryout, errxout, scale = stitch(x1,y1,erry1,errx1, x2,y2,erry2,errx2, startoverlap, stopoverlap)
-    # plot
-    from matplotlib import pyplot as plt
-    fig, ax = plt.subplots()
-    expected_x,expected_y,expected_err,expected_errx = loadData("sMCM_cc_stitched.txt")
-    ax.plot(expected_x, expected_y, '+', label='expected')
-    ax.plot(x1, y1, 'v', label='low q')
-    ax.plot(x2, y2*scale, 'o', label='high q scaled')
-    ax.plot(xout, yout, label='stitched')
-    ax.legend()
-    plt.savefig("stitch.pdf")
+    if not headless:
+        # plot
+        from matplotlib import pyplot as plt
+        fig, ax = plt.subplots()
+        expected_x,expected_y,expected_err,expected_errx = loadData("sMCM_cc_stitched.txt")
+        ax.plot(expected_x, expected_y, '+', label='expected')
+        ax.plot(x1, y1, 'v', label='low q')
+        ax.plot(x2, y2*scale, 'o', label='high q scaled')
+        ax.plot(xout, yout, label='stitched')
+        ax.legend()
+        plt.savefig("stitch.pdf")
     # check
     assert np.allclose(xout, expected_x)
     assert np.allclose(yout, expected_y, rtol=2e-4)

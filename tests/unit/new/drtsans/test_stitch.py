b4834d35249dd9d30283b68003b26ed4be52218b
Jiao Lin <linjiao@ornl.gov>
2019-09-27 11:03:48 -0400

    Refs #218. added a test code
    Former-commit-id: 3cf3796f499b50327a8ac43dfd5bec4503bc75c1

---
+++ tests/unit/new/drtsans/test_stitch.py
@@ -0,0 +1,56 @@
+import os, numpy as np
+import pytest
+from mantid.simpleapi import (CompareWorkspaces, Load,
+                              LoadEmptyInstrument,
+                              MoveInstrumentComponent)
+from mantid.simpleapi import CreateWorkspace, WorkspaceFactory, Stitch1D
+from drtsans import solid_angle_correction
+
+
+def test_stitch(reference_dir):
+    def loadData(filename):
+        datadir = os.path.join(reference_dir.new.eqsans, "test_stitch")
+        data = os.path.join(datadir, filename)
+        x,y,erry,errx = np.loadtxt(data, skiprows=2).T
+        return x,y,erry,errx
+    def toWS(x,y, newx):
+        newy = np.interp(newx, x, y)
+        nbins = newx.size
+        ws = WorkspaceFactory.create(
+            "Workspace2D", NVectors=1, XLength=nbins, YLength=nbins)
+        ws.setX(0, newx)
+        ws.setY(0, newy)
+        return ws
+    # load data
+    (x1, y1, erry1, errx1) = loadData("sMCM_cc_4m_Iq.txt")
+    (x2, y2, erry2, errx2) = loadData("sMCM_cc_hq_Iq.txt")
+    startoverlap, stopoverlap = 0.04, 0.08
+    newx = np.linspace(startoverlap, stopoverlap, 100)
+    ws1 = toWS(x1, y1, newx)
+    ws2 = toWS(x2, y2, newx)
+    # stitch
+    _, scale = Stitch1D(LHSWorkspace=ws1, RHSWorkspace=ws2, StartOverlap=startoverlap, EndOverlap=stopoverlap)
+    lowqrange = x1<startoverlap
+    highqrange = x2>startoverlap
+    xout = np.concatenate((x1[lowqrange], x2[highqrange]))
+    yout = np.concatenate((y1[lowqrange], scale*y2[highqrange]))
+    # plot
+    from matplotlib import pyplot as plt
+    fig, ax = plt.subplots()
+    expected_x,expected_y,expected_err,expected_errx = loadData("sMCM_cc_stitched.txt")
+    ax.plot(expected_x, expected_y, '+', label='expected')
+    # ax.errorbar(stitched)
+    ax.plot(x1, y1, 'v', label='low q')
+    ax.plot(x2, y2*scale, 'o', label='high q scaled')
+    ax.plot(xout, yout, label='stitched')
+    ax.legend()
+    plt.savefig("stitch.pdf")
+    # check
+    assert np.allclose(xout, expected_x)
+    assert np.allclose(yout, expected_y, rtol=2e-4)
+    return
+
+
+if __name__ == '__main__':
+    pytest.main()
+ 

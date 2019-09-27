031ed7ea73284a9074e097e5fb2fb90f2cf20ae6
Cooper, Caleb <coopercd@ornl.gov>
2019-09-27 13:55:47 -0400

    Update .gitlab-ci.yml, scripts/analysis-update.sh, scripts/analysis-update-user.sh files
    Former-commit-id: 640fe6122efaa96edfa4424861b410d4161d198d

---
+++ scripts/analysis-update-user.sh
@@ -0,0 +1,3 @@
+groupadd -g 52321 sns_ndav_team
+useradd -s /bin/bash -u 13972 -m -g sns_ndav_team lj7
+runuser -l lj7 -c analysis-update.sh
---
+++ scripts/analysis-update.sh
@@ -0,0 +1,4 @@
+export PATH=/SNS/software/miniconda2/bin:$PATH
+source activate sans
+conda install -q -y  -c mantid/label/nightly mantid-framework
+pip install https://$CI_USER:$CI_PASS@code.ornl.gov/sns-hfir-scse/sans/sans-backend.git

7be71bb4cab96179e67ffc0fc37368fc9a2bbc06
Cooper, Caleb <coopercd@ornl.gov>
2019-10-02 10:56:42 -0400

    Update scripts/analysis-update.sh
    Former-commit-id: dd727d7421a757f7312da8974ca58769737bb9fa

--- scripts/analysis-update.sh
+++ scripts/analysis-update.sh
@@ -1,3 +1,7 @@
+ls -la /SNS/software/miniconda2
+ls -la /SNS/software/miniconda2/envs
+ls -la /SNS/software/miniconda2/envs/sans
+
 export PATH=/SNS/software/miniconda2/bin:$PATH
 source activate sans
 conda install -q -y  -c mantid/label/nightly mantid-framework

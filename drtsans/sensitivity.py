70b111dde367ac83fc3688504c3e76766d5dd312
Savici, Andrei T <saviciat@ornl.gov>
2019-10-02 10:28:01 -0400

    Apply sensitivity threshold
    Former-commit-id: f86c059ada06402e30961cee257474ad679bad21

--- drtsans/sensitivity.py
+++ drtsans/sensitivity.py
@@ -328,6 +328,8 @@ def inf_value_to_mask(ws):
 
 def apply_sensitivity_correction(input_workspace, sensitivity_filename=None,
                                  sensitivity_workspace=None,
+                                 min_threshold=None,
+                                 max_threshold=None,
                                  output_workspace=None):
     '''Apply a previously calculated sensitivity correction
 
@@ -337,6 +339,7 @@ def apply_sensitivity_correction(input_workspace, sensitivity_filename=None,
     :ref:`Divide <algm-Divide-v1>`,
     :ref:`LoadNexusProcessed <algm-LoadNexusProcessed-v1>`,
     :ref:`MaskDetectors <algm-MaskDetectors-v1>`
+    :ref:`MaskDetectorsIf <algm-MaskDetectorsIf-v1>`
 
     Parameters
     ----------
@@ -373,8 +376,23 @@ def apply_sensitivity_correction(input_workspace, sensitivity_filename=None,
                        OutputWorkspace=output_workspace)
     MaskDetectors(Workspace=output_workspace,
                   MaskedWorkspace=sensitivity_workspace)
-    Divide(LHSWorkspace=output_workspace, RHSWorkspace=sensitivity_workspace,
+
+    # additional masking dependent on threshold
+    temp_sensitivity = CloneWorkspace(InputWorkspace=sensitivity_workspace,
+                                      OutputWorkspace=unique_workspace_name(prefix="__sensitivity_"))
+    if min_threshold is not None:
+        MaskDetectorsIf(InputWorkspace=temp_sensitivity,
+                        Operator='LessEqual',
+                        Value=min_threshold,
+                        OutputWorkspace=temp_sensitivity)
+    if max_threshold is not None:
+        MaskDetectorsIf(InputWorkspace=temp_sensitivity,
+                        Operator='GreaterEqual',
+                        Value=max_threshold,
+                        OutputWorkspace=temp_sensitivity)
+    Divide(LHSWorkspace=output_workspace, RHSWorkspace=temp_sensitivity,
            OutputWorkspace=output_workspace)
+    DeleteWorkspace(temp_sensitivity)
 
     if cleanupSensitivity:
         DeleteWorkspace(sensitivity_workspace)

3265d9961d3ca5c68cf2f447941af2ffc6c6b593
Jose Borreguero <borreguero@gmail.com>
2019-10-07 17:45:36 -0400

    Refs #174 normalization for mono mode
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: aa2f5d6ef5f7ea7db149a4595f75b27426f45dd9

--- tests/integration/new/drtsans/mono/test_normalization.py
+++ tests/integration/new/drtsans/mono/test_normalization.py
@@ -1,46 +1,83 @@
 import pytest
+import itertools
 import numpy as np
 
+# CreateWorkspace <https://docs.mantidproject.org/nightly/algorithms/CreateWorkspace-v1.html>
+from mantid.simpleapi import CreateWorkspace
+
+# unique_workspace_dundername <>
+# SampleLogs <>
+# time, monitor <>
+from drtsans.settings import unique_workspace_dundername
 from drtsans.samplelogs import SampleLogs
 from drtsans.mono.normalisation import time, monitor
 
 
-# sample data for integration tests
-x, y = np.meshgrid(np.linspace(-1, 1, 5), np.linspace(-1, 1, 5))
-I_sam = (x + 5 * y) * 10 + 100
+@pytest.fixture(scope='module')
+def data_test_16a():
+    return dict(t_sam=5,  # duration of the sample run
+                wavelength_bin=[2.0, 3.0],  # some arbitrary wavelength bin to aid in the creation of a workspace
+                precision=1e-04,  # desired precision for comparisons,
+                n_pixels=25,
+                I_sam=[[40.,  45.,  50.,  55.,  60.],
+                       [65.,  70.,  75.,  80.,  85.],
+                       [90.,  95., 100., 105., 110.],
+                       [115., 120., 125., 130., 135.],
+                       [140., 145., 150., 155., 160.]],
+                I_sam_err=[[6.3246, 6.7082, 7.0711, 7.4162, 7.746],
+                           [8.0623, 8.3666, 8.6603, 8.9443, 9.2195],
+                           [9.4868, 9.7468, 10., 10.247, 10.4881],
+                           [10.7238, 10.9545, 11.1803, 11.4018, 11.619],
+                           [11.8322, 12.0416, 12.2474, 12.4499, 12.6491]],
+                I_samnorm=[[8.,  9., 10., 11., 12.],
+                           [13., 14., 15., 16., 17.],
+                           [18., 19., 20., 21., 22.],
+                           [23., 24., 25., 26., 27.],
+                           [28., 29., 30., 31., 32.]],
+                I_samnorm_err=[[1.26492, 1.34164, 1.41422, 1.48324, 1.5492 ],
+                               [1.61246, 1.67332, 1.73206, 1.78886, 1.8439 ],
+                               [1.89736, 1.94936, 2., 2.0494, 2.09762],
+                               [2.14476, 2.1909, 2.23606, 2.28036, 2.3238 ],
+                               [2.36644, 2.40832, 2.44948, 2.48998, 2.52982]],
+                flux_sam=5e8,  # flux at the monitor
+                )
 
 
-@pytest.mark.parametrize('generic_workspace',
-                         [{'axis_values': x,
-                          'intensities': I_sam}],
-                         indirect=True)
-def test_normalization_by_time(generic_workspace):
-    ws = generic_workspace
-    t_sam = 5  # value from test
-    SampleLogs(ws).insert('timer', t_sam, 'Second')
-    # Calculate following equation 6.1 in master document
-    I_samnorm = I_sam / t_sam
-    # Calculate using API function
+def test_normalization_by_time(data_test_16a):
+    # Create a Mantid workspace with the sample intensities
+    intensities_list = list(itertools.chain(*data_test_16a['I_sam']))
+    errors_list = list(itertools.chain(*data_test_16a['I_sam_err']))
+    ws = CreateWorkspace(DataX=data_test_16a['wavelength_bin'],
+                         DataY=intensities_list, DataE=errors_list,
+                         NSpec=data_test_16a['n_pixels'],
+                         OutputWorkspace=unique_workspace_dundername())
+    # Insert the duration of the run as a metadata item
+    SampleLogs(ws).insert('timer', data_test_16a['t_sam'], 'Second')
+    # Carry out the normalization
     ws_samnorm = time(ws)
-    # Check results from both calculations  match
-    assert np.allclose(ws_samnorm.extractY().ravel(), I_samnorm.ravel())
+    # Compare normalized intensities to those of the test
+    intensities_list = list(itertools.chain(*data_test_16a['I_samnorm']))
+    assert ws_samnorm.extractY() == pytest.approx(intensities_list, abs=data_test_16a['precision'])
+    # Compare normalized errors to those of the test
+    errors_list = list(itertools.chain(*data_test_16a['I_samnorm_err']))
+    assert ws_samnorm.extractE() == pytest.approx(errors_list, abs=data_test_16a['precision'])
+    ws_samnorm.delete()  # some clean up
 
 
-@pytest.mark.parametrize('generic_workspace',
-                         [{'axis_values': x,
-                          'intensities': I_sam}],
-                         indirect=True)
-def test_normalization_by_monitor(generic_workspace):
-    ws = generic_workspace
-    factor_is = 10**8  # value from test
-    flux_sam = 5 * 10**8  # value from test
-    SampleLogs(ws).insert('monitor', flux_sam)
-    # Calculate following equation 6.4 in master document
-    I_samnorm = factor_is / flux_sam * I_sam
-    # Calcuate using API function
+def test_normalization_by_monitor(data_test_16a):
+    # Create a Mantid workspace with the sample intensities
+    intensities_list = list(itertools.chain(*data_test_16a['I_sam']))
+    ws = CreateWorkspace(DataX=data_test_16a['wavelength_bin'],
+                         DataY=intensities_list,
+                         NSpec=data_test_16a['n_pixels'],
+                         OutputWorkspace=unique_workspace_dundername())
+    # Insert the flux at the monitor as a metadata item
+    SampleLogs(ws).insert('monitor', data_test_16a['flux_sam'])
     ws_samnorm = monitor(ws)
-    # Check results match
-    assert np.allclose(ws_samnorm.extractY().ravel(), I_samnorm.ravel())
+    # Compare normalized intensities to those of the test
+    intensities_list = list(itertools.chain(*data_test_16a['I_samnorm']))
+    assert ws_samnorm.extractY() == pytest.approx(intensities_list, abs=data_test_16a['precision'])
+    ws_samnorm.delete()  # some clean up
 
 
 if __name__ == '__main__':

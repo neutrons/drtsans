c4cb617b40e64b8925bb7760191a6f52b77a2b7d
Cooper, Caleb <coopercd@ornl.gov>
2019-10-09 11:09:44 -0400

    Update test_job.sh
    Former-commit-id: 39fc8025811ab03414f63d219fee40b432c24f8f

--- test_job.sh
+++ test_job.sh
@@ -7,4 +7,4 @@ TEST_SCOPE=$1  # 'unit' or 'integration'
 source activate mantid
 cd /opt/sans-backend
 echo "Writing tests results to $(pwd)/${TEST_SCOPE}_test_results.xml"
-pytest -v /opt/sans-backend/tests/${TEST_SCOPE} --junitxml=./${TEST_SCOPE}_test_results.xml
+pytest -v /opt/sans-backend/tests/${TEST_SCOPE} -n 7 --junitxml=./${TEST_SCOPE}_test_results.xml

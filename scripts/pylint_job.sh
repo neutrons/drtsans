e9e577927c26800724016aaf44d495e4717601ad
Pete Peterson <petersonpf@ornl.gov>
2019-10-10 13:28:21 -0400

    Whitelist python modules to load
    Also make sure that code is using the conda version of pylint
    
    
    Former-commit-id: b6caf5bdf56b76d44211c9876c2c81bd10ffe0e9

--- scripts/pylint_job.sh
+++ scripts/pylint_job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 source activate mantid
-pylint --disable=C --disable=no-name-in-module drtsans tests
+conda install pylint
+python -m pylint --disable=C --disable=no-name-in-module drtsans tests --extension-pkg-whitelist=mantid,numpy --ignore drtsans/_version.py

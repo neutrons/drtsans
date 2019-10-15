da2bd0994f7f846d5a17256ebb2f76c8dca81213
Jose Borreguero <borreguero@gmail.com>
2019-10-14 21:01:57 -0400

    Refs #156 reformat mono.dark_current to have same behavior as tof.dark_current
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: a0f1c6558f700ae90ae151b135ec4acb4089c417

--- drtsans/mono/dark_current.py
+++ drtsans/mono/dark_current.py
@@ -1,11 +1,13 @@
-from mantid.simpleapi import Multiply, Subtract, mtd, CreateSingleValuedWorkspace
-from drtsans.settings import (unique_workspace_dundername as uwd)
+from mantid.simpleapi import Multiply, Subtract, mtd, CreateSingleValuedWorkspace, DeleteWorkspace, Scale
+from drtsans.settings import unique_workspace_dundername
+from drtsans.samplelogs import SampleLogs
+
 from drtsans.dark_current import duration
 
 __all__ = ['subtract_dark_current', 'normalise_to_workspace']
 
 
-def normalise_to_workspace(dark_ws, data_ws, output_workspace=None):
+def normalise_to_workspace(dark_workspace, data_workspace, output_workspace=None):
     r"""
     Scale and Rebin in wavelength a `dark` current workspace with information
     from a `data` workspace.
@@ -17,32 +19,35 @@ def normalise_to_workspace(dark_ws, data_ws, output_workspace=None):
 
     Parameters
     ----------
-    dark_ws: str, ~mantid.api.MatrixWorkspace
+    dark_workspace: str, ~mantid.api.MatrixWorkspace
         Dark current workspace with units in time-of-flight
-    data_ws: str, ~mantid.api.MatrixWorkspace
+    data_workspace: str, ~mantid.api.MatrixWorkspace
         Sample scattering with intensities versus wavelength
     output_workspace : str
         Name of the normalised dark workspace. If None, the name of the input
-        workspace `dark_ws` is chosen (and the input workspace is overwritten).
+        workspace `dark_workspace` is chosen (and the input workspace is overwritten).
     Returns
     -------
     MatrixWorkspace
         Output workspace, dark current rebinned to wavelength and rescaled
     """
     if output_workspace is None:
-        output_workspace = str(dark_ws)
-    dark = mtd[str(dark_ws)]
-    data = mtd[str(data_ws)]
-    t_sam = duration(data).value
-    t_data = duration(dark).value
-    time_workspace = CreateSingleValuedWorkspace(t_sam/t_data, OutputWorkspace=uwd())
-    Multiply(LHSWorkspace=time_workspace,
-             RHSWorkspace=dark_ws,
-             OutputWorkspace=output_workspace)
+        output_workspace = str(dark_workspace)
+
+    # Find out the duration of the dark current from the logs, and divide
+    dark_duration = duration(dark_workspace)
+    normalizing_factor = unique_workspace_dundername()  # temporary workspace
+    Scale(InputWorkspace=dark_workspace, Factor=1. / dark_duration.value, Operation='Multiply',
+          OutputWorkspace=output_workspace)
+
+    # Save the name of the log used to calculate the duration
+    SampleLogs(output_workspace).insert('normalizing_duration', dark_duration.log_key)
+
+    DeleteWorkspace(normalizing_factor)
     return mtd[output_workspace]
 
 
-def subtract_dark_current(data_ws, dark_current_ws, output_workspace=None):
+def subtract_dark_current(data_workspace, dark_workspace, output_workspace=None):
     r"""
     Subtract normalized dark from data, taking into account the duration
     of both `data` and `dark` runs.
@@ -51,23 +56,32 @@ def subtract_dark_current(data_ws, dark_current_ws, output_workspace=None):
 
     Parameters
     ----------
-    data_ws: MatrixWorkspace
+    data_workspace: MatrixWorkspace
         Sample scattering with intensities versus wavelength.
         NOT NORMALISED!
-    dark_current_ws: MatrixWorkspace
+    dark_workspace: MatrixWorkspace
         Normalized dark current
     output_workspace : str
         Name of the output workspace. If None, the name of the input
-        workspace `data_ws` is chosen (and the input workspace is overwritten).
+        workspace `data_workspace` is chosen (and the input workspace is overwritten).
     Returns
     -------
     MatrixWorkspace
         `data` minus `dark` current
     """
-    normalise_to_workspace(dark_current_ws, data_ws, output_workspace=str(dark_current_ws))
     if output_workspace is None:
-        output_workspace = str(data_ws)
-    Subtract(LHSWorkspace=data_ws,
-             RHSWorkspace=dark_current_ws,
-             OutputWorkspace=output_workspace)
+        output_workspace = str(data_workspace)
+
+    # Normalize the dark current
+    normalized_dark_current = unique_workspace_dundername()  # temporary workspace
+    normalise_to_workspace(dark_workspace, data_workspace, output_workspace=normalized_dark_current)
+
+    # Find the duration of the data run using the same log key than that of the dark current
+    duration_log_key = SampleLogs(normalized_dark_current).normalizing_duration.value
+    data_duration = duration(data_workspace, log_key=duration_log_key).value
+    Scale(InputWorkspace=normalized_dark_current, Factor=data_duration, Operation='Multiply',
+          OutputWorkspace=normalized_dark_current)
+    Subtract(LHSWorkspace=data_workspace, RHSWorkspace=normalized_dark_current, OutputWorkspace=output_workspace)
+
+    DeleteWorkspace(normalized_dark_current)  # some clean-up
     return mtd[output_workspace]

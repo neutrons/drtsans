ba039af81bc7c0389b80f3fb65550b27e157a7d7
Pete Peterson <petersonpf@ornl.gov>
2019-10-22 16:27:38 -0400

    Remove empty implementation from eqsans
    Former-commit-id: 56439ae322187182fa4fd626231fe630c2381311

--- drtsans/tof/eqsans/__init__.py
+++ drtsans/tof/eqsans/__init__.py
@@ -1,8 +1,8 @@
 import drtsans.transmission
 
+from drtsans.beam_finder import *  # noqa: F403
 from drtsans.transmission import *  # noqa: F403
 from .api import *   # noqa: F403
-from .beam_finder import *  # noqa: F403
 from .cfg import *  # noqa: F403
 from .correct_frame import *  # noqa: F403
 from .dark_current import *  # noqa: F403
@@ -13,6 +13,7 @@ from .mask import *  # noqa: F403
 from .normalisation import *  # noqa: F403
 from .transmission import *  # noqa: F403
 
-__all__ = (drtsans.transmission.__all__ + api.__all__ + beam_finder.__all__ + cfg.__all__ +  # noqa: F405
-           correct_frame.__all__ + dark_current.__all__ + geometry.__all__ + iq.__all__ +  # noqa: F405
-           load.__all__ + mask.__all__ + normalisation.__all__ + transmission.__all__)  # noqa: F405
+__all__ = (drtsans.beam_finder.__all__ + drtsans.transmission.__all__ + api.__all__  # noqa: F405
+           + cfg.__all__ + correct_frame.__all__ + dark_current.__all__ + geometry.__all__  # noqa: F405
+           + iq.__all__ + load.__all__ + mask.__all__ + normalisation.__all__  # noqa: F405
+           + transmission.__all__)  # noqa: F405

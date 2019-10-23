efa4b2916e9d1d9ad9e5a0ad5a9aa615c6d5f650
Cooper, Caleb <coopercd@ornl.gov>
2019-10-23 10:30:10 -0400

    Update scripts/analysis-update-user.sh, scripts/analysis-update.sh files
    Former-commit-id: bc4e3ef1430e80121f86c0419ecf01161e12ae46

--- scripts/analysis-update-user.sh
+++ scripts/analysis-update-user.sh
@@ -1,3 +1,11 @@
-groupadd -g 52321 sns_ndav_team
-useradd -s /bin/bash -u 13972 -m -g sns_ndav_team lj7
-runuser -l lj7 -c "bash /opt/sans-backend/scripts/analysis-update.sh"
+#! /bin/bash
+set -expu
+
+NDAV_USER='jbq'
+NDAV_USER_ID='11226'
+NDAV_GROUP='sns_ndav_team'
+NDAV_GROUP_ID='52321'
+
+groupadd -g "${NDAV_GROUP_ID}" "${NDAV_GROUP}"
+useradd -s /bin/bash -u "${NDAV_USER_ID}" -m -g "${NDAV_GROUP}" "${NDAV_USER}"
+runuser -l "${NDAV_USER}" -c "bash /opt/sans-backend/scripts/analysis-update.sh"

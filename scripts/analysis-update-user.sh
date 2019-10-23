e5721f55763d788f6adcdc845709c8b5815f2a95
Cooper, Caleb <coopercd@ornl.gov>
2019-10-23 13:45:58 -0400

    Update scripts/analysis-update-user.sh
    Former-commit-id: 60ddbc6677f90e054a6f006279c5dd4e41ad51dc

--- scripts/analysis-update-user.sh
+++ scripts/analysis-update-user.sh
@@ -8,13 +8,13 @@ NDAV_GROUP_ID='52321'
 PIP_DIR='/SNS/software/miniconda2/envs/sans'
 
 groupadd -g "${NDAV_GROUP_ID}" "${NDAV_GROUP}"
+useradd -s /bin/bash -u "${NDAV_USER_ID}" -m -g "${NDAV_GROUP}" "${NDAV_USER}"
 
 CURRENT_OWNER="$(ls -ld ${PIP_DIR} | awk '{print $3}')"
 if [[ ! "${CURRENT_OWNER}" == "${NDAV_USER}" ]]; then
   CURRENT_OWNER_ID="$(ls -lnd ${PIP_DIR} | awk '{print $3}')"
   useradd -s /bin/bash -u "${CURRENT_OWNER_ID}" -m -g "${NDAV_GROUP}" "${CURRENT_OWNER}"
-  runuser -l "${CURRENT_OWNER}" -c "chown -R ${PIP_DIR}"
+  runuser -l "${CURRENT_OWNER}" -c "chown -R ${NDAV_USER} ${PIP_DIR}"
 fi
 
-useradd -s /bin/bash -u "${NDAV_USER_ID}" -m -g "${NDAV_GROUP}" "${NDAV_USER}"
 runuser -l "${NDAV_USER}" -c "bash /opt/sans-backend/scripts/analysis-update.sh"

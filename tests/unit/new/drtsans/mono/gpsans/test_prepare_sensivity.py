9056db3a88b4de4fd3ed409c829bae175b055c2e
Wenduo Zhou <zhouw@ornl.gov>
2019-10-23 21:05:02 -0400

    Refs #205. Checkpointing progress on prototype and unit test.
    Former-commit-id: 73e7bf387dff0f3f88f0c5095f48c369b7498ed4

--- tests/unit/new/drtsans/mono/gpsans/test_prepare_sensivity.py
+++ tests/unit/new/drtsans/mono/gpsans/test_prepare_sensivity.py
@@ -9,6 +9,35 @@ import pytest
 # SME - William Heller <hellerwt@ornl.gov>, Lisa
 
 
+def show_diff(test_data, gold_data):
+    """
+
+    Parameters
+    ----------
+    gold_data : ndarray
+        gold data in 2D
+    test_data : ndarray
+        test data in 2D
+
+    Returns
+    -------
+    None
+    """
+    if gold_data.shape != test_data.shape:
+        # Data shape are different
+        print('Gold data and test data are in different dimension: {} vs {}'
+              ''.format(gold_data.shape, test_data.shape))
+
+    else:
+        print('      gold\ttest\tdiff')
+        for m in range(gold_data.shape[0]):
+            for n in range(gold_data.shape[1]):
+                print('({}, {}):  {}\t{}\t'
+                      .format(m, n, gold_data[m, n], test_data[m, n]))
+
+    return
+
+
 def generate_test_data():
     """Generate test data from IS (Lisa)
 
@@ -38,6 +67,52 @@ def generate_test_data():
     return l_matrix_a, error_matrix_a, l_matrix_b, error_matrix_b, l_matrix_c, error_matrix_c
 
 
+def get_weighted_averaged_matrix_abc():
+    """Get the weighted average for 3 test files A, B, C with the uncertainties
+
+    Returns
+    -------
+    ndarray, ndarray, ndarray, ndarray, ndarray, ndarray
+    """
+    m_a = np.array([
+        [1.138985248, 4.601960598, 1.127480346],
+        [1.104470543, 1.092965642, np.nan],
+        [1.115975445, 1.127480346, 0.414176454]
+    ])
+
+    m_b = np.array([
+        [1.175024613, 5.465230759, 1.216013844],
+        [1.257003075, np.nan, 1.243339998],
+        [1.297992305, 1.325318459, 0.491870768]
+    ])
+
+    m_c = np.array([
+        [1.137521253, 4.596045467, 1.068580571],
+        [np.nan, 1.206461935, 1.080070685],
+        [1.091560798, 1.137521253, 0.413644092]
+    ])
+
+    m_a_sigma = np.array([
+        [0.122350147, 0.288793202, 0.121654003],
+        [0.120254372, 0.119550801, np.nan],
+        [0.120955425, 0.121654003, 0.070793758]
+    ])
+
+    m_b_sigma = np.array([
+        [0.136908362, 0.364490799, 0.139623949],
+        [0.142310717, np.nan, 0.141418228],
+        [0.144970268, 0.146728914, 0.084804125]
+    ])
+
+    m_c_sigma = np.array([
+        [0.122183097, 0.288354303, 0.11797513],
+        [np.nan, 0.126304947, 0.118682824],
+        [0.119387912, 0.122183097, 0.070700527]
+    ])
+
+    return m_a, m_a_sigma, m_b, m_b_sigma, m_c, m_c_sigma
+
+
 def verify_result(sensitivity, sensitivity_error):
     """Verify the test result
 
@@ -53,6 +128,8 @@ def verify_result(sensitivity, sensitivity_error):
     boolean
         result is same as gold value
     """
+    assert sensitivity
+    assert sensitivity_error
 
 
 def normalize_by_monitor(flood_data, flood_data_error, monitor_counts):
@@ -93,10 +170,11 @@ def calculate_weighted_average_with_error(normalized_data, normalized_error):
 
     """
     # Calculate weighted average
-    # a = sum I(m, n) / sigma^2(m, n)
-    weighted_sum = np.sum(normalized_data / normalized_error**2)
+    # a = sum_{m, n} I(m, n) / sigma^2(m, n)
+    weighted_sum = np.sum(normalized_data[~np.isnan(normalized_data)] /
+                          normalized_error[~np.isnan(normalized_error)]**2)
     # b = sum 1 / sigma^2(m, n)
-    weights_square = np.sum(1. / normalized_error**2)
+    weights_square = np.sum(1. / normalized_error[~np.isnan(normalized_error)]**2)
     # Avg = a / b
     weighted_average = weighted_sum / weights_square
     # sigma Avg = 1 / sqrt(b)
@@ -188,14 +266,16 @@ def normalize_sensitivities(d_matrix, sigma_d_matrix):
         scalar sensitivity, error of scalar sensitivity
     """
     # Calculate wighted-average of pixel-wise sensitivities: sum on (m, n)
-    sens_avg = np.sum(d_matrix / sigma_d_matrix) / np.sum(1 / sigma_d_matrix)
+    sens_avg = np.sum(d_matrix[~(np.isinf(d_matrix) | np.isnan(d_matrix))] /
+                      sigma_d_matrix[~(np.isinf(d_matrix) | np.isnan(d_matrix))]) /\
+               np.sum(1 / sigma_d_matrix[~(np.isinf(d_matrix) | np.isnan(d_matrix))])
 
     # Normalize pixel-wise sensitivities
     sensitivities = d_matrix / sens_avg
 
     # Calculate scalar sensitivity's error
     # sigma_S_avg = sqrt(1 / sum_{m, n}(1 / sigma_D(m, n)^2))
-    sigma_sens_avg = np.sqrt(1 / np.sum(1 / sigma_d_matrix))
+    sigma_sens_avg = np.sqrt(1 / np.sum(1 / sigma_d_matrix[~(np.isinf(d_matrix) | np.isnan(d_matrix))]))
 
     # Propagate the sensitivities
     # sigma_sens(m, n) = D(m, n) / S_avg * [(sigma_D(m, n) / D(m, n))^2 + (sigma_S_avg / S_avg)^2]^{1/2}
@@ -238,6 +318,36 @@ def test_prepare_moving_det_sensitivity():
                                                                                   normalized_error=sigma_b)
     matrix_c, sigma_c, avg_c, sigma_avg_c = calculate_weighted_average_with_error(normalized_data=matrix_c,
                                                                                   normalized_error=sigma_c)
+
+    assert abs(avg_a - 8.69) < 0.01, 'Average A is not correct'
+    assert abs(sigma_avg_a - 0.329) < 0.001, 'Average Sigma_A is not correct'
+
+    # compare A, B and C to Lisa's gold data
+    gold_data_set = get_weighted_averaged_matrix_abc()
+
+    # A
+    if not np.allclose(matrix_a, gold_data_set[0], 1E-8):
+        show_diff(matrix_a, gold_data_set[0])
+        assert False, 'Weighted-averaged A does not match gold data'
+    if not np.allclose(sigma_a, gold_data_set[1], 1E-8):
+        show_diff(sigma_a, gold_data_set[1])
+        assert False, 'Weighted-averaged sigma A does not match gold data'
+
+    # B
+    if not np.allclose(matrix_b, gold_data_set[2], 1E-8):
+        show_diff(matrix_b, gold_data_set[2])
+        assert False, 'Weighted-averaged B does not match gold data'
+    if not np.allclose(sigma_b, gold_data_set[3], 1E-8):
+        show_diff(sigma_b, gold_data_set[3])
+        assert False, 'Weighted-averaged sigma B does not match gold data'
+
+    # A
+    if not np.allclose(matrix_c, gold_data_set[4], 1E-8):
+        show_diff(matrix_c, gold_data_set[4])
+        assert False, 'Weighted-averaged A does not match gold data'
+    if not np.allclose(sigma_c, gold_data_set[5], 1E-8):
+        show_diff(sigma_c, gold_data_set[5])
+        assert False, 'Weighted-averaged sigma C does not match gold data'
 
     # Apply bad pixel threshold to the data
     matrix_a, sigma_a = process_bad_pixels(matrix_a, sigma_a, threshold_min, threshold_max)

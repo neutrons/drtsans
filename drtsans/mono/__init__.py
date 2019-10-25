9105baa2315a70ea25f37eddc7382d8090cfd419
Jose Borreguero <borreguero@gmail.com>
2019-10-25 11:06:12 -0400

    Refs #171 flaky flake8
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: e574db10253278a4d0c22e48a14c5989ab9eed99

--- drtsans/mono/__init__.py
+++ drtsans/mono/__init__.py
@@ -1,4 +1,4 @@
-from .absolute_units import  *  # noqa: F403
+from .absolute_units import *  # noqa: F403
 from .dark_current import *  # noqa: F403
 
 __all__ = (absolute_units.__all__ + dark_current.__all__)  # noqa: F405

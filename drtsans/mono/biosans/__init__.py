db169560ae6212580b5037cb4e194ed0760c873d
Jose Borreguero <borreguero@gmail.com>
2019-10-25 11:43:58 -0400

    Refs #171 expose empty_beam_scaling to gpsans and biosans namespaces
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: b8ba4d11e41a1a02657437f19eb6d93de75609ab

--- drtsans/mono/biosans/__init__.py
+++ drtsans/mono/biosans/__init__.py
@@ -2,8 +2,11 @@
 # flake8: noqa
 from drtsans.transmission import apply_transmission_correction
 
-from ..load import load_histogram
-from .beam_finder import *
-from .solid_angle import *
+import drtsans.mono.absolute_units
 
-__all__ = (['load_histogram'] + beam_finder.__all__ + solid_angle.__all__)
+from ..load import load_histogram
+from ..absolute_units import *  # noqa: F403
+from .beam_finder import *  # noqa: F403
+from .solid_angle import *  # noqa: F403
+
+__all__ = ['load_histogram'] + beam_finder.__all__ + solid_angle.__all__ + drtsans.mono.absolute_units.__all__

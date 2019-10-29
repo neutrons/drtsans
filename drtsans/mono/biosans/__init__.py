8636fa2fab855154c7fab85ecc0f936155354750
Jose Borreguero <borreguero@gmail.com>
2019-10-29 17:02:50 -0400

    Refs #175 calculate_transmission exposed to the instrument namespaces
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: a6d050d61879b4008cd84a3845e587fb9ebbf610

--- drtsans/mono/biosans/__init__.py
+++ drtsans/mono/biosans/__init__.py
@@ -1,15 +1,17 @@
 # flake8: noqa
 from drtsans.transmission import apply_transmission_correction
 import drtsans.absolute_units
+import drtsans.transmission
 import drtsans.mono.absolute_units
 import drtsans.mono.geometry
 
 from ..load import load_histogram
+from ..geometry import *
+from ...transmission import *
 from ..absolute_units import *
 from ...absolute_units import *
-from ..geometry import *
 from .beam_finder import *
 from .solid_angle import *
 
 __all__ = ['load_histogram'] + beam_finder.__all__ + solid_angle.__all__ + drtsans.absolute_units.__all__\
-          + drtsans.mono.absolute_units.__all__ + drtsans.mono.geometry.__all__
+          + drtsans.transmission.__all__ + drtsans.mono.absolute_units.__all__ + drtsans.mono.geometry.__all__

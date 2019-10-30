4fa186fac798265c5d20bcc8905ef621a6a89cf6
Ross Whitfield <whitfieldre@ornl.gov>
2019-10-30 16:13:54 -0400

    Send Mantid output to Null during plotting
    Signed-off-by: Ross Whitfield <whitfieldre@ornl.gov>
    
    Former-commit-id: 12491286fbdc99985ec8e07a9b162ddc7d0115ff

--- scripts/plot/EQSANS_2D.py
+++ scripts/plot/EQSANS_2D.py
@@ -1,12 +1,20 @@
 import numpy as np
 import json
 import sys
+import os
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt # noqa E402
 from matplotlib.colors import LogNorm # noqa E402
 import mpld3 # noqa E402
 from mpld3 import plugins # noqa E402
+
+# Shut up Mantid, send everything to Null
+# Create file to suppress output, this overrides the settings in /etc/mantid.local.properties
+if not os.path.exists(os.path.expanduser("~/.mantid")):
+    os.makedirs(os.path.expanduser("~/.mantid"))
+with open(os.path.expanduser("~/.mantid/Mantid.user.properties"), "a") as f:
+    f.write("\nlogging.channels.consoleChannel.class=NullChannel\n")
 from mantid.simpleapi import Load # noqa E402
 
 
@@ -40,3 +48,5 @@ ax.set_ylabel("Pixel")
 plugins.connect(fig, plugins.MousePosition(fontsize=14, fmt=".0f"))
 
 print(json.dumps(mpld3.fig_to_dict(fig)))
+
+os.remove(os.path.expanduser("~/.mantid/Mantid.user.properties"))

bde2080a6cb576704192ba27048a121b683454fc
Jose Borreguero <borreguero@gmail.com>
2019-11-01 10:53:38 -0400

    Refs #174 hyperlinks to mantid algorithms
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: e5ca05635f1a7fc20ab7eb62eb0626dcb09ec18b

--- drtsans/dark_current.py
+++ drtsans/dark_current.py
@@ -1,10 +1,13 @@
 from dateutil.parser import parse as parse_date
 import numpy as np
 
-# Links to mantid algorithms
-# Integration <https://docs.mantidproject.org/nightly/algorithms/Integration-v1.html>
-# DeleteWorkspace <https://docs.mantidproject.org/nightly/algorithms/DeleteWorkspace-v1.html>
-from mantid.simpleapi import mtd, Integration, DeleteWorkspace
+r"""
+Links to mantid algorithms
+Integration <https://docs.mantidproject.org/nightly/algorithms/Integration-v1.html>
+DeleteWorkspace <https://docs.mantidproject.org/nightly/algorithms/DeleteWorkspace-v1.html>
+"""
+from mantid.simpleapi import Integration, DeleteWorkspace
+from mantid.api import mtd
 
 r"""
 Hyperlinks to drtsans functions

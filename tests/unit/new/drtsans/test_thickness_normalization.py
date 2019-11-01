eed33f4fca1386c98280ac72cc7a4ded9b7b211d
Jose Borreguero <borreguero@gmail.com>
2019-11-01 10:54:00 -0400

    Refs #174 Use CreateWorkspace in place of WorkspaceFactory
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 539d625f41d032996ccdac53b1cada407fd56551

--- tests/unit/new/drtsans/test_thickness_normalization.py
+++ tests/unit/new/drtsans/test_thickness_normalization.py
@@ -1,9 +1,22 @@
 import pytest
 import os, numpy as np          # noqa: E401
 from numpy.testing import assert_allclose
-from mantid.simpleapi import WorkspaceFactory
-# https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/blob/next/drtsans/thickness_normalization.py
+
+r"""
+Links to mantid algorithms
+CreateWorkspace <https://docs.mantidproject.org/nightly/algorithms/CreateWorkspace-v1.html>
+"""
+from mantid.simpleapi import CreateWorkspace
+
+
+r"""
+Hyperlinks to drtsans functions
+normalize_by_thickness <https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/blob/next/drtsans
+/thickness_normalization.py>
+"""  # noqa: E501
+from drtsans.settings import unique_workspace_dundername
 from drtsans.thickness_normalization import normalize_by_thickness
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -22,27 +35,19 @@ def workspaces(testdata):
     """create workspace using the data loaded from the csv file provided by LiLin He"""
     # load data from csv
     I, E, normedI, normedE = testdata
-    nrows = 1
     nbins = I.size
-    # create input workspace
-    inputws = WorkspaceFactory.create(
-        "Workspace2D", NVectors=nrows, XLength=nbins+1, YLength=nbins
-    )
-    inputws.setX(0, np.arange(nbins+1))
-    inputws.setY(0, I)
-    inputws.setE(0, E)
-    # create expected output workspace
-    expected_output_ws = WorkspaceFactory.create(
-        "Workspace2D", NVectors=nrows, XLength=nbins+1, YLength=nbins
-    )
-    expected_output_ws.setX(0, np.arange(nbins+1))
-    expected_output_ws.setY(0, normedI)
-    expected_output_ws.setE(0, normedE)
-    return inputws, expected_output_ws
+    inputws = CreateWorkspace(NSpec=1, DataX=np.arange(nbins+1), DataY=I, DataE=E,
+                              OutputWorkspace=unique_workspace_dundername())
+    expected_output_ws = CreateWorkspace(NSpec=1, DataX=np.arange(nbins+1), DataY=normedI, DataE=normedE,
+                                         OutputWorkspace=unique_workspace_dundername())
+    yield inputws, expected_output_ws
+    # Delete upon closure of the fixture
+    [workspace.delete() for workspace in (inputws, expected_output_ws)]
 
 
 def test_thickness_normalization(workspaces):
-    '''Test thickness normalization using data from a cvs file.
+    r"""
+    Test thickness normalization using data from a cvs file.
     The normalized result is compared to expected result provided in a csv file.
 
     Function tested: drtsans.tof.eqsans.api.normalize_by_thickness
@@ -51,7 +56,7 @@ def test_thickness_normalization(workspaces):
 
     dev - Jiao Lin <linjiao@ornl.gov>
     SME - LiLin He <hel3@ornl.gov>
-    '''
+    """
     inputws, expected_output_ws = workspaces
     thickness = 0.1
     normed = normalize_by_thickness(inputws, thickness)

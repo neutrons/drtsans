eb182c28a29f43b571a6e89b62d471c59fd5c74b
Jose Borreguero <borreguero@gmail.com>
2019-10-31 20:26:11 -0400

    Refs #174 reorganize the import statements in EQSANS init file
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 781a37a0e380c209ffb89e70afb1d6ee68ff255b

--- drtsans/tof/eqsans/__init__.py
+++ drtsans/tof/eqsans/__init__.py
@@ -1,12 +1,17 @@
 # flake8: noqa
-import drtsans.transmission
-import drtsans.absolute_units
-import drtsans.thickness_normalization
 
-from ...transmission import *
-from ...beam_finder import *
+import drtsans.absolute_units
 from ...absolute_units import *
+
+import drtsans.beam_finder
+from ...beam_finder import *
+
+import drtsans.thickness_normalization
 from ...thickness_normalization import *
+
+import drtsans.transmission
+from ...transmission import *
+
 from .api import *
 from .cfg import *
 from .correct_frame import *
@@ -18,8 +23,18 @@ from .mask import *
 from .normalisation import *
 from .transmission import *
 
-__all__ = (drtsans.beam_finder.__all__ + drtsans.transmission.__all__ + drtsans.absolute_units.__all__
-           + drtsans.thickness_normalization.__all__ + api.__all__ + cfg.__all__ + correct_frame.__all__
-           + dark_current.__all__ + geometry.__all__
-           + iq.__all__ + load.__all__ + mask.__all__ + normalization.__all__
-           + transmission.__all__)  # noqa: F405
+__all__ = [] + drtsans.absolute_units.__all__\
+          + drtsans.beam_finder.__all__\
+          + drtsans.thickness_normalization.__all__\
+          + drtsans.transmission.__all__\
+          + api.__all__\
+          + cfg.__all__\
+          + correct_frame.__all__\
+          + dark_current.__all__\
+          + geometry.__all__\
+          + load.__all__\
+          + iq.__all__\
+          + mask.__all__\
+          + normalisation.__all__\
+          + transmission.__all_
+

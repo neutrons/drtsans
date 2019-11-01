f20dac38f9208d34b0ef32de0e7068b50f223a97
Jose Borreguero <borreguero@gmail.com>
2019-11-01 11:32:06 -0400

    Refs #174 add hyperlinks to mantid and drtsans algorithms
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 724f8c9334e8db3a5669ea87ac6c11f61eb45987

--- drtsans/mono/normalisation.py
+++ drtsans/mono/normalisation.py
@@ -1,13 +1,27 @@
-# https://docs.mantidproject.org/nightly/algorithms/CreateSingleValuedWorkspace-v1.html
-# https://docs.mantidproject.org/nightly/algorithms/Divide-v1.html
-from mantid.simpleapi import CreateSingleValuedWorkspace, Divide, mtd
-from drtsans.samplelogs import SampleLogs
+r"""
+Links to mantid algorithms
+CreateSingleValuedWorkspace <https://docs.mantidproject.org/nightly/algorithms/CreateSingleValuedWorkspace-v1.html>
+Divide <https://docs.mantidproject.org/nightly/algorithms/Divide-v1.html>
+"""
+from mantid.simpleapi import CreateSingleValuedWorkspace, Divide
+from mantid.api import mtd
+
+r"""
+Hyperlinks to drtsans functions
+unique_workspace_dundername <https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/blob/next/drtsans/settings.py>
+SampleLogs <https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/blob/next/drtsans/samplelogs.py>
+"""  # noqa: E501
 from drtsans.settings import unique_workspace_dundername
+from drtsans.samplelogs import SampleLogs
 
 
 def normalize_by_time(input_workspace, output_workspace=None):
     """Normalise by time
     Used to normalise dark current
+
+    **Mantid algorithms used:**
+    :ref:`Divide <algm-Divide-v1>`,
+        <https://docs.mantidproject.org/nightly/algorithms/Divide-v1.html>
 
     Parameters
     ----------
@@ -20,9 +34,7 @@ def normalize_by_time(input_workspace, output_workspace=None):
         output_workspace = input_workspace
     timer = SampleLogs(input_workspace).timer.value  # seconds
     timer_workspace = CreateSingleValuedWorkspace(timer, OutputWorkspace=unique_workspace_dundername())
-    Divide(LHSWorkspace=input_workspace,
-           RHSWorkspace=timer_workspace,
-           OutputWorkspace=output_workspace)
+    Divide(LHSWorkspace=input_workspace, RHSWorkspace=timer_workspace, OutputWorkspace=output_workspace)
     return mtd[output_workspace]
 
 
@@ -31,7 +43,9 @@ def normalize_by_monitor(input_workspace, output_workspace=None, factor_is=1.e08
 
     **Mantid algorithms used:**
     :ref: `CreateSingleValuedWorkspace <algm-CreateSingleValuedWorkspace-v1>
+         <https://docs.mantidproject.org/nightly/algorithms/CreateSingleValuedWorkspace-v1.html>
     :ref:`Divide <algm-Divide-v1>`,
+        <https://docs.mantidproject.org/nightly/algorithms/Divide-v1.html>
 
     Parameters
     ----------

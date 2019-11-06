ebd94ddabd8da3f09079da0aad11d5a9bf29066c
Jose Borreguero <borreguero@gmail.com>
2019-11-06 11:11:21 -0500

    Refs #256 transform_to_wavelength and option to overwrite instrument
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 27727c023c457cb27745310fe5fc8fc645020018

--- drtsans/mono/load.py
+++ drtsans/mono/load.py
@@ -1,17 +1,17 @@
 import os
 
 # https://docs.mantidproject.org/nightly/algorithms/LoadHFIRSANS-v1.html
-from mantid.simpleapi import LoadHFIRSANS, LoadEventNexus, CloneWorkspace
+from mantid.simpleapi import LoadHFIRSANS, LoadEventNexus, CloneWorkspace, LoadInstrument, HFIRSANS2Wavelength
 from mantid.api import mtd
 
 from drtsans.instruments import instrument_enum_name
 from drtsans.settings import amend_config
 from drtsans.samplelogs import SampleLogs
 
-__all__ = ['load_events', 'load_histogram']
+__all__ = ['load_events', 'load_histogram', 'transform_to_wavelength']
 
 
-def load_events(run, output_workspace=None, data_dir=None, **kwargs):
+def load_events(run, output_workspace=None, data_dir=None, overwrite_instrument=False, **kwargs):
     r"""
     Load an event Nexus file produced by the HFIR instruments at ORNL.
 
@@ -19,10 +19,14 @@ def load_events(run, output_workspace=None, data_dir=None, **kwargs):
     ----------
     run: int, str
         Examples: ``55555`` or ``CG3_55555`` or file path.
-    data_dir: str, list
-        Additional data search directories
     output_workspace: str
         If not specified it will be ``BIOSANS_55555`` determined from the supplied value of ``run``.
+    data_dir: str, list
+        Additional data search directories
+    overwrite_instrument: bool, str
+        If not :py:obj:`False`, ignore the instrument embedeed in the Nexus file. If :py:obj:`True`, use the
+        latest instrument definition file (IDF) available in Mantid. If ``str``, then it should be the filepath to the
+        desired IDF.
     kwargs: dict
         Additional positional arguments for :ref:`LoadEventNexus <algm-LoadEventNexus-v1>`.
 
@@ -42,9 +46,16 @@ def load_events(run, output_workspace=None, data_dir=None, **kwargs):
 
     if isinstance(run, int) or isinstance(run, str):
         with amend_config({'default.instrument': instrument_name}, data_dir=data_dir):
+            if overwrite_instrument is not False:
+                kwargs['LoadNexusInstrumentXML'] = False
             LoadEventNexus(Filename=str(run), OutputWorkspace=output_workspace, LoadMonitors=True, **kwargs)
     else:
         CloneWorkspace(run, OutputWorkspace=output_workspace)
+
+    if overwrite_instrument is not False:
+        optional_arguments = dict(InstrumentName=instrument_name) if overwrite_instrument is True else dict(
+            Filename=overwrite_instrument)
+        LoadInstrument(Workspace=output_workspace, RewriteSpectraMap=False, **optional_arguments)
 
     # Insert monitor counts as log entry
     monitor_workspace = mtd[output_workspace + '_monitors']
@@ -86,3 +97,29 @@ def load_histogram(filename, output_workspace=None, wavelength=None, wavelength_
     ws = LoadHFIRSANS(Filename=filename, Wavelength=wavelength, WavelengthSpread=wavelength_spread,
                       SampleDetectorDistance=sample_det_cent, OutputWorkspace=output_workspace)
     return ws
+
+
+def transform_to_wavelength(input_workspace, output_workspace=None):
+    r"""
+    Transforms the event files with fake time of flight from the SANS instruments at HFIR into histograms
+    in wavelength.
+
+    **Mantid Algorithms used:**
+    :ref:`Divide <algm-HFIRSANS2Wavelength-v1>`,
+
+    Parameters
+    ----------
+    input_workspace: str, ~mantid.api.IEventWorkspace
+        Events workspace in time-of-flight.
+    output_workspace: str
+        Name of the output workspace. If :py:obj:`None`, the name of the input_workspace will be
+        used, thus overwritting the input workspace.
+    Returns
+    -------
+    ~mantid.api.MatrixWorkspace
+    """
+    if output_workspace is None:
+        output_workspace = str(input_workspace)
+
+    HFIRSANS2Wavelength(InputWorkspace=input_workspace, OutputWorkspace=output_workspace)
+    return mtd[output_workspace]

41c40ccb83d9eace35593de9d2a1d3b33bbc5a33
Pete Peterson <petersonpf@ornl.gov>
2019-11-07 14:55:34 -0500

    Change to use new central function
    Former-commit-id: b6f1686c28bc828a29b397a1f917dd3e55a6d656

--- drtsans/mono/biosans/solid_angle.py
+++ drtsans/mono/biosans/solid_angle.py
@@ -12,6 +12,7 @@ Links to drtsans modules and functions
 solid_angle <https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/blob/next/drtsans/solid_angle.py>
 """  # noqa: E501
 from drtsans import solid_angle
+from drtsans.geometry import bank_workspace_index_range
 
 __all__ = ['solid_angle_correction', ]
 
@@ -42,20 +43,17 @@ def solid_angle_correction(input_workspace, output_workspace=None):
     if output_workspace is None:
         output_workspace = str(input_workspace)
 
-    # The first two spectra could contain monitor detectors, e.g. if using LoadEmptyInstrument or if using
-    # LoadInstrument(RewriteSpectraMap=True)
-    shift = 2 if mtd[str(input_workspace)].spectrumInfo().isMonitor(0) is True else 0
-
     # Apply correction to main detector
-    spectra_in_main_detector = 24 * 8 * 256  # there are 24 eight-packs, 8 tubes/eight-pack, 256 pixels/tube
-    first_index, last_index = shift, shift + spectra_in_main_detector - 1
+    first_index, last_index = bank_workspace_index_range(input_workspace, 'detector1')
+    last_index -= 1  # SolidAngle has inclusive indices
     solid_angle.solid_angle_correction(input_workspace, detector_type='VerticalTube',
                                        StartWorkspaceIndex=first_index, EndWorkspaceIndex=last_index,
                                        output_workspace=output_workspace)
+
     # Apply correction to wing detector
-    spectra_in_wing_detector = 20 * 8 * 256  # there are 20 eight-packs
-    first_index = shift + spectra_in_main_detector
-    last_index = shift + spectra_in_main_detector + spectra_in_wing_detector - 1
+    first_index, last_index = bank_workspace_index_range(input_workspace, 'wing_detector')
+    last_index -= 1  # SolidAngle has inclusive indices
     solid_angle.solid_angle_correction(output_workspace, detector_type='VerticalWing',
                                        StartWorkspaceIndex=first_index, EndWorkspaceIndex=last_index)
+
     return mtd[output_workspace]

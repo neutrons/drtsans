eebfb902626bc714767e75a2faae0dfaff7a2ac0
Mathieu Doucet <doucetm@ornl.gov>
2019-11-12 16:25:28 -0500

    Simplify biosans script
    Former-commit-id: f57d71d8ce606e7804e03a74acb40b41397b57a5

--- scripts/biosans_reduction.py
+++ scripts/biosans_reduction.py
@@ -4,118 +4,22 @@
 import json
 import os
 import sys
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 import mantid.simpleapi as msapi  # noqa E402
 
 import drtsans  # noqa E402
 from drtsans.mono import biosans as sans  # noqa E402
-from drtsans.iq import BinningMethod, BinningParams  # noqa E402
-from drtsans.save_ascii import save_ascii_binned_1D, save_ascii_binned_2D  # noqa E402
 from drtsans.settings import unique_workspace_dundername as uwd  # noqa E402
 
+from common_utils import get_Iq, get_Iqxqy, setup_configuration
 
 INSTRUMENT = 'BIOSANS'
-
-
-def load_data(filename, is_wing=False, output_workspace=None):
-    """
-        Load data. This should be part of the drtsans.mono API.
-        TODO: Need to deal with detector and sample offsets.
-    """
-    if output_workspace is None:
-        output_workspace = uwd()
-
-    # If we didn't get a file path, make sure Mantid can find the file
-    if not os.path.isfile(filename):
-        filename = "{}{}".format(INSTRUMENT, filename)
-
-    ws = msapi.LoadEventNexus(Filename=filename, OutputWorkspace=output_workspace, LoadNexusInstrumentXML=False)
-    ws = msapi.HFIRSANS2Wavelength(ws, OutputWorkspace=output_workspace)
-    ws = msapi.SetUncertainties(ws, "sqrtOrOne", OutputWorkspace=output_workspace)
-
-    if is_wing:
-        msapi.MaskDetectors(ws, ComponentList='detector1')
-    else:
-        msapi.MaskDetectors(ws, ComponentList='wing_detector')
-
-    return ws
-
-
-def setup_configuration(json_params):
-    """
-        Extract configuration
-    """
-    config = json_params['configuration']
-
-    # Masking
-    # TODO: get default mask from configuration
-    config["mask"] = None
-    default_mask = []
-    w = msapi.LoadEmptyInstrument(InstrumentName=INSTRUMENT, OutputWorkspace=uwd())
-    if config["useDefaultMask"]:
-        for d in default_mask:
-            msapi.MaskBTP(Workspace=w, **d)
-        config["mask"] = msapi.ExtractMask(w, OutputWorkspace=uwd()).OutputWorkspace
-
-    if config["useMaskBackTubes"]:
-        msapi.logger.notice("Masking back tubes is not implemented")
-
-    return config
-
-
-def process_data(filename, cfg, output_workspace=None):
-    """
-        Load and process data.
-        This should be in drtsans.mono.biosans
-    """
-    ws = load_data(filename, is_wing=cfg['is_wing'],
-                   output_workspace=output_workspace)
-
-    if cfg['center_x'] is not None and cfg['center_y'] is not None:
-        sans.center_detector(ws, center_x=cfg['center_x'], center_y=cfg['center_y'],
-                             center_y_wing=cfg['center_y_wing'])
-
-    # Dark current
-    if cfg['useDarkFileName']:
-        dark_ws = load_data(cfg['darkFileName'],
-                            is_wing=cfg['is_wing'],
-                            output_workspace=uwd())
-        sans.subtract_dark_current(ws, dark_ws)
-        msapi.logger.notice("Dark current subtracted")
-
-    # Normalization
-    if cfg['normalization'] == 'Time':
-        sans.normalize_by_time(ws)
-    elif cfg['normalization'] == 'Monitor':
-        sans.normalize_by_monitor(ws)
-
-    # Solid angle
-    if cfg['useSolidAngleCorrection']:
-        sans.solid_angle_correction(ws)
-        msapi.logger.notice("Solid angle correction performed")
-
-    # Sensitivity
-    sensitivity_filename = cfg['sensitivityFileName']
-    if cfg['is_wing']:
-        msapi.logger.warning("WE NEED TO DEAL WITH WING SENSITIVITY")
-        sensitivity_filename = sensitivity_filename.replace('_flood_', '_flood_wing_')
-    if os.path.isfile(sensitivity_filename):
-        drtsans.apply_sensitivity_correction(ws, sensitivity_filename=sensitivity_filename)
-    else:
-        msapi.logger.warning("File doesn't exist: %s" % sensitivity_filename)
-
-    return ws
 
 
 def apply_transmission(ws, transmission_run, empty_run, cfg):
     """
         Apply transmission
-
-        Note: this doesn't currently work because we are missing DAS logs
     """
     # TODO: there must be a better way to indicate that we are supplying a transmission value
     try:
@@ -131,92 +35,26 @@ def apply_transmission(ws, transmission_run, empty_run, cfg):
         msapi.logger.notice('Applying transmission correction with transmission file.')
 
         # We need to see the beam, which is on the main detector
-        _is_wing = cfg['is_wing']
-        cfg['is_wing'] = False
-        ws_tr_sample = process_data(transmission_run, cfg)
-        ws_tr_direct = process_data(empty_run, cfg)
-        cfg['is_wing'] = _is_wing
+        _mask_detector = cfg['mask_detector']
+        cfg['mask_detector'] = 'wing_detector'
+        ws_tr_sample = sans.prepare_data(transmission_run, **cfg)
+        ws_tr_direct = sans.prepare_data(empty_run, **cfg)
+        cfg['mask_detector'] = _mask_detector
 
         # TODO: use the number of pixels around the beam spot
         tr_ws = sans.calculate_transmission(ws_tr_sample,
                                             ws_tr_direct,
                                             radius=None,
                                             radius_unit="mm")
-        ws = sans.apply_transmission_correction(ws,
-                                                trans_workspace=tr_ws)
+        ws = sans.apply_transmission_correction(ws, trans_workspace=tr_ws)
     return ws
-
-
-def get_Iq(ws, json_params, config):
-    """
-        Compute I(q) from corrected workspace
-    """
-    q_data = drtsans.convert_to_q(ws, mode='scalar')
-
-    # TODO: Need to get min/max Q from either drtsans or the frontend
-    linear_binning = config["QbinType"] == "linear"
-    q_min = np.min(q_data.mod_q)
-    q_max = np.max(q_data.mod_q)
-    bin_params = BinningParams(min=q_min, max=q_max,
-                               bins=int(config["numQBins"]))
-    iq_output = sans.bin_intensity_into_q1d(q_data,
-                                            bin_params=bin_params,
-                                            linear_binning=linear_binning,
-                                            bin_method=BinningMethod.NOWEIGHT)
-
-    wing_label = '_wing' if config['is_wing'] else ''
-    output_file = os.path.join(config["outputDir"],
-                               json_params["outputFilename"] + wing_label + '_Iq.txt')
-    save_ascii_binned_1D(output_file, "I(Q)", iq_output)
-
-    fig, ax = plt.subplots()
-    ax.errorbar(iq_output.mod_q, iq_output.intensity, yerr=iq_output.error, label="I(Q)")
-    ax.set_xlabel("$Q (\AA^{-1})$")  # noqa W605
-    plt.ylabel('Intensity')
-    output_file = os.path.join(config["outputDir"],
-                               json_params["outputFilename"] + wing_label + '_Iq.png')
-
-    fig.savefig(output_file)
-
-
-def get_Iqxqy(ws, json_params, config):
-    """
-        Compute I(qx,qy) from corrected workspace
-    """
-    q_data = drtsans.convert_to_q(ws, mode='azimuthal')
-    qx_min = np.min(q_data.qx)
-    qx_max = np.max(q_data.qx)
-
-    binning_x = BinningParams(qx_min, qx_max, int(config["numQxQyBins"]))
-    qy_min = np.min(q_data.qy)
-    qy_max = np.max(q_data.qy)
-    binning_y = BinningParams(qy_min, qy_max, int(config["numQxQyBins"]))
-
-    iq_output = sans.bin_iq_into_linear_q2d(q_data,
-                                            qx_bin_params=binning_x,
-                                            qy_bin_params=binning_y,
-                                            method=BinningMethod.NOWEIGHT)
-
-    wing_label = '_wing' if config['is_wing'] else ''
-    filename = os.path.join(config["outputDir"],
-                            json_params["outputFilename"] + wing_label + '_Iqxqy.txt')
-    save_ascii_binned_2D(filename, "I(Qx,Qy)", iq_output)
-
-    fig, ax = plt.subplots()
-    pcm = ax.pcolormesh(iq_output.qx, iq_output.qy, iq_output.intensity,
-                        norm=colors.LogNorm())
-    fig.colorbar(pcm, ax=ax)
-    picture_file = os.path.join(config["outputDir"],
-                                json_params["outputFilename"] + wing_label + '_Iqxqy.png')
-    fig.savefig(picture_file)
-
 
 def reduction(json_params, config):
     """
         Perform the whole reduction
     """
     # Load and prepare scattering data
-    ws = process_data(json_params["runNumber"], config)
+    ws = sans.prepare_data(json_params["runNumber"], **config)
 
     # Transmission
     transmission_run = json_params["transmission"]["runNumber"]
@@ -227,7 +65,7 @@ def reduction(json_params, config):
     # Background
     bkg_run = json_params["background"]["runNumber"]
     if bkg_run != "":
-        ws_bck = process_data(json_params["background"]["runNumber"], config)
+        ws_bck = sans.prepare_data(json_params["background"]["runNumber"], **config)
 
         # Background transmission
         transmission_run = json_params["background"]["transmission"]["runNumber"]
@@ -240,18 +78,21 @@ def reduction(json_params, config):
         msapi.logger.notice("Background subtracted")
 
     # Final normalization
-    absolute_scale = float(config["absoluteScale"])
+    absolute_scale = float(json_params["configuration"]["absoluteScale"])
     sample_thickness = float(json_params["thickness"])
     ws /= sample_thickness
     ws *= absolute_scale
 
-    # Apply user mask
-    if config['useMaskFileName']:
-        drtsans.mask_utils.apply_mask(ws, mask=config["maskFileName"])
-
     # Convert the Q
-    get_Iq(ws, json_params, config)
-    get_Iqxqy(ws, json_params, config)
+    wing_label = '_wing' if config['is_wing'] else ''
+    get_Iq(ws, json_params["configuration"]["outputDir"],
+           json_params["outputFilename"], label=wing_label,
+           linear_binning=json_params["configuration"]["QbinType"] == "linear",
+           nbins=int(json_params["configuration"]["numQBins"]))
+
+    get_Iqxqy(ws, json_params["configuration"]["outputDir"],
+              json_params["outputFilename"], label=wing_label,
+              nbins=int(json_params["configuration"]["numQxQyBins"]))
 
 
 if __name__ == "__main__":
@@ -277,7 +118,9 @@ if __name__ == "__main__":
     empty_run = json_params["empty"]["runNumber"]
     if empty_run != "":
         # Load and compute beam center position
-        db_ws = load_data(empty_run, is_wing=False)
+        db_ws = sans.load_events(empty_run,
+                                 overwrite_instrument=True, output_workspace=uwd())
+        msapi.MaskDetectors(db_ws, ComponentList='wing_detector')
         center = sans.find_beam_center(db_ws)
 
         # Store the center position for later use
@@ -292,7 +135,12 @@ if __name__ == "__main__":
     # at the same time, but we need to be able to process the sensitivity in two steps.
     # This could be hidden in the API and done automatically.
     config['is_wing'] = False
+    config['mask_detector'] = 'wing_detector'
     reduction(json_params, config)
 
     config['is_wing'] = True
+    config['mask_detector'] = 'detector1'
+    if json_params['configuration']['useSensitivityFileName']:
+        config['sensitivity_file_path'] = json_params['configuration']['sensitivityFileName'].replace('_flood_', '_flood_wing_')
+
     reduction(json_params, config)

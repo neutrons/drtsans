af10fcb01e8a140ca377e9adb625c22a232698f6
Jose Borreguero <borreguero@gmail.com>
2019-11-15 11:29:03 -0500

    Refs #218 pylint not necessary to install
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: faa4cbcf1508a129af03f0483868727fe7932326

--- scripts/pylint_job.sh
+++ scripts/pylint_job.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -x
 source activate mantid
-conda install pylint
 python -m pylint --disable=C --disable=fixme --disable=no-name-in-module drtsans tests --ignore drtsans/_version.py

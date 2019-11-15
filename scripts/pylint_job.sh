b6959bbe3aafa160f1ca74288c579c1e83da2848
Cooper, Caleb <coopercd@ornl.gov>
2019-11-15 10:11:59 -0500

    Update pylint_job.sh
    Former-commit-id: 89ac723ba6b5896a306998fd8c61f6e794912a01

--- scripts/pylint_job.sh
+++ scripts/pylint_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 source activate mantid
-conda install pylint
+conda install -y pylint
 python -m pylint --disable=C --disable=fixme --disable=no-name-in-module drtsans tests --ignore drtsans/_version.py

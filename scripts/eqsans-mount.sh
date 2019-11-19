18a11a286ba6b34fa4e78319fc53fe30cff5bcef
Cooper, Caleb <coopercd@ornl.gov>
2019-11-19 18:56:44 -0500

    Update scripts/eqsans-mount.sh
    Former-commit-id: 3264adc3cc412d3328e0b62a5eb608a2456cb7a8

--- scripts/eqsans-mount.sh
+++ scripts/eqsans-mount.sh
@@ -2,6 +2,7 @@
 set -puex
 
 date +%F-%T
+hostname -A
 declare COUNT=0
 if ! grep -qs 'EQSANS' /proc/mounts; then
   until mount -t nfs -o "tcp,ro,noatime,rsize=32768,wsize=32768" snsdata.ornl.gov:/stornext/snfs1/instruments/EQSANS /SNS/EQSANS; do

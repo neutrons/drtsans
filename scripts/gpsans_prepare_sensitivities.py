306f3c91904ced69da478db21600234af9def801
Wenduo Zhou <zhouw@ornl.gov>
2019-11-21 14:52:05 -0500

    More on sensitivities.
    Former-commit-id: 3604c34a3e801b7e2b0a988f197d8c2a9c10f2a7

--- scripts/gpsans_prepare_sensitivities.py
+++ scripts/gpsans_prepare_sensitivities.py
@@ -5,6 +5,7 @@
 import json
 import os
 import sys
+from mantid.simpleapi import SaveNexusProcessed
 from drtsans.mono.load import load_events
 from drtsans.mask_utils import circular_mask_from_beam_center, apply_mask
 import drtsans.mono.gpsans as gp
@@ -16,6 +17,7 @@ import matplotlib.colors as colors
 import warnings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 import mantid.simpleapi as msapi  # noqa E402
+import h5py
 
 import drtsans  # noqa E402
 from drtsans.mono import gpsans as sans  # noqa E402
@@ -27,40 +29,74 @@ from drtsans.settings import unique_workspace_dundername as uwd  # noqa E402
 INSTRUMENT = 'GPSANS'
 
 
-def load_data(data):
+def export_detector_view(ws, png_name):
+    """Export detector view
+
+    Parameters
+    ----------
+    ws : ~mantid.api.MatrixWorkspace
+
+    Returns
+    -------
+
+    """
+    if isinstance(ws, np.ndarray):
+        vec_y = ws
+    else:
+        vec_y = ws.extractY().transpose()
+    vec_y = vec_y.reshape((192, 256)).transpose()
+
+    plt.imshow(vec_y)
+
+    plt.savefig(png_name)
+
+    return
+
+
+def load_data(nexus_file_name):
     """Load data
     
-    :param data: int, str
+    :param nexus_file_name: int, str
         Examples: ``55555`` or ``CG3_55555`` or file path.
     :return: 
     """
-    ws_name = str(data)
-    ws = load_events(data, output_workspace=ws_name, data_dir=None, overwrite_instrument=False)
+    ws_name = os.path.basename(nexus_file_name).split('.')[0]
+    ws = load_events(nexus_file_name, output_workspace=ws_name, data_dir=None, overwrite_instrument=False)
 
     return ws
 
 
-def calculate_beam_center_mask(beam_center_ws):
-    circular_ids = circular_mask_from_beam_center(beam_center_ws, radius=3.1415926, unit='mm')
-
-    return None
-
-
-def mask_detectors(data_ws, beam_center_mask):
+def mask_data(data_ws, beam_center_mask):
     """Mask detectors in a workspace
 
     Mask (1) beam center, (2) top and (3) bottom
 
-    :param data_ws:
-    :return:
+    Parameters
+    ----------
+    data_ws
+    beam_center_mask
+
+    Returns
+    -------
+    ~mantid.api.MatrixWorkspace
+
     """
-    # Mask beam centers
-    if beam_center_mask is not None:
-        data_ws = apply_mask(data_ws, mask=beam_center_mask, panel=None, output_workspace=None)
+    # Use beam center ws to find beam center
+    xc, yc = gp.find_beam_center(beam_center_mask)
 
-    # Mask top
+    # Center detector to the data workspace (change in geometry)
+    gp.center_detector(data_ws, xc, yc)
 
-    # Mask bottom
+    # Mask the new beam center by 65 mm (Lisa's magic number)
+    det = list(drtsans.mask_utils.circular_mask_from_beam_center(data_ws, 65))
+    drtsans.mask_utils.apply_mask(data_ws, mask=det)
+
+    # Mask top and bottom
+    apply_mask(data_ws, Pixel='1-8,249-256')
+
+    # Optionally as a debugging step, save the workspace with masked applied
+    SaveNexusProcessed(InputWorkspace=data_ws, Filename='{}_masked.nxs'.format(data_ws))
+    export_detector_view(data_ws, '{}_masked.png'.format(data_ws))
 
     return data_ws
 
@@ -77,34 +113,30 @@ def setup_configuration(json_params):
     -------
 
     """
-    flood_files = json_params['flood files']
-    direct_beam_files = json_params['direct beam files']
+    # Parse ITPS and runs
+    ipts_number = json_params['IPTS-Number']
+    flood_runs = json_params['Flood Runs']
+    direct_beam_runs = json_params['Direct Beam Runs']
 
-    if len(flood_files) != len(direct_beam_files):
+    # Check
+    if len(flood_runs) != len(direct_beam_runs):
         raise RuntimeError('Number of flood files ({}) shall be equal to number of direct beam files ({})'
-                           ''.format(len(flood_files), len(direct_beam_files)))
+                           ''.format(len(flood_runs), len(direct_beam_runs)))
 
-    return flood_files, direct_beam_files
+    # Form NeXus file names from IPTS and run
+    flood_files = list()
+    direct_beam_files = list()
 
+    for f_index in range(len(flood_runs)):
+        flood_files.append('/HFIR/CG2/IPTS-{}/nexus/CG2_{}.nxs.h5'.format(ipts_number, flood_runs[f_index]))
+        direct_beam_files.append('/HFIR/CG2/IPTS-{}/nexus/CG2_{}.nxs.h5'.format(ipts_number,
+                                                                                direct_beam_runs[f_index]))
+    # END-FOR
 
-def center_detector(ws):
-    """Center detector to detector beam center
+    # Monitor
+    monitor_array = np.array(json_params['Monitor'])
 
-    Parameters
-    ----------
-    ws : ~mantid.Workspace
-
-    Returns
-    -------
-    ~mantid.Workspace
-
-    """
-    xc, yc = gp.find_beam_center(ws)
-
-    # center the detector
-    gp.center_detector(ws, xc, yc)
-
-    return ws
+    return flood_files, direct_beam_files, monitor_array
 
 
 def prepare_data(flood_run_ws_list, beam_center_run_ws_list):
@@ -124,16 +156,15 @@ def prepare_data(flood_run_ws_list, beam_center_run_ws_list):
 
     masked_flood_list = list()
     for i_pair in range(num_ws_pairs):
-        # use beam center run to locate the beam center and do mask
+        # use beam center run to locate the beam center and then do mask to data workspace
         bc_ws = beam_center_run_ws_list[i_pair]
-        beam_center_mask = calculate_beam_center_mask(bc_ws)
-
-        # mask flood workspace
         flood_ws = flood_run_ws_list[i_pair]
-        mask_detectors(flood_ws, beam_center_mask)
-        flood_ws = set_init_uncertainties(flood_ws, flood_ws)
-
-        masked_flood_list.append(flood_ws)
+        # mask
+        masked_flood_ws = mask_data(flood_ws, bc_ws)
+        # set uncertainties
+        masked_flood_ws = set_init_uncertainties(flood_ws, masked_flood_ws)
+        # append
+        masked_flood_list.append(masked_flood_ws)
     # END-FOR
 
     # Combine to numpy arrays: N, M
@@ -175,38 +206,52 @@ def main(argv):
     output_file = json_params['output_file_name']
 
     # Set up the configuration
-    flood_runs, beam_center_runs = setup_configuration(json_params)
-    assert len(flood_runs) == len(beam_center_runs)
-
-    # Prepare files for preparing sensitivities
+    flood_files, direct_beam_files, monitor_array = setup_configuration(json_params)
 
     # Load data/runs
     flood_ws_list = list()
     beam_center_ws_list = list()
-    num_ws_pairs = len(flood_runs)
+    num_ws_pairs = len(flood_files)
     for f_index in range(num_ws_pairs):
         # load flood
-        flood_ws_i = load_data('/HFIR/CG2/IPTS-23801/nexus/CG2_{}.nxs.h5'.format(flood_runs[f_index]))
+        flood_ws_i = load_data(flood_files[f_index])
         flood_ws_list.append(flood_ws_i)
 
         # load beam center
-        beam_center_ws_list.append(load_data('/HFIR/CG2/IPTS-23801/nexus/CG2_{}.nxs.h5'
-                                             ''.format(beam_center_runs[f_index])))
+        beam_center_ws_i = load_data(direct_beam_files[f_index])
+        beam_center_ws_list.append(beam_center_ws_i)
     # END-FOR
 
     flood_data_array, flood_sigma_array = prepare_data(flood_ws_list, beam_center_ws_list)
+    # Export flood data and sigma
+    sens_h5 = h5py.File('{}.h5'.format(output_file.split('.')[0]), 'w')
+    flood_group = sens_h5.create_group('Flood')
+    flood_group.create_dataset('flood', data=flood_data_array)
+    flood_group.create_dataset('flood error', data=flood_sigma_array)
 
     # Calculate sensitivities for each file
     sens_set = prepare_sensitivity(flood_data_matrix=flood_data_array, flood_sigma_matrix=flood_sigma_array,
-                                   monitor_counts=np.array([1.] * num_ws_pairs),
-                                   threshold_min=0, threshold_max=1000000)
+                                   monitor_counts=monitor_array,
+                                   threshold_min=0.5, threshold_max=1.5)
 
     # Export sensitivities calculated in file to ....
     sensitivities, sensitivities_error = sens_set
+
     print(sensitivities)
     print(sensitivities_error)
-    print(len(np.where(np.isnan(sensitivities))))
-    print(len(np.where(np.isinf(sensitivities))))
+    print('Number of NaN: {}'.format(len(np.where(np.isnan(sensitivities))[0])))
+    print('Number of Infinity: {}'.format(len(np.where(np.isinf(sensitivities))[0])))
+    print(sensitivities.shape)
+    # Export 2D view
+    export_detector_view(sensitivities, 'Sensitivity.png')
+    # Export to hdf5
+    # sens_h5 = h5py.File('{}.h5'.format(output_file.split('.')[0]), 'w')
+    sens_group = sens_h5.create_group('Sensitivities')
+    sens_group.create_dataset('sensitivities', data=sensitivities)
+    sens_group.create_dataset('sensitivities error', data=sensitivities_error)
+    sens_h5.close()
+
+    return
 
 
 def generate_test_json():
@@ -228,8 +273,11 @@ def generate_test_json():
 
     """
     # Set up the dictionary for JSON
-    json_dict = {'flood files': [585, 587, 589],
-                 'direct beam files': [586, 588, 590],
+
+    json_dict = {'IPTS-Number': 24664,
+                 'Flood Runs': [1697, 1699, 1701],
+                 'Direct Beam Runs': [1698, 1700, 1702],
+                 'Monitor': [1., 1., 1],
                  'output_file_name': 'test_gp_sensitivity.nxs'}
     json_str = json.dumps(json_dict)
 

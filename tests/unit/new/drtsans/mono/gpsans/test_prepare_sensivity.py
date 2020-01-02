43f426404e295a0495a62fe0e948c3713b0c059b
Wenduo Zhou <zhouw@ornl.gov>
2020-01-02 17:31:15 -0500

    Refs #205. Looked same.
    Former-commit-id: 8fce3c532c16e7633b19cad069f3fb59f7b81511

--- tests/unit/new/drtsans/mono/gpsans/test_prepare_sensivity.py
+++ tests/unit/new/drtsans/mono/gpsans/test_prepare_sensivity.py
@@ -497,6 +497,7 @@ def test_prepare_moving_det_sensitivity():
     gold_final_sen_matrix, gold_final_sigma_matrix = get_final_sensitivities()
 
     # verify that the refactored high level method renders the same result from prototype
+    print('Difference = {}'.format(np.sqrt(np.sum(gold_final_sen_matrix.flatten() - test_sens_array)**2)))
     np.testing.assert_allclose(gold_final_sen_matrix.flatten(), test_sens_array, rtol=1e-3, atol=1e-3)
     np.testing.assert_allclose(gold_final_sigma_matrix.flatten(), test_sens_sigma_array, rtol=1e-3, atol=1e-3)
 

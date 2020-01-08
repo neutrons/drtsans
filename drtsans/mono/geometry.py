79246d55a70e1f0853f31787aec39fb7df8b7ba6
Jose Borreguero <borreguero@gmail.com>
2020-01-08 14:33:00 -0500

    Refs #312 refactor function beam_radius
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: b3d52b237750504ebedac24a73ad12d7432d188a

--- drtsans/mono/geometry.py
+++ drtsans/mono/geometry.py
@@ -1,46 +1,37 @@
 from mantid import mtd, logger
-
+from drtsans.geometry import (sample_aperture_diameter, sample_detector_distance, source_aperture_diameter,
+                              source_sample_distance)
 
 __all__ = ['beam_radius']
 
 
-def beam_radius(input_workspace, unit='mm',
-                sample_aperture_diameter_log='sample_aperture_diameter',
-                source_aperture_diameter_log='source_aperture_diameter',
-                sdd_log='sample-detector-distance',
-                ssd_log='source-sample-distance'):
+def beam_radius(input_workspace, unit='mm'):
     """
-    Calculate the radius in mm according to:
-    R_beam = R_sampleAp + SDD * (R_sampleAp + R_sourceAp) / SSD
+    Calculate the beam radius impinging on the detector
+
+    R_beam = R_sampleAp + SDD * (R_sampleAp + R_sourceAp) / SSD, where
+    R_sampleAp: radius of the sample aperture,
+    SDD: distance between the sample and the detector,
+    R_sourceAp: radius of the source aperture,
+    SSD: distance between the source and the sample.
 
     Parameters
     ----------
-    input_workspace: MatrixWorkspace, str
+    input_workspace: ~mantid.api.MatrixWorkspace, str
         Input workspace
     unit: str
         Units of the output beam radius. Either 'mm' or 'm'.
-    sample_aperture_diameter_log: str
-        Log entry for the sample_aperture diameter
-    source_aperture_diameter_log: str
-        Log entry for the source_aperture diameter
-    sdd_log: str
-        Log entry for the sample to detector distance
-    ssd_log: str
-        Log entry for the source_aperture to sample distance
 
     Returns
     -------
     float
     """
-    from drtsans.tof.eqsans.geometry import beam_radius as eqsans_beam_radius
-    from drtsans.mono.momentum_transfer import retrieve_instrument_setup
-    ws = mtd[str(input_workspace)]
-    if ws.getInstrument().getName() == 'EQ-SANS':
-        return eqsans_beam_radius(ws, unit='mm')
+    r_sa = sample_aperture_diameter(input_workspace, unit='m') / 2.0  # radius
+    r_so = source_aperture_diameter(input_workspace, unit='m') / 2.0  # radius
+    l1 = source_sample_distance(input_workspace, unit='m')
+    l2 = sample_detector_distance(input_workspace, unit='m')
 
-    inst = retrieve_instrument_setup(ws)
-    radius = inst.sample_aperture_radius + inst.sample_det_center_distance * (inst.sample_aperture_radius
-                                                                              + inst.source_aperture_radius) / inst.l1
+    radius = r_sa + (r_sa + r_so) * (l2 / l1)
 
     logger.notice("Radius calculated from the input workspace = {:.2} mm".format(radius * 1e3))
     return radius if unit == 'm' else 1e3 * radius

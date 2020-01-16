ab58e5da2e8817d0f1a4b58387a91641b0fc25f0
Wenduo Zhou <zhouw@ornl.gov>
2020-01-16 10:54:35 -0500

    Refs #316. Checkpointing progress.
    Former-commit-id: fdab95a3f15bf958d7129ce6959740686ee23419

--- scripts/prepare_sensitivities.py
+++ scripts/prepare_sensitivities.py
@@ -14,7 +14,7 @@ from drtsans.settings import unique_workspace_dundername as uwd  # noqa E402
 INSTRUMENT = 'GPSANS'  # From 'EQSANS', 'BIOSANS'
 
 IPTS = 24664
-FLOOD_RUNS = [1697, 1701, 1699]
+FLOOD_RUNS = [1697, 1701, 1699]  # Single value integer or a list or tuple
 
 DIRECT_BEAM_RUNS = [1698, 1702, 1700]
 MASK_BEAM_CENTER_RADIUS = 0.5  # mm
@@ -29,6 +29,24 @@ MOVING_DETECTORS = True
 # Normalize data
 NORMALIZE_COUNTS = True  # BIO/EQ-SANS may not, But GPSANS does regardless
 
+# END OF USER INPUTS
+
+def prepare_data():
+    from drtsans.mono.gpsans.api import prepare_data
+    from drtsans.mono.biosans.api import prepare_data
+    from drtsans.tof.eqsans.api import prepare_data
+
+    prepare_data(mask=UNIVERSAL_MASK)
+
+
+
+
+if INSTRUMENT.lower() == 'gpsans' and MOVING_DETECTORS is True:
+    # Use the moving detector algorithm
+    pass
+else:
+    # Use the sensitivity patch method
+    pass
 #
 prepare_data()
 

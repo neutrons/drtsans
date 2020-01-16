6f253d1ecb422d513be0fa9152f992428029b7d7
Wenduo Zhou <zhouw@ornl.gov>
2020-01-16 09:15:59 -0500

    Refs #316. Checkpointing progress.
    Former-commit-id: bfea84439bc5958bcb1224a162d638c6888bb3e3

--- scripts/prepare_sensitivities.py
+++ scripts/prepare_sensitivities.py
@@ -11,10 +11,30 @@ import drtsans  # noqa E402
 from drtsans.mono import gpsans as sans  # noqa E402
 from drtsans.settings import unique_workspace_dundername as uwd  # noqa E402
 
-INSTRUMENT = 'GPSANS'
+INSTRUMENT = 'GPSANS'  # From 'EQSANS', 'BIOSANS'
+
+IPTS = 24664
+FLOOD_RUNS = [1697, 1701, 1699]
+
+DIRECT_BEAM_RUNS = [1698, 1702, 1700]
+MASK_BEAM_CENTER_RADIUS = 0.5  # mm
+
+BEAM_CENTER_MASKS = None
+
+UNIVERSAL_MASK = 'Mask.XML'
 
 # If it is GPSANS, there could be 2 options
 MOVING_DETECTORS = True
+
+# Normalize data
+NORMALIZE_COUNTS = True  # BIO/EQ-SANS may not, But GPSANS does regardless
+
+#
+prepare_data()
+
+calculate_sensitivities()
+
+export()
 
 
 def main():

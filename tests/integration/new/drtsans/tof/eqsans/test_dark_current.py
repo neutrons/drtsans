5c38badeb9d77fe62dbf1b990405df5564ceb68a
Savici, Andrei T <saviciat@ornl.gov>
2020-02-10 14:59:10 -0500

    Add test for zero dark current
    Former-commit-id: a102b6ee02a762cc58f3d5bd04f9e2ec5a8557df

--- tests/integration/new/drtsans/tof/eqsans/test_dark_current.py
+++ tests/integration/new/drtsans/tof/eqsans/test_dark_current.py
@@ -19,6 +19,7 @@ def data_test_16a():
     <https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/issues/174#dark-current-normalized-at-eq-sans>
     """
     l_min, l_step, l_max = 2.5, 0.1, 6.0
+    f = 0.0006746  # sample_run_duration*(t_frame-t_low-t_high)*lstep/(dark_current_duration*t_frame*(l_max-l_min))
     return dict(t_frame=1. / 60. * 1000000.,  # time duration of a frame, in microseconds
                 t_low=500,  # sample measurement tof range, in microseconds
                 t_high=2000,
@@ -27,34 +28,40 @@ def data_test_16a():
                 l_min=l_min,  # minimum wavelength
                 l_max=l_max,
                 l_step=l_step,  # wavelength bin width
-                n_pixels=25,  # 25 pixels in the detector
+                n_pixels=30,  # 30 pixels in the detector
+
                 # dark current intensities and errors
                 I_dc=[[40., 45., 50., 55., 60.],  # intensity per pixel, integrated over all wavelength bins
                       [65., 70., 75., 80., 85.],
                       [90., 95., 100., 105., 110.],
                       [115., 120., 125., 130., 135.],
-                      [140., 145., 150., 155., 160.]],
+                      [140., 145., 150., 155., 160.],
+                      [0., 0., 0., 0., 0.]],
                 I_dc_err=[[6.3246,  6.7082,  7.0711,  7.4162,  7.746],
                           [8.0623,  8.3666,  8.6603,  8.9443,  9.2195],
                           [9.4868,  9.7468, 10., 10.247, 10.4881],
                           [10.7238, 10.9545, 11.1803, 11.4018, 11.619],
-                          [11.8322, 12.0416, 12.2474, 12.4499, 12.6491]],
+                          [11.8322, 12.0416, 12.2474, 12.4499, 12.6491],
+                          [0., 0., 0., 0., 0.]],
                 # sample data intensities and errors. Numers are selected in such a way that subtracting the dark
                 # current will yield no intensities.
                 I_data=[[0.9444, 1.0625, 1.1806, 1.2986, 1.4167],  # intensity per pixel, integrated over all
                         [1.5347, 1.6528, 1.7708, 1.8889, 2.0069],  # wavelength bins
                         [2.125, 2.2431, 2.3611, 2.4792, 2.5972],
                         [2.7153, 2.8333, 2.9514, 3.0694, 3.1875],
-                        [3.3056, 3.4236, 3.5417, 3.6597, 3.7778]],
+                        [3.3056, 3.4236, 3.5417, 3.6597, 3.7778],
+                        [0., 0., 0., 0., 0.]],
                 I_data_err=[[0.0252, 0.0268, 0.0282, 0.0296, 0.0309],
                             [0.0322, 0.0334, 0.0346, 0.0357, 0.0368],
                             [0.0379, 0.0389, 0.0399, 0.0409, 0.0419],
                             [0.0428, 0.0437, 0.0446, 0.0455, 0.0464],
-                            [0.0472, 0.0481, 0.0489, 0.0497, 0.0505]],
+                            [0.0472, 0.0481, 0.0489, 0.0497, 0.0505],
+                            [0., 0., 0., 0., 0.]],
                 # Sample data normalized by the dark current. As stated before, the sample data was selected such
                 # that the normalization will yield no intensities.
                 I_data_norm=[[0., 0., 0., 0., 0.],  # data minus normalized dark current
                              [0., 0., 0., 0., 0.],  # is all zeroes by construction
+                             [0., 0., 0., 0., 0.],
                              [0., 0., 0., 0., 0.],
                              [0., 0., 0., 0., 0.],
                              [0., 0., 0., 0., 0.]],
@@ -62,7 +69,8 @@ def data_test_16a():
                                  [0.0013, 0.0013, 0.0014, 0.0014, 0.0015],
                                  [0.0015, 0.0016, 0.0016, 0.0017, 0.0017],
                                  [0.0017, 0.0018, 0.0018, 0.0018, 0.0019],
-                                 [0.0019, 0.0019, 0.002, 0.002, 0.002]],
+                                 [0.0019, 0.0019, 0.002, 0.002, 0.002],
+                                 [f, f, f, f, f]],
                 precision=1.e-4  # precision to compare reduction framework to test results
                 )
 
@@ -155,6 +163,7 @@ def test_subtract_dark_current(data_test_16a):
     computed_errors = np.transpose(mtd[data_workspace].extractE())[0].ravel()
     test_errors = np.array(data_test_16a['I_data_norm_err']).ravel()
     assert computed_errors == pytest.approx(test_errors, abs=data_test_16a['precision'])
+    assert computed_errors[-1] == pytest.approx(test_errors[-1], abs=data_test_16a['precision'] * 0.01)
 
 
 if __name__ == '__main__':

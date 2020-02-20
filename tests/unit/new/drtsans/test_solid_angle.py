2f2893c92710ba8af50d7e7ca4cc27755c2f4214
Pete Peterson <petersonpf@ornl.gov>
2020-02-20 13:33:51 -0500

    Extract calculating the solid angle into a separate function
    Former-commit-id: 23c7830b3c28b2bb9f83e942b0e2cb3316efaad5

--- tests/unit/new/drtsans/test_solid_angle.py
+++ tests/unit/new/drtsans/test_solid_angle.py
@@ -1,9 +1,10 @@
+import numpy as np
 from os.path import join
 import pytest
 from mantid.simpleapi import (CompareWorkspaces, Load,
                               LoadEmptyInstrument,
                               MoveInstrumentComponent)
-from drtsans import solid_angle_correction
+from drtsans import calculate_solid_angle, solid_angle_correction
 
 
 def test_solid_angle(reference_dir):
@@ -60,6 +61,23 @@ def test_solid_angle_input_output(reference_dir):
     reference_workspace = Load(Filename=join(reference_dir.new.eqsans,
                                              'test_solid_angle.nxs'))
     assert CompareWorkspaces(wsInput, reference_workspace)
+
+
+@pytest.mark.parametrize('instrument, num_monitor',
+                         [('BIOSANS', 2), ('GPSANS', 2), ('EQSANS', 1)],
+                         ids=['BIOSANS', 'GPSANS', 'EQSANS'])
+def test_solid_angle_calculation(instrument, num_monitor):
+    ws = LoadEmptyInstrument(InstrumentName=instrument)
+    ws = calculate_solid_angle(ws)
+
+    values = ws.extractY()
+    # monitors have zero solid angle
+    np.testing.assert_equal(values[:1], 0.)
+    # everything other than monitors should have a positive solid angle
+    assert np.all(values[num_monitor:] > 0.)
+
+    # cleanup
+    ws.delete()
 
 
 if __name__ == '__main__':

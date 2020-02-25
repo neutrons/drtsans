6d43e46fa0902e9e020e38c54d40a82f6a69c8e5
Pete Peterson <petersonpf@ornl.gov>
2020-02-25 14:35:56 -0500

    Allow test to fail
    Former-commit-id: ade558861cbe3b19e5e5530ac1588f470751b941

--- tests/unit/new/test_mounts.py
+++ tests/unit/new/test_mounts.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.mark.parametrize('root',
                          ['/HFIR/CG3', '/SNS/EQSANS', '/HFIR/CG2'],
                          ids=['BIOSANS', 'EQSANS', 'GPSANS'])
+@pytest.mark.xfail(reason='HFIR mounts are not stable yet', strict=False)
 def test_mounts(root):
     path = os.path.join(root, 'shared')
     assert os.path.isdir(path)

12933fc47176735867ef74b5794544bba5c9801a
Ross Whitfield <whitfieldre@ornl.gov>
2020-02-25 13:32:17 -0500

    Update mounts for HFIR
    Signed-off-by: Ross Whitfield <whitfieldre@ornl.gov>
    
    Former-commit-id: f1dfbc453fa47c6a9ba6dd5943a81e4884b641f4

--- scripts/eqsans-mount.sh
+++ scripts/eqsans-mount.sh
@@ -5,8 +5,8 @@ date +%F-%T
 hostname -A
 hostname -I
 declare COUNT=0
-if ! grep -qs 'EQSANS' /proc/mounts; then
-  until mount -t nfs -o "tcp,ro,noatime,rsize=32768,wsize=32768" snsdata.ornl.gov:/stornext/snfs1/instruments/EQSANS /SNS/EQSANS; do
+if ! grep -qs 'SNS' /proc/mounts; then
+  until mount -t nfs -o "tcp,ro,noatime,rsize=32768,wsize=32768" snsdata.ornl.gov:/stornext/snfs1/instruments /SNS; do
     sleep 10
     COUNT=$((COUNT + 1))
     if [[ ${COUNT} == 5 ]]; then
@@ -14,4 +14,16 @@ if ! grep -qs 'EQSANS' /proc/mounts; then
     fi
   done
 fi
-ls /SNS/EQSANS
+ls /SNS
+
+declare COUNT=0
+if ! grep -qs 'HFIR' /proc/mounts; then
+  until mount -t nfs -o "tcp,ro,noatime,rsize=32768,wsize=32768" hfir-storage.ornl.gov:/volumes/datapool/instruments /SNS; do
+    sleep 10
+    COUNT=$((COUNT + 1))
+    if [[ ${COUNT} == 5 ]]; then
+      sudo reboot
+    fi
+  done
+fi
+ls /HFIR

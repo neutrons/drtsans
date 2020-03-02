f2304176741fdc7eb007fe19e1110045cc2a482a
Steven Hahn <hahnse@ornl.gov>
2020-03-02 13:12:48 -0500

    Refs #403. Script runs without /HFIR mount.
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>
    
    Former-commit-id: 76cd3c1c04addc2a2bf150d5b6b34e612b3fab86

--- scripts/biosans_reduction.py
+++ scripts/biosans_reduction.py
@@ -86,27 +86,27 @@ def reduction(json_params, config):
 
     # Load and prepare scattering data
     # all the run numbers are associated with instrument name
-    ws = sans.prepare_data(json_params["instrumentName"] + json_params["runNumber"],
+    ws = sans.prepare_data(json_params["runNumber"],
                            output_suffix=wksp_suffix('_data', config), **config)
 
     # Transmission
     transmission_run = json_params["transmission"]["runNumber"]
     if transmission_run.strip() != '':
-        transmission_fn = json_params["instrumentName"] + "_" + json_params["transmission"]["runNumber"]
-        empty_run = json_params["instrumentName"] + "_" + json_params["empty"]["runNumber"]
+        transmission_fn = json_params["transmission"]["runNumber"]
+        empty_run = json_params["empty"]["runNumber"]
         ws = apply_transmission(ws, transmission_fn, empty_run, config)
 
     # Background
     bkg_run = json_params["background"]["runNumber"]
     if bkg_run != "":
-        bkg_run = json_params["instrumentName"] + bkg_run
+        bkg_run = bkg_run
         ws_bck = sans.prepare_data(bkg_run, output_suffix=wksp_suffix('_bkg', config), **config)
 
         # Background transmission
         transmission_run = json_params["background"]["transmission"]["runNumber"]
         if transmission_run.strip() != '':
-            transmission_fn = json_params["instrumentName"] + "_" + transmission_run
-            empty_run = json_params["instrumentName"] + "_" + json_params["empty"]["runNumber"]
+            transmission_fn = transmission_run
+            empty_run = json_params["empty"]["runNumber"]
             ws_bck = apply_transmission(ws_bck, transmission_fn, empty_run, config)
 
         # Subtract background
@@ -169,7 +169,7 @@ if __name__ == "__main__":
 
     # Find the beam center
     # TODO: We need a way to pass a pre-calculated beam center
-    empty_run = json_params["instrumentName"] + "_" + json_params["empty"]["runNumber"]
+    empty_run = json_params["empty"]["runNumber"]
     if empty_run != "":
         # Load and compute beam center position
         db_ws = sans.load_events(empty_run,

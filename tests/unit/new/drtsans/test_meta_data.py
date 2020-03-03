bc0ed736b3b6b1ac52053e38dccb98ca3080fc6d
Wenduo Zhou <zhouw@ornl.gov>
2020-03-03 09:01:06 -0500

    Refs #400. Added unit test.
    Former-commit-id: e1eedf0e947a9557dd48130c8bda0ac16391c504

--- tests/unit/new/drtsans/test_meta_data.py
+++ tests/unit/new/drtsans/test_meta_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from drtsans.mono.meta_data import set_meta_data as mono_set_meta_data
 from drtsans.tof.eqsans.meta_data import set_meta_data as eqsans_set_meta_data
 from drtsans.geometry import sample_aperture_diameter, source_aperture_diameter, sample_detector_distance,\
-    source_sample_distance
+    source_sample_distance, pixel_size
 
 
 @pytest.mark.parametrize('workspace_with_instrument', [{'Nx': 3, 'Ny': 3}], indirect=True)
@@ -35,6 +35,11 @@ def test_set_mono_meta_data(workspace_with_instrument):
 
     test_sdd = sample_detector_distance(data_ws, 'm')
     assert test_sdd == 16.2
+
+    # verify pixel size
+    test_ps_x, test_ps_y = pixel_size(data_ws)
+    assert test_ps_x == 0.0021
+    assert test_ps_y == 0.0022
 
 
 @pytest.mark.parametrize('workspace_with_instrument', [{'Nx': 3, 'Ny': 3}], indirect=True)

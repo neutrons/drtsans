908423938ba94d378a64293b03a6fa57c5a2b6a5
Jose Borreguero <borreguero@gmail.com>
2020-03-10 17:31:45 -0400

    Refs #244 unit test for source_aperture
    Former-commit-id: aff0bd61cbd7c1fccf19e2ac6468e7005c07cf1d

--- tests/unit/new/drtsans/tof/eqsans/test_geometry.py
+++ tests/unit/new/drtsans/tof/eqsans/test_geometry.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import os
 import pytest
 r"""
@@ -7,7 +8,7 @@ LoadInstrument <https://docs.mantidproject.org/nightly/algorithms/LoadInstrument
 from mantid.simpleapi import LoadInstrument
 
 from drtsans.geometry import main_detector_panel
-from drtsans.tof.eqsans.geometry import (detector_id, pixel_coordinates, sample_aperture_diameter,
+from drtsans.tof.eqsans.geometry import (detector_id, pixel_coordinates, sample_aperture_diameter, source_aperture,
                                          source_aperture_diameter, source_monitor_distance, translate_detector_by_z)
 from drtsans.samplelogs import SampleLogs
 
@@ -37,6 +38,39 @@ def test_sample_aperture_diameter(serve_events_workspace):
     sad = SampleLogs(ws).single_value('sample_aperture_diameter')
     # ISSUE1887 TODO assert sad == approx(10)
     assert sad > 0
+
+
+# Test function source_aperture() with three different aperture settings for the three
+# slits [(1, 7, 4,), (4, 1, 6), (6, 3, 3)] and for two different run numbers [9999, 10000].
+# Compare the diameter and Aperture-Sample-Distance (asd) from running source_aperture() against the values
+# stored here
+DataSourceAperture = namedtuple('DataSourceAperture', 'run_number vBeamSlit vBeamSlit2 vBeamSlit3 diameter asd')
+data_source_aperture = [DataSourceAperture(9999, 1, 7, 4, 0.005, 4.042),
+                        DataSourceAperture(10000, 1, 7, 4, 0.005, 4.042),
+                        DataSourceAperture(9999, 4, 1, 6, 0.0, 2.966),
+                        DataSourceAperture(10000, 4, 1, 6, 0.0, 2.966),
+                        DataSourceAperture(9999, 6, 3, 3, 0.01, 2.966),
+                        DataSourceAperture(10000, 6, 3, 3, 0.01, 2.966)]
+
+
+@pytest.mark.parametrize('data', data_source_aperture)
+@pytest.mark.parametrize('generic_workspace', [{'name': 'EQ-SANS', 'l1': -14.122}], indirect=True)
+def test_source_aperture(generic_workspace, data):
+    r"""
+    Test function source_aperture for different aperture settings and run numbers.
+    Use a mock EQ-SANS instrument with the moderator 14.122 meters away from the sample.
+    """
+    workspace = generic_workspace
+    sample_logs = SampleLogs(workspace)
+    sample_logs.insert('run_number', data.run_number)
+    for log_key in ['vBeamSlit', 'vBeamSlit2', 'vBeamSlit3']:
+        data_index = data._fields.index(log_key)  # which item in object `data` stores info for this particular slit?
+        diameter_index = data[data_index]  # diameter index for the particular slit
+        times = [0.0, 3600]  # the run started at time 0.0 and ended after one hour, here in seconds
+        sample_logs.insert_time_series(log_key, times, [diameter_index, diameter_index])
+    result = source_aperture(workspace)
+    assert result.diameter == pytest.approx(data.diameter, abs=1.e-05)
+    assert result.distance_to_sample == pytest.approx(data.asd, abs=1.e-05)
 
 
 def test_source_aperture_diameter(serve_events_workspace):

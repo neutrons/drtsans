b47b097e044d47a30a1ccfd8e70851ccf7adf716
Wenduo Zhou <zhouw@ornl.gov>
2020-03-18 11:32:21 -0400

    Refs #435. Expanded the integration test for BIO-SANS (2).
    Former-commit-id: f7018e4131802b87e3a3c9c38ce28a6cb62f1bd7

--- tests/integration/new/drtsans/mono/test_load.py
+++ tests/integration/new/drtsans/mono/test_load.py
@@ -1,7 +1,7 @@
 # Test load GPSANS and BIOSANS data
 import pytest
 import os
-# import numpy as np
+import numpy as np
 from drtsans.mono.load import load_events
 from drtsans.mono.meta_data import get_sample_detector_offset
 from drtsans.samplelogs import SampleLogs
@@ -70,9 +70,55 @@ def test_load_biosans():
 
     # Check current instrument setup and meta data (sample logs)
     logs = SampleLogs(ws)
-    print('[TEST INFO] SampleToSi = {} mm'.format(logs.find_log_with_units('CG3:CS:SampleToSi', unit='mm')))
+    print('[TEST INFO] (Raw) sampleToSi = {} mm'.format(logs.find_log_with_units('CG3:CS:SampleToSi', unit='mm')))
     raw_sample_det_distance = sample_detector_distance(ws)
-    print('[TEST INFO] Sample to detector distance = {} /{} meter'
+    print('[TEST INFO] (Raw) sample to detector distance = {} /{} meter'
+          ''.format(raw_sample_det_distance,
+                    sample_detector_distance(ws, log_key='sample_detector_distance', search_logs=True)))
+
+    # Calculate offset without any overwriting
+    # sample and detector offsets can only be retrieved from a loaded workspace
+    # This is a technical debt
+    sample_offset, detector_offset = get_sample_detector_offset(ws, 'CG3:CS:SampleToSi', 71. * 1E-3)
+    print('[TEST INFO] Sample offset = {}, Detector offset = {}'
+          ''.format(sample_offset, detector_offset))
+
+    # Verify: No sample offset from nominal position (origin)
+    assert sample_offset == pytest.approx(0., 1E-12)
+    # Verify: No sample offset
+    assert detector_offset == pytest.approx(0., 1E-12)
+    # Verify: sample position at (0., 0., 0.)
+    sample_pos = np.array(ws.getInstrument().getSample().getPos())
+    expected_sample_pos = np.array([0., 0., 0.])
+    np.testing.assert_allclose(sample_pos, expected_sample_pos, atol=1E-12)
+    # Verify: sample detector distance is equal to 7.00000019 meter
+    sample_det_distance_cal = sample_detector_distance(ws, unit='m', search_logs=False)
+    sample_det_distance_meta = sample_detector_distance(ws, unit='mm', search_logs=True)
+    assert sample_det_distance_cal == pytest.approx(sample_det_distance_meta * 1E-3, 1E-7)
+    assert sample_det_distance_cal == pytest.approx(7.00000019, 1E-7)
+
+
+def test_load_biosans_sample_off():
+    """Test load BIOSANS data
+
+    Returns
+    -------
+
+    """
+    # Decide to skip data or not
+    nexus_file_name = '/HFIR/CG3/IPTS-23782/nexus/CG3_4829.nxs.h5'
+    if not os.path.exists(nexus_file_name):
+        pytest.skip('Skip due to NeXus file {} is not accessible.'.format(nexus_file_name))
+
+    # Load data
+    ws = load_events(nexus_file_name, output_workspace='biotest01', overwrite_instrument=True,
+                     detector_offset=0, sample_offset=0)
+
+    # Check current instrument setup and meta data (sample logs)
+    logs = SampleLogs(ws)
+    print('[TEST INFO] (Raw) sampleToSi = {} mm'.format(logs.find_log_with_units('CG3:CS:SampleToSi', unit='mm')))
+    raw_sample_det_distance = sample_detector_distance(ws)
+    print('[TEST INFO] (Raw) sample to detector distance = {} /{} meter'
           ''.format(raw_sample_det_distance,
                     sample_detector_distance(ws, log_key='sample_detector_distance', search_logs=True)))
 

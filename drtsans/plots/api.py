d82413f4fb99ffd55ba086fb768586110ea5ce00
Jose Borreguero <borreguero@gmail.com>
2020-03-18 10:36:03 -0400

    Refs #476 flaky flake8
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: de3c9074028c2b4b81721372d5a2206bdb2fe174

--- drtsans/plots/api.py
+++ drtsans/plots/api.py
@@ -267,7 +267,7 @@ def plot_IQazimuthal(workspace, filename, backend='d3',
 
 
 def plot_detector(input_workspace, filename=None, backend='d3', axes_mode='tube-pixel',
-                  panel_name=None, imshow_kwargs={'norm': LogNorm(vmin=1)}):
+                  panel_name=None, imshow_kwargs={'norm': LogNorm(vmin = 1)}):
     r"""
     Save a 2D plot representative of the supplied workspace
 

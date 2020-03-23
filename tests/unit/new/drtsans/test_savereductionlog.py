e9ee8efa36adf82253897211effb41013d903bb6
JeanBilheux <bilheuxjm@ornl.gov>
2020-03-23 15:29:04 -0400

    test writing iq and iqxqy. this refs #487
    Former-commit-id: 0e61d2b379cb37610168fc773ea92626084c36ef

--- tests/unit/new/drtsans/test_savereductionlog.py
+++ tests/unit/new/drtsans/test_savereductionlog.py
@@ -365,69 +365,69 @@ def test_writing_iqxqy():
         data = iqxqy_nxdata['Qydev'][:]
         _test_data(tested_data=data,
                    ref_data=np.array([0.008423, 0.008423]))
-#
-#
-# def test_writing_iq_and_iqxqy_scalar_mode():
-#     test_iq = [_create_iq()]
-#     test_iqxqy = _create_iqxqy()
-#     tmp_log_filename = _create_tmp_log_filename()
-#     savereductionlog(tmp_log_filename, detectordata={'main_detector': {'iq': test_iq,
-#                                                                        'iqxqy': test_iqxqy}})
-#
-#     assert os.path.exists(tmp_log_filename), 'log file {} does not exist'.format(tmp_log_filename)
-#
-#     with h5py.File(tmp_log_filename, 'r') as handle:
-#         top_group = _getGroup(handle, 'main_detector', 'NXdata')
-#
-#         iq_nxdata = _getGroup(top_group, 'I(Q)', 'NXdata')
-#
-#         data = iq_nxdata['I'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([93, 60]))
-#
-#         data = iq_nxdata['Idev'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([9.64365076, 7.74596669]),
-#                    abs=1e-7)
-#
-#         data = iq_nxdata['Q'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([0.0078897, 0.0059338]),
-#                    abs=1e-7)
-#
-#         data = iq_nxdata['Qdev'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([0.011912, 0.11912]),
-#                    abs=1e-6)
-#
-#         iqxqy_nxdata = _getGroup(top_group, 'I(QxQy)', 'NXdata')
-#
-#         data = iqxqy_nxdata['I'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([93, 60]))
-#
-#         data = iqxqy_nxdata['Idev'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([9.64365076, 7.74596669]),
-#                    abs=1e-8)
-#
-#         data = iqxqy_nxdata['Qx'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([-0.006134, -0.003254]),
-#                    abs=1e-6)
-#
-#         data = iqxqy_nxdata['Qxdev'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([0.008423, 0.008423]),
-#                    abs=1e-6)
-#
-#         data = iqxqy_nxdata['Qy'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([0.004962, 0.004962]))
-#
-#         data = iqxqy_nxdata['Qydev'][:]
-#         _test_data(tested_data=data,
-#                    ref_data=np.array([0.008423, 0.008423]))
+
+
+def test_writing_iq_and_iqxqy_scalar_mode():
+    test_iq = [_create_iq()]
+    test_iqxqy = _create_iqxqy()
+    tmp_log_filename = _create_tmp_log_filename()
+    savereductionlog(tmp_log_filename, detectordata={'slice_1': { 'main_detector': {'iq': test_iq,
+                                                                                  'iqxqy': test_iqxqy}}})
+
+    assert os.path.exists(tmp_log_filename), 'log file {} does not exist'.format(tmp_log_filename)
+
+    with h5py.File(tmp_log_filename, 'r') as handle:
+        top_group = _getGroup(handle, 'slice_1', 'NXdata')
+        mid_group = _getGroup(top_group, 'main_detector', 'NXdata')
+        iq_nxdata = _getGroup(mid_group, 'I(Q)', 'NXdata')
+
+        data = iq_nxdata['I'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([93, 60]))
+
+        data = iq_nxdata['Idev'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([9.64365076, 7.74596669]),
+                   abs=1e-7)
+
+        data = iq_nxdata['Q'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([0.0078897, 0.0059338]),
+                   abs=1e-7)
+
+        data = iq_nxdata['Qdev'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([0.011912, 0.11912]),
+                   abs=1e-6)
+
+        iqxqy_nxdata = _getGroup(mid_group, 'I(QxQy)', 'NXdata')
+
+        data = iqxqy_nxdata['I'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([93, 60]))
+
+        data = iqxqy_nxdata['Idev'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([9.64365076, 7.74596669]),
+                   abs=1e-8)
+
+        data = iqxqy_nxdata['Qx'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([-0.006134, -0.003254]),
+                   abs=1e-6)
+
+        data = iqxqy_nxdata['Qxdev'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([0.008423, 0.008423]),
+                   abs=1e-6)
+
+        data = iqxqy_nxdata['Qy'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([0.004962, 0.004962]))
+
+        data = iqxqy_nxdata['Qydev'][:]
+        _test_data(tested_data=data,
+                   ref_data=np.array([0.008423, 0.008423]))
 #
 #
 # def test_writing_iq_and_iqxqy_wedge_mode():

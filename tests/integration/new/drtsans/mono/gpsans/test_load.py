d604752310e6ce2ef9891e495a22f7bc1926863b
Wenduo Zhou <zhouw@ornl.gov>
2020-04-21 20:47:19 -0400

    Refs #542. Check progress to implement integration test for load_all_files.
    Former-commit-id: 732d8fd5765a7ca8a90ca5654c868559d1b90e3b

--- tests/integration/new/drtsans/mono/gpsans/test_load.py
+++ tests/integration/new/drtsans/mono/gpsans/test_load.py
@@ -1,16 +1,55 @@
-# Test gpsans.api.load_all_files()
-# ... ...
-# ... ...
+import pytest
+import json
+from mantid.simpleapi import mtd
 from drtsans.mono.gpsans import load_all_files
-from drtsans.mono.gpsans import load_and_split
+# from drtsans.mono.gpsans import load_and_split
 
 
-def test_load_all_files():
+def gpsans_load_all_files(data_dir, json_file, output_dir):
+    """Standard reduction workflow
 
-    load_all_files(reduction_input=balbla,
-                   prefix='TestCase1',
-                   load_params=None,
-                   path=Whatever)
+    Parameters
+    ----------'
+    data_dir
+    json_file
+    output_dir
+
+    Returns
+    -------
+
+    """
+    # USER Input here with scan numbers etc.
+    samples = ['9166']
+    samples_trans = ['9178']
+    sample_thick = ['0.1']
+    bkgd = ['9165']
+    bkgd_trans = ['9177']
+
+    # Sample names for output
+    sample_names = ["Al4"]
+
+    # Import JSON
+    with open(json_file) as f:
+        reduction_input = json.load(f)
+
+    # set output directory
+    reduction_input["configuration"]["outputDir"] = output_dir
+
+    reduction_input["runNumber"] = samples[0]
+    reduction_input["transmission"]["runNumber"] = samples_trans[0]
+    reduction_input["background"]["runNumber"] = bkgd[0]
+    reduction_input["background"]["transmission"]["runNumber"] = bkgd_trans[0]
+    reduction_input["outputFilename"] = sample_names[0]
+    reduction_input["thickness"] = sample_thick[0]
+    loaded = load_all_files(reduction_input,
+                            prefix='GP_TEST_LOAD',
+                            load_params=None,
+                            path=data_dir)
+    print('Type of loaded = {}'.format(loaded))
 
     for ws_name in mtd.getObjectNames():
         print(ws_name)
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])

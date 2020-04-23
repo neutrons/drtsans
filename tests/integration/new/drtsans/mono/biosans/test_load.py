ffa7e9bb36eb8fac95eb9d21d2ce16cd0d470564
Wenduo Zhou <zhouw@ornl.gov>
2020-04-22 21:55:59 -0400

    Refs #542. Fixed BIOSANS load integration test.
    Former-commit-id: 95e95f0d109c1b9097385ef9f93898480642fab4

--- tests/integration/new/drtsans/mono/biosans/test_load.py
+++ tests/integration/new/drtsans/mono/biosans/test_load.py
@@ -46,34 +46,34 @@ def test_load_all_files(reference_dir):
     load_all_files(reduction_input, path=nexus_dir, prefix='BioTestLoadAll')
 
     beam_center_run = mtd['BioTestLoadAll_CG3_1322_raw_histo']
-    dark_main_run = mtd['BioTestLoadAll_CG3_1383_raw_histo']
-    dark_wing_run = mtd['BioTestLoadAll_CG3_5705_raw_histo']
+    dark_run = mtd['BioTestLoadAll_CG3_1383_raw_histo']
+    empty_trans_run = mtd['BioTestLoadAll_CG3_5705_raw_histo']
     sample_run = mtd['BioTestLoadAll_CG3_5709_raw_histo']
     bkgd_run = mtd['BioTestLoadAll_CG3_5715_raw_histo']
 
     # Verify sample to si-window distance by checking the sample position with default setup
     # https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/issues/542#note_156296
-    for ws in [sample_run, beam_center_run, bkgd_run]:
+    for ws in [sample_run, beam_center_run, bkgd_run, empty_trans_run]:
         sample_pos_z = ws.getInstrument().getSample().getPos()[2]
         assert sample_pos_z == pytest.approx(-0.12952, 0.000004), '{} has a wrong sample Si-window distance {}' \
                                                                   ''.format(str(ws), sample_pos_z)
 
-    for ws in [dark_main_run, dark_wing_run]:
+    for ws in [dark_run]:
         sample_pos_z = ws.getInstrument().getSample().getPos()[2]
-        assert sample_pos_z == pytest.approx(-0.071, 0.000004), '{} has a wrong sample Si-window distance {}' \
+        assert sample_pos_z == pytest.approx(0.0000, 0.000004), '{} has a wrong sample Si-window distance {}' \
                                                                 ''.format(str(ws), sample_pos_z)
 
     # Verify sample to detector distance with default setup:
     # https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/issues/542#note_156296
-    for ws in [sample_run, beam_center_run, bkgd_run]:
+    for ws in [sample_run, beam_center_run, bkgd_run, empty_trans_run]:
         # reset SDD with sample run
         sdd_value = sample_detector_distance(ws, unit='m', search_logs=False)
         assert sdd_value == pytest.approx(14.31, 0.004), '{} has a wrong SDD {}'.format(str(ws), sdd_value)
 
-    for ws in [dark_wing_run, dark_main_run]:
+    for ws in [dark_run]:
         # EPICS recorded SDD with sample run
         sdd_value = sample_detector_distance(ws, unit='m', search_logs=False)
-        assert sdd_value == pytest.approx(10.00, 0.004), '{} has a wrong SDD {}'.format(str(ws), sdd_value)
+        assert sdd_value == pytest.approx(7.00, 1E-6), '{} has a wrong SDD {}'.format(str(ws), sdd_value)
 
 
 def generate_test_json(sens_nxs_dir):

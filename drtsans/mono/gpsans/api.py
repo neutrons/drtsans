41f52c07d056025196fde8d4e6ef4b3f36d75e15
Wenduo Zhou <zhouw@ornl.gov>
2020-05-04 13:51:20 -0400

    Refs #564. Flake8.
    Former-commit-id: f2c0ba88b9277f7883fcf1a61bd1a28293f09954

--- drtsans/mono/gpsans/api.py
+++ drtsans/mono/gpsans/api.py
@@ -82,9 +82,8 @@ def load_all_files(reduction_input, prefix='', load_params=None, path=None):
     #     # wave length spread cannot be overwritten by itself without wave length or vice verse
     #     # this is left for discussion on wave length
     #     wavelength = wavelength_spread_user = None
-    
     wave_length_dict, wave_length_spread_dict = meta_data.parse_json_wave_length_and_spread(reduction_config)
-    
+
     if reduction_config["useDefaultMask"]:
         # reduction_config["defaultMask"] is a list of python dictionaries
         default_mask = reduction_config["defaultMask"] if reduction_config["defaultMask"] is not None else []

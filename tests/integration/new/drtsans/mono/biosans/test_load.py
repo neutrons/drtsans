7a666bd63d2a41e85c7d5f55fb52f7fb6ab37ea8
Wenduo Zhou <zhouw@ornl.gov>
2020-05-05 15:09:13 -0400

    Refs #564. Added test for GPSANS.
    Former-commit-id: 9e27b13ea6e49d55372de278ee6ae411a72b1167

--- tests/integration/new/drtsans/mono/biosans/test_load.py
+++ tests/integration/new/drtsans/mono/biosans/test_load.py
@@ -100,7 +100,8 @@ def test_load_all_files(reference_dir):
         assert wave_length == pytest.approx(1.23, 1.E-7), \
             '{}-th workspace: wave length {} shall be equal to 1.23 angstrom'.format(ws_index, wave_length)
         assert wave_length_spread == pytest.approx(0.46, 1.E-7), \
-            '{}-th workspace: wave length spread {} shall be equal to 0.46 angstrom'.format(ws_index, wave_length_spread)
+            '{}-th workspace: wave length spread {} shall be equal to 0.46 angstrom' \
+            ''.format(ws_index, wave_length_spread)
 
     for ws_index, ws in enumerate([dark_run]):
         sample_log_i = SampleLogs(ws)

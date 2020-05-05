86bfc69f62e948d5d619cbea9d68602a0583ce83
Wenduo Zhou <zhouw@ornl.gov>
2020-05-05 14:36:58 -0400

    Refs #564. Make BIOSANS work.
    Former-commit-id: bf59624a4c76ef07ddd2fdb71fbe343b74cf507e

--- tests/integration/new/drtsans/mono/biosans/test_load.py
+++ tests/integration/new/drtsans/mono/biosans/test_load.py
@@ -92,9 +92,25 @@ def test_load_all_files(reference_dir):
             '{}-th workspace: Pixel size X {} (m) shall be equal to 2.34567 mm'.format(ws_index, pixel_size_y)
 
     # Check center wave length and spread
-    for ws_index, ws in enumerate([sample_run, beam_center_run]):
-        print(ws.readX(0))
-        print(ws.readX(1))
+    for ws_index, ws in enumerate([sample_run, beam_center_run, bkgd_run, empty_trans_run]):
+        sample_log_i = SampleLogs(ws)
+        wave_length = sample_log_i['wavelength'].value[0]
+        wave_length_spread = sample_log_i['wavelength_spread'].value[0]
+        # overwriting value
+        assert wave_length == pytest.approx(1.23, 1.E-7), \
+            '{}-th workspace: wave length {} shall be equal to 1.23 angstrom'.format(ws_index, wave_length)
+        assert wave_length_spread == pytest.approx(0.46, 1.E-7), \
+            '{}-th workspace: wave length spread {} shall be equal to 0.46 angstrom'.format(ws_index, wave_length_spread)
+
+    for ws_index, ws in enumerate([dark_run]):
+        sample_log_i = SampleLogs(ws)
+        wave_length = sample_log_i['wavelength'].value[0]
+        wave_length_spread = sample_log_i['wavelength_spread'].value[0]
+        # original value
+        assert wave_length == pytest.approx(6.00881338, 1.E-7), \
+            '{}-th workspace: wave length {} shall be equal to 6.00881338 angstrom'.format(ws_index, wave_length)
+        assert wave_length_spread == pytest.approx(0.1323529411, 1.E-7), \
+            '{}-th workspace: wave length spread {} shall be equal to 0.13235294 angstrom'.format(ws_index, wave_length_spread)
 
 
 def generate_test_json(sens_nxs_dir):
@@ -173,7 +189,7 @@ def generate_test_json(sens_nxs_dir):
          "useErrorWeighting": false,
          "useMaskBackTubes": false,
          "wavelength": "1.23",
-         "wavelengthSpread": "0.23",
+         "wavelengthSpread": "0.46",
          "overlapStitchQmin": "0.075",
          "overlapStitchQmax": "0.095",
          "useTimeSlice": false,

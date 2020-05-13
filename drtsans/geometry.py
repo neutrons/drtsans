f048e191b95a37f5b96fafc79784b48b6ad3283d
Wenduo Zhou <zhouw@ornl.gov>
2020-05-13 15:48:19 -0400

    Refs #592. Re-organize the unit tests.
    Former-commit-id: 3841b448c7b08749c15cb5a57cf1dfbc4b93f09b

--- drtsans/geometry.py
+++ drtsans/geometry.py
@@ -652,37 +652,6 @@ def source_aperture_diameter(input_workspace, unit='mm'):
     return diameter if unit == 'mm' else diameter / 1.e3
 
 
-# def beam_radius(input_workspace, unit='mm'):
-#     """
-#     Calculate the beam radius impinging on the detector
-#
-#     R_beam = R_sampleAp + SDD * (R_sampleAp + R_sourceAp) / SSD, where
-#     R_sampleAp: radius of the sample aperture,
-#     SDD: distance between the sample and the detector,
-#     R_sourceAp: radius of the source aperture,
-#     SSD: distance between the source and the sample.
-#
-#     Parameters
-#     ----------
-#     input_workspace: ~mantid.api.MatrixWorkspace, str
-#         Input workspace
-#     unit: str
-#         Units of the output beam radius. Either 'mm' or 'm'.
-#
-#     Returns
-#     -------
-#     float
-#     """
-#     r_sa = sample_aperture_diameter(input_workspace, unit=unit) / 2.0  # radius
-#     r_so = source_aperture_diameter(input_workspace, unit=unit) / 2.0  # radius
-#     l1 = source_sample_distance(input_workspace, unit=unit)
-#     l2 = sample_detector_distance(input_workspace, unit=unit)
-#
-#     radius = r_sa + (r_sa + r_so) * (l2 / l1)
-#     logger.notice("Radius calculated from the input workspace = {:.2} mm".format(radius * 1e3))
-#     return radius
-
-
 def translate_source_by_z(input_workspace, z=None, relative=False):
     r"""
       Adjust the Z-coordinate of the source.

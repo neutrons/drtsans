21f1cfbc4080664c2d239762631f8d80ce97cd46
Wenduo Zhou <zhouw@ornl.gov>
2020-06-25 16:13:41 -0400

    Refs #628. Implemented converter to histogram and unit test.
    Former-commit-id: d87b0f0f0f00b3bbca37e684957d7d32a7121991

--- drtsans/files/event_nexus_rw.py
+++ drtsans/files/event_nexus_rw.py
@@ -1,3 +1,7 @@
+import numpy as np
+from collections import namedtuple
+
+
 class EventNeXusWriter(object):
     """
     Write an Event NeXus file
@@ -48,3 +52,42 @@ def convert_histogram_to_events(det_id_array, det_counts_array, pulse_duration,
     # get total counts
 
     return
+
+
+def convert_to_histogram_bank(bank_entry):
+    """Convert events information in bank to histogram
+
+    Parameters
+    ----------
+    bank_entry
+
+    Returns
+    -------
+    ~namedtuple
+        Histogram converted from TOF bank information
+
+    """
+
+    # Calculate counts on each pixel:w
+    bank_event_ids = bank_entry['event_id']
+
+    # Count each value's appearance in bank event ids array
+    # result is from 0 to largest pixel ID in the event_id array
+    per_pixel_counts = np.bincount(bank_event_ids)
+    # ignore those pixels without any counts
+    pixel_id_array = np.where(per_pixel_counts > 0)[0]
+    pixel_counts_array = per_pixel_counts[per_pixel_counts > 0]
+
+    # Get pulse time duration information
+    pulse_start_times = bank_entry['event_time_zero'][()]
+    pulse_duration = (pulse_start_times[1:] - pulse_start_times[:-1]).mean()
+
+    # Get reasonable TOF information
+    tof_array = bank_entry['event_time_offset'][()]
+
+    # Define namedtuple to record histogram from TOF events
+    TofHistogram = namedtuple('Histogram', ['pixel_ids', 'counts', 'pulse_duration', 'tof_min', 'tof_max'])
+    histogram = TofHistogram(pixel_id_array, pixel_counts_array, pulse_duration,
+                             tof_array.min(), tof_array.max())
+
+    return histogram
--- tests/unit/new/drtsans/test_event_nexus_rw.py
+++ tests/unit/new/drtsans/test_event_nexus_rw.py
@@ -1,5 +1,9 @@
+import pytest
+import os
+import h5py
 from drtsans.files.event_nexus_rw import EventNeXusWriter
 from drtsans.files.event_nexus_rw import convert_histogram_to_events
+from drtsans.files.event_nexus_rw import convert_to_histogram_bank
 
 
 def test_imports():
@@ -14,3 +18,38 @@ def test_convert_histogram_to_events():
 
     """
     assert convert_histogram_to_events
+
+
+def test_convert_to_histogram(reference_dir):
+    """Test method to convert a single bank's TOF events to histogram
+
+    Parameters
+    ----------
+    reference_dir
+
+    Returns
+    -------
+
+    """
+    # Parse NeXus file manually for the values nodes
+    nexus_name = os.path.join(reference_dir.new.gpsans, 'CG2_9166.nxs.h5')
+    nexus_h5 = h5py.File(nexus_name, 'r')
+
+    # test with bank 9
+    bank9_entry = nexus_h5['/entry/bank9_events']
+    bank9_histogram = convert_to_histogram_bank(bank9_entry)
+    #  pixel_id_array, pixel_counts_array, pulse_duration, tof_array.min(), tof_array.max()
+
+    # close  file
+    nexus_h5.close()
+
+    # verify
+    assert bank9_histogram.counts.shape == (982, )
+    assert bank9_histogram.pixel_ids.min() >= 16384 and bank9_histogram.pixel_ids.max() < 17408
+    assert bank9_histogram.pulse_duration == pytest.approx(0.01666667, 1.E-6)
+    assert bank9_histogram.min_tof >= 0.0
+    assert bank9_histogram.max_tof == pytest.approx(16666.6, 0.1)
+
+
+if __name__ == '__main__':
+    pytest.main(__file__)

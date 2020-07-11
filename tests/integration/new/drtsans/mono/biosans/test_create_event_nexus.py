bffb11777beeae0d8b9e5c4deebff67b0293eda7
Wenduo Zhou <zhouw@ornl.gov>
2020-07-11 16:02:04 -0400

    Refs #628. Added one more test.
    Former-commit-id: f33b02da6f6e6767942a8e77fbfcbbeacb1e071e

--- tests/integration/new/drtsans/mono/biosans/test_create_event_nexus.py
+++ tests/integration/new/drtsans/mono/biosans/test_create_event_nexus.py
@@ -5,12 +5,16 @@ import pytest
 import numpy as np
 import os
 from drtsans.load import load_events
+import json
 import h5py
 from drtsans.files.hdf5_rw import GroupNode, DataSetNode
 from drtsans.files.event_nexus_nodes import InstrumentNode, DasLogNode, BankNode, MonitorNode
 from drtsans.files.event_nexus_rw import generate_events_from_histogram
 from drtsans.files.event_nexus_rw import generate_monitor_events_from_count
 from drtsans.files.event_nexus_rw import init_event_nexus, parse_event_nexus, EventNeXusWriter
+# drtsans imports
+from drtsans.mono.biosans import (load_all_files, plot_reduction_output, reduce_single_configuration,
+                                  reduction_parameters, validate_reduction_parameters)
 from mantid.simpleapi import LoadEventNexus
 
 
@@ -386,7 +390,7 @@ def verify_histogram(source_nexus, test_nexus):
         raise AssertionError(error_message)
 
 
-def not_now_test_reduction(reference_dir, cleanfile):
+def test_reduction(reference_dir, cleanfile):
     """Test generate (partially copy) an event Nexus file by
     verifying reduction result between raw and generated event nexus file
 
@@ -396,9 +400,148 @@ def not_now_test_reduction(reference_dir, cleanfile):
     -------
 
     """
-    assert reference_dir
-    assert cleanfile
-    raise NotImplementedError('Next Step')
+    # Set up test
+    json_str = generate_testing_json(os.path.join(reference_dir.new.biosans, 'overwrite_gold_04282020'), None, None)
+
+    # TODO / FIXME - switch to tempfile later
+    # output_dir = mkdtemp(prefix='meta_overwrite_bio_test1')
+    output_dir = '/tmp/nexuscg3reduction/'
+    cleanfile(output_dir)
+
+    # Run
+    reduce_biosans_data(reference_dir.new.biosans, json_str, output_dir, prefix='BioMetaRaw')
+
+    # Get result files
+    sample_names = ['csmb_ecoli1h_n2', 'insect1hTime_n2']
+    gold_path = os.path.join(reference_dir.new.biosans, 'overwrite_gold_04282020/test1/')
+
+    # Verify
+    verify_reduction_results(sample_names, output_dir, gold_path, title='Raw (no overwriting)', prefix='test1')
+
+
+def generate_testing_json(sens_nxs_dir, sample_to_si_window_distance, sample_to_detector_distance):
+    """Generating testing JSON
+
+    Parameters
+    ----------
+    sens_nxs_dir: str
+        directory path to sensitivity files
+    sample_to_si_window_distance: float or None
+        sample to silicon window distance to overwrite the EPICS value with unit millimeter
+    sample_to_detector_distance: float or None
+        sample to silicon window distance to overwrite the EPICS value with unit meter
+
+    Returns
+    -------
+    str
+        JSON string
+    """
+    specs = {
+        "iptsNumber": "23782",
+        "sample": {"runNumber": "4822", "thickness": "0.1", "transmission": {"runNumber": "4822"}},
+        "outputFileName": "CG3_4822",
+        "background": {"runNumber": "4821", "transmission": {"runNumber": "4821"}},
+        "beamCenter": {"runNumber": "1322"},
+        "emptyTransmission": {"runNumber": "5705"},
+        "configuration": {
+            "outputDir": "/HFIR/CG3/shared/UserAcceptance/override/test1",
+            "sampleApertureSize": "14",
+            "useDefaultMask": True,
+            "defaultMask": ["{'Pixel':'1-12,244-256'}", "{'Bank':'21-24,45-48'}"],
+            "darkMainFileName": "CG3_1383.nxs.h5",
+            "darkWingFileName": "CG3_1383.nxs.h5",
+            "sensitivityMainFileName": "/HFIR/CG3/shared/Cycle486/sens_f4829m7p0_TDC_SAC.h5",
+            "sensitivityWingFileName": "/HFIR/CG3/shared/Cycle486/sens_f4835w3p2_TDC_SAC.h5",
+            "absoluteScaleMethod": "standard",
+            "StandardAbsoluteScale": "0.0055e-8",
+            "numMainQxQyBins": "100",
+            "numWingQxQyBins": "100",
+            "1DQbinType": "scalar",
+            "QbinType": "log",
+            "useLogQBinsEvenDecade": False,
+            "LogQBinsPerDecadeMain": 20,
+            "LogQBinsPerDecadeWing": 25,
+            "WedgeMinAngles": "-30, 60",
+            "WedgeMaxAngles": "30, 120",
+            "AnnularAngleBin": "1",
+            "QminMain": "0.003",
+            "QminWing": "0.003",
+            "overlapStitchQmin": "0.075",
+            "overlapStitchQmax": "0.095",
+            "useTimeSlice": False,
+            "timeSliceInterval": "200",
+            "usePixelCalibration": False,
+            "useSubpixels": False
+        }
+    }
+    reduction_input = reduction_parameters(specs, 'BIOSANS', validate=False)  # add defaults and defer validation
+    reduction_config = reduction_input['configuration']  # a handy shortcut
+
+    #  '/SNS/EQSANS/shared/sans-backend/data/new/ornl/sans/meta_overwrite/biosans/sens_f4829m7p0_TDC_SAC.h5'
+    main_sens = os.path.join(sens_nxs_dir, 'sens_f4829m7p0_TDC_SAC.h5')
+    reduction_config['sensitivityMainFileName'] = main_sens
+
+    # '/SNS/EQSANS/shared/sans-backend/data/new/ornl/sans/meta_overwrite/biosans/sens_f4835w3p2_TDC_SAC.h5'
+    wing_sens = os.path.join(sens_nxs_dir, 'sens_f4835w3p2_TDC_SAC.h5')
+    reduction_config['sensitivityWingFileName'] = wing_sens
+
+    if sample_to_si_window_distance is not None:
+        reduction_config['sampleToSi'] = sample_to_si_window_distance
+
+    if sample_to_detector_distance is not None:
+        reduction_config['sampleDetectorDistance'] = sample_to_detector_distance
+
+    return json.dumps(reduction_input)  # return a string representation
+
+
+def reduce_biosans_data(nexus_dir, json_str, output_dir, prefix):
+    """Reduce BIOSANS runs
+
+    Parameters
+    ----------
+    nexus_dir: str
+        path to NeXus files
+    json_str: str
+        configuration json
+    output_dir: str
+        output directory
+    prefix: str
+        prefix for output workspaces' names
+
+    Returns
+    -------
+
+    """
+    # Set up (testing) runs
+    sample_names = ['csmb_ecoli1h_n2']
+    samples = ['5709']
+    samples_trans = samples
+    backgrounds = ['5715']
+    backgrounds_trans = backgrounds
+
+    # checking if output directory exists, if it doesn't, creates the folder
+    for subfolder in ['1D', '2D']:
+        output_folder = os.path.join(output_dir, subfolder)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+    # Load JSON for configuration
+    # start with a fresh set of reduction parameters for every sample, because the reduction "pollutes"
+    # the reduction parameters dictionary with additions not allowed by the schema
+    reduction_input = json.loads(json_str)
+    reduction_input["dataDirectories"] = nexus_dir
+    reduction_input["configuration"]["outputDir"] = output_dir
+    reduction_input["sample"]["runNumber"] = samples[0]
+    reduction_input["sample"]["transmission"]["runNumber"] = samples_trans[0]
+    reduction_input["background"]["runNumber"] = backgrounds[0]
+    reduction_input["background"]["transmission"]["runNumber"] = backgrounds_trans[0]
+    reduction_input["outputFileName"] = sample_names[0]
+    # always check after updating the parameters
+    reduction_input = validate_reduction_parameters(reduction_input)
+    loaded = load_all_files(reduction_input,
+                            path=nexus_dir,
+                            prefix=prefix)
+    reduce_single_configuration(loaded, reduction_input)
 
 
 def verify_reduction_results(sample_names, output_dir, gold_path, title, prefix):

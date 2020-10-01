9b4d60201abd9b3b94eabbc25d84e9a626251887
Wenduo Zhou <zhouw@ornl.gov>
2020-10-01 15:46:23 -0400

    Refs #660. More work on test.
    Former-commit-id: a1a15b809e170519498f0b5c24ed84425b8ba3c8

--- tests/integration/new/drtsans/mono/gpsans/test_pixel_map.py
+++ tests/integration/new/drtsans/mono/gpsans/test_pixel_map.py
@@ -1,6 +1,23 @@
 import pytest
+import os
+from tempfile import mkdtemp
+from drtsans.mono.bar_scan_pixel_calibration import generate_pixel_map_legacy
 
 
+def test_pixel_calibration(reference_dir, cleanfile):
+    """
+
+    Parameters
+    ----------
+    reference_dir
+
+    Returns
+    -------
+
+    """
+    expected_calib_nexus = os.path.join(reference_dir.new.gpsans, 'calibrations/CG2_Pixel_Calibration_Expected_111.nxs')
+    assert os.path.exists(expected_calib_nexus)
+    test_calib_nexus = '/tmp/test_pixel_calibration/runs_1_111/tables/Test_CG2_Pixel_Calibration.nxs'
 
 
 

51a9e046438744a2638d0714a8376bc9d33962b7
Wenduo Zhou <zhouw@ornl.gov>
2020-10-06 23:24:42 -0400

    Refactored gpsans overwrite integration test.
    Former-commit-id: 1d195af070b4148daa33d16628d79ca93cc98878

--- tests/integration/new/drtsans/mono/gpsans/test_overwrite_geometry_meta_data.py
+++ tests/integration/new/drtsans/mono/gpsans/test_overwrite_geometry_meta_data.py
@@ -5,6 +5,7 @@ import pytest
 from tempfile import mkdtemp
 import time
 import os
+from drtsans.files.log_h5_reader import verify_reduction_results
 
 from drtsans.mono.gpsans import (load_all_files, plot_reduction_output, reduce_single_configuration,
                                  reduction_parameters, update_reduction_parameters)
@@ -66,102 +67,102 @@ def reduce_gpsans_data(data_dir, reduction_input_common, output_dir, prefix):
     print('Execution Time: {}'.format(end_time - start_time))
 
 
-def get_iq1d(log_file_name):
-    """Get I(Q) from output SANS log file
-
-    Parameters
-    ----------
-    log_file_name: str
-        log file name
-
-    Returns
-    -------
-    tuple
-        numpy 1D array for Q, numpy 1D array for intensity
-
-    """
-    # Open file and entry
-    log_h5 = h5py.File(log_file_name, 'r')
-
-    if '_slice_1' in log_h5:
-        data_entry = log_h5['_slice_1']['main']
-    else:
-        data_entry = log_h5['main']
-
-    # Get data
-    iq1d_entry = data_entry['I(Q)']
-
-    # Get data with a copy
-    vec_q = np.copy(iq1d_entry['Q'].value)
-    vec_i = np.copy(iq1d_entry['I'].value)
-
-    # close file
-    log_h5.close()
-
-    return vec_q, vec_i
-
-
-def compare_reduced_iq(test_log_file, gold_log_file, title, prefix):
-    """Compare I(Q) from reduced file and gold file
-
-    Parameters
-    ----------
-    test_log_file
-    gold_log_file
-    title: str
-        title of output figure
-    prefix: str
-        prefix of output file
-
-    Returns
-    -------
-
-    """
-    # Plot main
-    test_q_vec, test_intensity_vec = get_iq1d(test_log_file)
-    gold_q_vec, gold_intensity_vec = get_iq1d(gold_log_file)
-
-    # Verify result
-    try:
-        np.testing.assert_allclose(test_q_vec, test_q_vec, atol=1E-4)
-        np.testing.assert_allclose(test_intensity_vec, gold_intensity_vec, atol=1E-7)
-    except AssertionError as assert_err:
-        from matplotlib import pyplot as plt
-        plt.cla()
-        plt.plot(test_q_vec, test_intensity_vec, color='red', label='Corrected')
-        plt.plot(gold_q_vec, gold_intensity_vec, color='black', label='Before being corrected')
-        plt.legend()
-        plt.title(title)
-        plt.yscale('log')
-        out_name = prefix + '_' + os.path.basename(test_log_file).split('.')[0] + '.png'
-        plt.savefig(out_name)
-
-        raise assert_err
-
-
-def verify_reduction_results(sample_names, output_dir, gold_path, title, prefix):
-
-    unmatched_errors = ''
-
-    for sample_name in sample_names:
-        # output log file name
-        output_log_file = os.path.join(output_dir, '{}_reduction_log.hdf'.format(sample_name))
-        assert os.path.exists(output_log_file), 'Output {} cannot be found'.format(output_log_file)
-        # gold file
-        gold_log_file = os.path.join(gold_path, '{}_reduction_log.hdf'.format(sample_name))
-        assert os.path.exists(gold_path), 'Gold file {} cannot be found'.format(gold_log_file)
-        # compare
-        title_i = '{}: {}'.format(sample_name, title)
-        try:
-            compare_reduced_iq(output_log_file, gold_log_file, title_i, prefix)
-        except AssertionError as unmatched_error:
-            unmatched_errors = 'Testing output {} is different from gold result {}:\n{}' \
-                               ''.format(output_log_file, gold_log_file, unmatched_error)
-    # END-FOR
-
-    # raise error for all
-    if unmatched_errors != '':
-        raise AssertionError(unmatched_errors)
+# def get_iq1d(log_file_name):
+#     """Get I(Q) from output SANS log file
+#
+#     Parameters
+#     ----------
+#     log_file_name: str
+#         log file name
+#
+#     Returns
+#     -------
+#     tuple
+#         numpy 1D array for Q, numpy 1D array for intensity
+#
+#     """
+#     # Open file and entry
+#     log_h5 = h5py.File(log_file_name, 'r')
+#
+#     if '_slice_1' in log_h5:
+#         data_entry = log_h5['_slice_1']['main']
+#     else:
+#         data_entry = log_h5['main']
+#
+#     # Get data
+#     iq1d_entry = data_entry['I(Q)']
+#
+#     # Get data with a copy
+#     vec_q = np.copy(iq1d_entry['Q'].value)
+#     vec_i = np.copy(iq1d_entry['I'].value)
+#
+#     # close file
+#     log_h5.close()
+#
+#     return vec_q, vec_i
+#
+#
+# def compare_reduced_iq(test_log_file, gold_log_file, title, prefix):
+#     """Compare I(Q) from reduced file and gold file
+#
+#     Parameters
+#     ----------
+#     test_log_file
+#     gold_log_file
+#     title: str
+#         title of output figure
+#     prefix: str
+#         prefix of output file
+#
+#     Returns
+#     -------
+#
+#     """
+#     # Plot main
+#     test_q_vec, test_intensity_vec = get_iq1d(test_log_file)
+#     gold_q_vec, gold_intensity_vec = get_iq1d(gold_log_file)
+#
+#     # Verify result
+#     try:
+#         np.testing.assert_allclose(test_q_vec, test_q_vec, atol=1E-4)
+#         np.testing.assert_allclose(test_intensity_vec, gold_intensity_vec, atol=1E-7)
+#     except AssertionError as assert_err:
+#         from matplotlib import pyplot as plt
+#         plt.cla()
+#         plt.plot(test_q_vec, test_intensity_vec, color='red', label='Corrected')
+#         plt.plot(gold_q_vec, gold_intensity_vec, color='black', label='Before being corrected')
+#         plt.legend()
+#         plt.title(title)
+#         plt.yscale('log')
+#         out_name = prefix + '_' + os.path.basename(test_log_file).split('.')[0] + '.png'
+#         plt.savefig(out_name)
+#
+#         raise assert_err
+#
+#
+# def verify_reduction_results(sample_names, output_dir, gold_path, title, prefix):
+#
+#     unmatched_errors = ''
+#
+#     for sample_name in sample_names:
+#         # output log file name
+#         output_log_file = os.path.join(output_dir, '{}_reduction_log.hdf'.format(sample_name))
+#         assert os.path.exists(output_log_file), 'Output {} cannot be found'.format(output_log_file)
+#         # gold file
+#         gold_log_file = os.path.join(gold_path, '{}_reduction_log.hdf'.format(sample_name))
+#         assert os.path.exists(gold_path), 'Gold file {} cannot be found'.format(gold_log_file)
+#         # compare
+#         title_i = '{}: {}'.format(sample_name, title)
+#         try:
+#             compare_reduced_iq(output_log_file, gold_log_file, title_i, prefix)
+#         except AssertionError as unmatched_error:
+#             unmatched_errors = 'Testing output {} is different from gold result {}:\n{}' \
+#                                ''.format(output_log_file, gold_log_file, unmatched_error)
+#     # END-FOR
+#
+#     # raise error for all
+#     if unmatched_errors != '':
+#         raise AssertionError(unmatched_errors)
 
 
 # dev - Wenduo Zhou <wzz@ornl.gov>

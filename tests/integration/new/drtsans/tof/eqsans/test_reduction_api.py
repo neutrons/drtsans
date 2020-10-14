64703426c2014e367a17594cd6689db4c317265e
1im <mcgahajr@ornl.gov>
2020-10-14 01:23:52 -0400

    Implement basic integration test #632
    Former-commit-id: a430fa2d7e426f9f46b4472be5dfe24f15fc06c2

--- tests/integration/new/drtsans/tof/eqsans/test_reduction_api.py
+++ tests/integration/new/drtsans/tof/eqsans/test_reduction_api.py
@@ -1,7 +1,5 @@
 import pytest
 import os
-import sys
-import json
 import tempfile
 from drtsans.tof.eqsans import validate_reduction_parameters
 from drtsans.tof.eqsans.api import (load_all_files, reduce_single_configuration,plot_reduction_output)  # noqa E402
@@ -46,28 +44,15 @@ def test_wavelength_step(reference_dir, cleanfile):
         }
     }
 
-    # Get test JSON file
-    test_json_file = os.path.join(reference_dir.new.eqsans, 'whatevername.json')
+    # validate and clean configuration
+    input_config = validate_reduction_parameters(configuration)
 
     # Create output directory
-    test_output_dir = tempfile.mkdtemp()
-    cleanfile(test_output_dir)
-
-    # Load JSON file to configuration
-    with open(sys.argv[1], 'r') as fd:
-        input_config = json.load(fd)
-        input_config = validate_reduction_parameters(input_config)
-
-    # checking if output directory exists, if it doesn't, creates the folder
-    input_config["configuration"]["outputDir"] = test_output_dir
-    if not os.path.exists(test_output_dir):
-        os.makedirs(test_output_dir)
-
-    loaded = load_all_files(input_config)
-    out = reduce_single_configuration(loaded, input_config)
-
-    # verify out
-    # blabla...
+    with tempfile.TemporaryDirectory() as test_dir:
+        input_config['configuration']['outputDir'] = test_dir
+        loaded = load_all_files(input_config)
+        _ = reduce_single_configuration(loaded, input_config)
+        assert os.path.isfile(f'{test_dir}/test_wavelength_step.nxs')
 
 
 if __name__ == '__main__':

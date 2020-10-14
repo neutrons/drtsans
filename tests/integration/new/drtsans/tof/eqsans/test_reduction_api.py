ed5f8253f0dcfbdf3f8a03e795df69e988dabe1d
1im <mcgahajr@ornl.gov>
2020-10-14 01:31:06 -0400

    Flake compliance
    Former-commit-id: 0c856a3ba77d8e7d45aec7bad38032de5818e47a

--- tests/integration/new/drtsans/tof/eqsans/test_reduction_api.py
+++ tests/integration/new/drtsans/tof/eqsans/test_reduction_api.py
@@ -34,8 +34,11 @@ def test_wavelength_step(reference_dir, cleanfile):
             'cutTOFmax': '1500',
             'wavelengthStepType': 'constant Delta lambda/lambda',
             'sampleApertureSize': '10',
-            'fluxMonitorRatioFile': '/SNS/EQSANS/IPTS-24769/shared/EQSANS_110943.out',
-            'sensitivityFileName': '/SNS/EQSANS/shared/NeXusFiles/EQSANS/2020A_mp/Sensitivity_patched_thinPMMA_2o5m_113514_mantid.nxs',
+            'fluxMonitorRatioFile': ('/SNS/EQSANS/'
+                                     'IPTS-24769/shared/EQSANS_110943.out'),
+            'sensitivityFileName': ('/SNS/EQSANS/shared/NeXusFiles/'
+                                    'EQSANS/2020A_mp/'
+                                    'Sensitivity_patched_thinPMMA_2o5m_113514_mantid.nxs'),
             'numQBins': 100,
             'WedgeMinAngles': '-30, 60',
             'WedgeMaxAngles': '30, 120',
@@ -51,7 +54,7 @@ def test_wavelength_step(reference_dir, cleanfile):
     with tempfile.TemporaryDirectory() as test_dir:
         input_config['configuration']['outputDir'] = test_dir
         loaded = load_all_files(input_config)
-        _ = reduce_single_configuration(loaded, input_config)
+        reduce_single_configuration(loaded, input_config)
         assert os.path.isfile(f'{test_dir}/test_wavelength_step.nxs')
 
 

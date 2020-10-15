1de39453cef5476a9007a36bdcedced55a298d29
Wenduo Zhou <zhouw@ornl.gov>
2020-10-15 19:31:33 -0400

    Refs #671.  Updated spice to nexus template.
    Former-commit-id: 89c2915b9b1dcbcd8802bf030887ddb9f7e5a044

--- scripts/jupyter_notebooks/biosans_spice_to_nexus_template.py
+++ scripts/jupyter_notebooks/biosans_spice_to_nexus_template.py
@@ -25,14 +25,17 @@ from drtsans.mono.biosans.cg3_spice_to_nexus import convert_spice_to_nexus  # no
 nexus_names = set()
 for scan_num, pt_num in scan_pt_list:
     nexus = convert_spice_to_nexus(ipts, exp, scan_num, pt_num, TEMPLATE_EVENT_NEXUS,
+                                   masked_detector_pixels=[70911],
                                    output_dir=f'/HFIR/CG3/IPTS-{ipts}/shared/Exp{exp}')
     nexus_names.add(nexus)
 
 # Check
 from mantid.simpleapi import LoadEventNexus  # noqa: E401
+print(f'\n\n-----  Verification -------\n\n')
 for nexus_name in nexus_names:
     try:
-        ws = LoadEventNexus(Filename=nexus_name, )
+        print(f'Loading {nexus_name}')
+        ws = LoadEventNexus(Filename=nexus_name, LoadNexusInstrumentXML=True)
         print(f'{nexus_name}: {ws.getNumberHistograms()}, {ws.getNumberEvents()}')
     except RuntimeError as run_err:
         print(f'Failed to laod {nexus_name} due to {run_err}')

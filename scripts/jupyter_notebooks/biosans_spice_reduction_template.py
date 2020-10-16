8fd6b82b40e9d38327644edf3bc1fd232218c9d4
Wenduo Zhou <zhouw@ornl.gov>
2020-10-16 16:40:31 -0400

    Refs #675. Cleaned up the script.
    Former-commit-id: 05cbf1ad85ba3a9dd6fd4cae2df7623fac595da3

--- scripts/jupyter_notebooks/biosans_spice_reduction_template.py
+++ scripts/jupyter_notebooks/biosans_spice_reduction_template.py
@@ -31,96 +31,88 @@ overWrite = True                  # Option to overwrite existing data or create 
 # ## Instrument Scientist or Local contact input below (And Expert Users) 
 
 # Advanced Settings for Data Reduction--
-#Buffer clearing frequency
-clearBuffer  = False
-refreshCycle           = 25                    # loops... depending on the activities.
+# Buffer clearing frequency
+clearBuffer = False
+refreshCycle = 25                    # loops... depending on the activities.
 
-#Common setting for all options
-scalefac               = "4.05e-9"
-beam_center            = '6378'
-empty_trans            = '6379'
-dark_mfname            = 'CG3_6402.nxs.h5'    
-dark_wfname            = 'CG3_6402.nxs.h5'    
-sens_mfname            = '/HFIR/CG3/shared/Cycle488/Sens_f6368m4p0_bsSVP.nxs'
-sens_wfname            = '/HFIR/CG3/shared/Cycle488/Sens_f6380w1p4_bsSVP.nxs'
+# Common setting for all options
+scalefac = "4.05e-9"
+beam_center = (220, 1)
+empty_trans = (220, 1)
+dark_mfname = (44, 1)
+dark_wfname = (44, 1)
+sens_mfname = '/HFIR/CG3/shared/Cycle488/Sens_f6368m4p0_bsSVP.nxs'
+sens_wfname = '/HFIR/CG3/shared/Cycle488/Sens_f6380w1p4_bsSVP.nxs'
 
-#Plotting range--
-q_range_main           = [0.003, 0.045]        # Q-range for isotropic data
-q_range_wing           = [0.03, 0.9]
-OL_range               = [0.0325, 0.0425]
+# Plotting range--
+q_range_main = [0.003, 0.045]        # Q-range for isotropic data
+q_range_wing = [0.03, 0.9]
+OL_range = [0.0325, 0.0425]
 
-#Miscellaneous settings--
-base_output_directory  = '/HFIR/CG3/IPTS-'+IPTS_Number+'/shared/'+User3LetInitial+'/'
-scaling_beam_radius    = None
-flexible_pixelsizes    = True                  # 'True'- if use barscan/flood information for flexible pixel sizes, else 'False'
-                                               # Make sure the barscan used sensitivity file used above if 'True'
+# Miscellaneous settings--
+base_output_directory = f'/HFIR/CG3/IPTS-{IPTS_Number}/shared/{User3LetInitial}/'
+scaling_beam_radius = None
+flexible_pixelsizes = True  # 'True'- if use barscan/flood information for flexible pixel sizes, else 'False'
+# Make sure the barscan used sensitivity file used above if 'True'
 
-#Plotting Options--
-Plot_type              = 'scalar'              # 'scalar' for isotropic and 'wedge' for anisotropic (manual or auto)
-Plot_binning           = 'log'                 # 'log' or 'linear' Q-binning
-#LINEAR BINNING
-Lin1DQbins_Main        = ""                    # No. of bins for linear binning of 1D Main Detector, Default is 100; If per decade is used default is ''
-Lin1DQbins_Wing        = ""                    # No. of bins for linear binning of 1D Wing Detector, Default is 100; If per decade is used default is ''
-Lin2DQxy_Main          = 100                   # No. of bins for linear binning of 2D Main Detector, Default is 100
-Lin2DQxy_Wing          = 100                   # No. of bins for linear binning of 2D Main Detector, Default is 100
-#LOGARITHMIC BINNING
+# Plotting Options--
+Plot_type = 'scalar'  # 'scalar' for isotropic and 'wedge' for anisotropic (manual or auto)
+Plot_binning = 'log'  # 'log' or 'linear' Q-binning
+# LINEAR BINNING
+Lin1DQbins_Main = ""  # No. of bins for linear binning of 1D Main Detector, Default is 100;
+# If per decade is used default is ''
+Lin1DQbins_Wing = ""  # No. of bins for linear binning of 1D Wing Detector, Default is 100;
+# If per decade is used default is ''
+Lin2DQxy_Main = 100  # No. of bins for linear binning of 2D Main Detector, Default is 100
+Lin2DQxy_Wing = 100  # No. of bins for linear binning of 2D Main Detector, Default is 100
+
+# LOGARITHMIC BINNING
 LogQbinsPerDecade_Main = 25                    # No. of bins per decade of 1D Main Detector, Default is 33
 LogQbinsPerDecade_Wing = 25                    # No. of bins per decade of 1D Main Detector, Default is 33
 
+# If time Slicing--
+timeSliceExpt = False                 # 'True' if time slice experiment, else 'False'
+timeSliceDuration = 60                    # Units - seconds and irrelevant if above is 'False'
 
-#If time Slicing--
-timeSliceExpt          = False                 # 'True' if time slice experiment, else 'False'
-timeSliceDuration      = 60                    # Units - seconds and irrelevant if above is 'False'
+# ANISOTROPIC DATA REDUCTION--
+# Wedge_0...
+q_range_main_wedge0 = [0.003, 0.0425]       # Q-range for anisotropic data -- wedge0
+q_range_wing_wedge0 = [0.02, 0.45]
+OL_range_wedge0 = [0.025, 0.04]
 
-#ANISOTROPIC DATA REDUCTION--
-#Wedge_0... 
-q_range_main_wedge0    = [0.003, 0.0425]       # Q-range for anisotropic data -- wedge0 
-q_range_wing_wedge0    = [0.02, 0.45]
-OL_range_wedge0        = [0.025, 0.04]
+# Wedge_1...
+q_range_main_wedge1 = [0.003, 0.0425]       # Q-range for anisotropic data -- wedge1
+q_range_wing_wedge1 = [0.03, 0.45]
+OL_range_wedge1 = [0.03, 0.04]
 
-#Wedge_1... 
-q_range_main_wedge1    = [0.003, 0.0425]       # Q-range for anisotropic data -- wedge1
-q_range_wing_wedge1    = [0.03, 0.45]
-OL_range_wedge1        = [0.03, 0.04]
+# If Manual Wedges--
+wedge_min_angles = None  # If AUTOWEDGE reduction type 'None'; Irrelevant for ISOTROPIC [Wedge0_min,Wedge1_min]
+wedge_max_angles = None  # If AUTOWEDGE reduction type 'None'; Irrelevant for ISOTROPIC [Wedge0_max,Wedge1_max] 
 
-#If Manual Wedges--
-wedge_min_angles       = None                  # If AUTOWEDGE reduction type 'None'; Irrelevant for ISOTROPIC [Wedge0_min,Wedge1_min]
-wedge_max_angles       = None                  # If AUTOWEDGE reduction type 'None'; Irrelevant for ISOTROPIC [Wedge0_max,Wedge1_max] 
+# If automatic determination of wedge angles-- (To determine Wedges-TDW)
+Qmin_TDW = 0.003  # Minimum Q of the Main Detector
+Qmax_TDW = 0.04   # Maximum Q of the Main Detector
+Qdelta_TDW = 0.01    # Q step-size (or annual ring widths); Default is 0.01. Too fine will fail autodetection.
+PeakWidth_TDW = 0.5  # Wedge0 opening angle based of peak width (Default is 0.5--- 50%)
+AziDelta_TDW = 1.0   # Azimuthal Angle, Phi step-size
+BkgWidth_TDW = 1.0   # Wedge1 opening angle based of peak width (Default is 1.5--- 150%)
+MinSigtoNoise_TDW = 2.0  # The intensity ratio between peak and background to detect a peak (Default is 2.0)
 
-#If automatic determination of wedge angles-- (To determine Wedges-TDW)
-Qmin_TDW               = 0.003                 # Minimum Q of the Main Detector
-Qmax_TDW               = 0.04                  # Maximum Q of the Main Detector
-Qdelta_TDW             = 0.01                  # Q step-size (or annual ring widths); Default is 0.01. Too fine will fail autodetection.
-PeakWidth_TDW          = 0.5                   # Wedge0 opening angle based of peak width (Default is 0.5--- 50%)
-AziDelta_TDW           = 1.0                   # Azimuthal Angle, Phi step-size
-BkgWidth_TDW           = 1.0                   # Wedge1 opening angle based of peak width (Default is 1.5--- 150%)
-MinSigtoNoise_TDW      = 2.0                   # The intensity ratio between peak and background to detect a peak (Default is 2.0)
-
-
-# ## Body of Reduction - DO NOT CHANGE....
-
-# In[13]:
-
-
-from mantid.simpleapi import mtd
+# Body of Reduction - DO NOT CHANGE....
+from mantid.simpleapi import mtd  # noqa: E401
 if clearBuffer:
     mtd.clear()
 
-import numpy as np
-import warnings
+import numpy as np  # noqa: E401
+import warnings  # noqa: E401
 warnings.filterwarnings('ignore')
 
-import json
-get_ipython().run_line_magic('matplotlib', 'inline')
-import os
-from pprint import pprint as pretty_print
-import time
+# get_ipython().run_line_magic('matplotlib', 'inline')
+import os  # noqa: E401
+import time  # noqa: E401
 
 from drtsans.mono.biosans import (load_all_files, reduce_single_configuration, plot_reduction_output,
-                                  reduction_parameters, update_reduction_parameters, validate_reduction_parameters)
-from drtsans import stitch
-from drtsans.plots import plot_IQmod
-from drtsans.dataobjects import save_iqmod
+                                  reduction_parameters, update_reduction_parameters)  # noqa: E401
 
 # reduction parameters common to all the reduction runs to be carried out in this notebook
 common_configuration = {
@@ -133,11 +125,11 @@ common_configuration = {
         "darkWingFileName": dark_wfname,   
         "sensitivityMainFileName": sens_mfname,
         "sensitivityWingFileName": sens_wfname,
-        "defaultMask": [{'Pixel': '1-18,239-256'}, {'Bank': '18-24,42-48'}, {'Bank':'49','Tube':'1'}],
+        "defaultMask": [{'Pixel': '1-18,239-256'}, {'Bank': '18-24,42-48'}, {'Bank': '49', 'Tube': '1'}],
         'StandardAbsoluteScale': scalefac,
         "DBScalingBeamRadius": scaling_beam_radius,
         "mmRadiusForTransmission": "",
-        "absoluteScaleMethod":"standard",
+        "absoluteScaleMethod": "standard",
         "numMainQBins": Lin1DQbins_Main,
         "numWingQBins": Lin1DQbins_Wing,
         "numMainQxQyBins": Lin2DQxy_Main,
@@ -181,8 +173,9 @@ common_configuration = {
         "wedge2overlapStitchQmax": OL_range_wedge1[1],
     }
 }
+
 common_configuration_full = reduction_parameters(common_configuration, 'BIOSANS', validate=False)
-#pretty_print(common_configuration_full)
+# pretty_print(common_configuration_full)
 
 if len(backgrounds) == 1 and len(samples) > len(backgrounds):
     backgrounds = backgrounds*len(samples)
@@ -197,7 +190,7 @@ output_dir = base_output_directory
 if not overWrite:
     suffix = 0
     while os.path.exists(output_dir):
-        output_dir = base_output_directory[0,len(base_output_directory)-2]+"_"+str(suffix)+"/"
+        output_dir = base_output_directory[0, len(base_output_directory)-2] + "_" + str(suffix) + "/"
         suffix += 1
 
 if not timeSliceExpt and sample_identifier is not '':
@@ -211,14 +204,13 @@ if not timeSliceExpt and sample_identifier is not '':
         common_configuration_full = update_reduction_parameters(common_configuration_full,
                                                                 change_outputdir,
                                                                 validate=False)
-    for subfolder in ['1D','2D']:
-        output_folder=os.path.join(output_dir,subfolder)
+    for subfolder in ['1D', '2D']:
+        output_folder = os.path.join(output_dir, subfolder)
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
     
 start_time = time.time()
-for i in range(start_index-1,end_index):
-    
+for i in range(start_index-1, end_index):
     start_time_loop = time.time()
     if timeSliceExpt:
         output_dir = base_output_directory+"timeslice/t"+str(timeSliceDuration)+"/"+sample_names[i]+"/"        
@@ -230,12 +222,12 @@ for i in range(start_index-1,end_index):
         common_configuration_full = update_reduction_parameters(common_configuration_full,
                                                                 timeslice_outputdir,
                                                                 validate=False)
-        for subfolder in ['1D','2D']:
-            output_folder=os.path.join(output_dir,subfolder)
+        for subfolder in ['1D', '2D']:
+            output_folder = os.path.join(output_dir, subfolder)
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)        
 
-    print("Reducing...",samples[i],":",sample_names[i],'\n')
+    print("Reducing...", samples[i], ":", sample_names[i], '\n')
     run_data = {
         'sample': {
             'runNumber': samples[i],
@@ -250,27 +242,20 @@ for i in range(start_index-1,end_index):
     }
 
     # Update our common settings with the particulars of the current reduction
-    reduction_input = update_reduction_parameters(common_configuration_full,
-                                                        run_data,
-                                                        validate=True)
-    #pretty_print(reduction_input)    
+    reduction_input = update_reduction_parameters(common_configuration_full, run_data, validate=True)
+    # pretty_print(reduction_input)
     reduction_input['configuration']['WedgeMinAngles'] = wedge_min_angles
     reduction_input['configuration']['WedgeMaxAngles'] = wedge_max_angles
     loaded = load_all_files(reduction_input)
-    out = reduce_single_configuration(loaded,reduction_input)
+    out = reduce_single_configuration(loaded, reduction_input)
     plot_reduction_output(out, reduction_input)
 
-    print('\nloop_'+str(i+1)+": ",time.time()-start_time_loop)
-    
-    if np.remainder(i,refreshCycle)==0 and i > 0:
+    print('\nloop_'+str(i+1)+": ", time.time()-start_time_loop)
+
+    if np.remainder(i, refreshCycle) == 0 and i > 0:
         mtd.clear()
-            
-print('Total Time : ',time.time()-start_time)
 
+print('Total Time : ', time.time()-start_time)
 
-# In[2]:
-
-
-from mantid import mtd
+from mantid import mtd  # noqa: E401
 mtd.getObjectNames()
-

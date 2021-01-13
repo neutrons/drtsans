60901db7bfb70b7247a756a21ed6ff0e3958991e
Wenduo Zhou <zhouw@ornl.gov>
2021-01-12 21:39:44 -0500

    Refs #726. Added new classes to work with correction.
    Former-commit-id: 36ca9e772f29c04e3b9166e8509f98114d63b70e

---
+++ drtsans/tof/eqsans/correction_api.py
@@ -0,0 +1,69 @@
+# This module contains workflow algorithms and methods correct intensity and error of
+# sample and background data accounting wavelength-dependent incoherent inelastic scattering.
+# The workflow algorithms will be directly called by eqsans.api.
+
+
+# TODO - when python is upgraded to 3.7+, this class shall be wrapped as dataclass
+class CorrectionConfiguration:
+    """
+    A data class/structure to hold the parameters configured to do incoherence/inelastic
+    scattering correction
+    """
+    def __init__(self):
+
+        self._do_correction = False
+        self._select_min_incoherence = False
+        self._elastic_ref_run_setup = None
+
+    @property
+    def do_correction(self):
+        return self._do_correction
+
+    @property
+    def select_min_incoherence(self):
+        return self._select_min_incoherence
+
+    @select_min_incoherence.setter
+    def select_min_incoherence(self, flag):
+        self._select_min_incoherence = flag
+
+    def set_elastic_reference_run(self, reference_run_setup):
+        """Set elastic reference run reduction setup
+
+        Parameters
+        ----------
+        reference_run_setup: ElasticReferenceRunSetup
+            reduction setup
+
+        Returns
+        -------
+
+        """
+        self._elastic_ref_run_setup = reference_run_setup
+
+
+# TODO - when python is upgraded to 3.7+, this class shall be wrapped as dataclass
+class ElasticReferenceRunSetup:
+    """
+    A data class/structure to hold the reference run
+    """
+    def __init__(self, ref_run_number, trans_run_number=None, trans_value=None):
+        self.ref_run_number = ref_run_number
+        self.trans_run_number = trans_run_number
+        self.trans_value = trans_value
+
+
+def parse_correction_config(reduction_config):
+    """Parse correction configuration from reduction configuration
+
+    Parameters
+    ----------
+    reduction_config: ~dict
+        reduction configuration from JSON
+
+    Returns
+    -------
+    CorrectionConfiguration
+        incoherence/inelastic scattering correction configuration
+    """
+    raise NotImplementedError('Jesse!')

875dae8c8820807650575cd3663382514646f4c1
Steven Hahn <hahnse@ornl.gov>
2021-02-17 15:26:41 -0500

    stick with 2D array?
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>
    
    Former-commit-id: bb90b0fbf8598aa5a4dcfda9c3697eb485516ee8

--- drtsans/iq.py
+++ drtsans/iq.py
@@ -1037,16 +1037,16 @@ def _do_2d_no_weight_binning_wavelength(qx_array, dqx_array, qy_array, dqy_array
                                                                                        filtered_matrix[:, 3],
                                                                                        filtered_matrix[:, 4])
         # build up the final output
-        binned_iq_array = np.dstack([binned_iq_array, i_final_array]) \
+        binned_iq_array = np.concatenate((binned_iq_array, i_final_array),axis=1) \
             if binned_iq_array.size else i_final_array
-        binned_sigma_iq_array = np.dstack([binned_sigma_iq_array, sigma_final_array]) \
+        binned_sigma_iq_array = np.concatenate((binned_sigma_iq_array, sigma_final_array),axis=1) \
             if binned_sigma_iq_array.size else sigma_final_array
         if dqx_array is not None:
-            binned_dqx_array = np.dstack([binned_dqx_array, dqx_final_array]) \
+            binned_dqx_array = np.concatenate((binned_dqx_array, dqx_final_array),axis=1) \
                 if binned_dqx_array.size else dqx_final_array
-            binned_dqy_array = np.dstack([binned_dqy_array, dqy_final_array]) \
+            binned_dqy_array = np.concatenate((binned_dqy_array, dqy_final_array),axis=1) \
                 if binned_dqy_array.size else dqy_final_array
-        binned_wl_array = np.dstack([binned_wl_array, np.zeros_like(i_final_array) + wl_i]) \
+        binned_wl_array = np.concatenate((binned_wl_array, np.zeros_like(i_final_array) + wl_i),axis=1) \
             if binned_wl_array.size else np.zeros_like(i_final_array) + wl_i
     # END-FOR (wl_i)
 

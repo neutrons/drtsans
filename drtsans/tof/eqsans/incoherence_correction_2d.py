4e33fb939a73cb6bd7cb4e6a93af8d119edc1434
1im <mcgahajr@ornl.gov>
2021-02-23 13:41:39 -0500

    start q_subset_mask
    Former-commit-id: 75953f2bf03f2e1f113464cc0fa5976f6f21b131

---
+++ drtsans/tof/eqsans/incoherence_correction_2d.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def gen_q_subset_mask(i_of_q):
+    """Generate filtering array for q_subset from intensity vector for 2d case
+
+    Calculation of B requires usage of only qx, qy where all lambda exist,
+    so this function handles creating a boolean array for filtering
+
+    Parameters
+    ----------
+    i_of_q
+
+    Returns
+    -------
+
+    """
+    pass

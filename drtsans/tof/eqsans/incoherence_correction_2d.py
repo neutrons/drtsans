da0ce3de649b474fb34cfbaf906bb704e1505580
1im <mcgahajr@ornl.gov>
2021-02-23 16:44:24 -0500

    calculate b2d skeleton
    Former-commit-id: 2c4454d4c8231fb18b1b61aca2afb05fba3a3f58

--- drtsans/tof/eqsans/incoherence_correction_2d.py
+++ drtsans/tof/eqsans/incoherence_correction_2d.py
@@ -64,3 +64,26 @@ def gen_q_subset_mask(i_of_q, qx_len, qy_len, wavelength_len):
     _q_by_wavelength = i_of_q.intensity.reshape((qx_len*qy_len, wavelength_len))
     _mask_squeezed = np.all(np.isfinite(_q_by_wavelength), 1)
     return _mask_squeezed.repeat(wavelength_len)
+
+
+def calculate_b2d(i_of_q, q_subset_mask, wavelength_len, min_incoh=False):
+    """Calculates the 2D b parameters
+
+    Parameters
+    ----------
+    i_of_q: ~drtsans.dataobjects.IQazimuthal
+        Input reshaped I(Qx, Qy, wavelength)
+    q_subset_mask: ~numpy.ndarray
+        Boolean array defining q_subset
+    wavelength_len: int
+        Number of unique wavelength values
+    min_incoh: bool
+        Whether to select minimum b wavelength for b recalculation
+
+    Returns
+    -------
+    ~numpy.ndarray
+        calculated 2D b values
+
+    """
+    pass

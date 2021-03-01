4a205ac9bf20861f44cab2c1e808aaaaae9b461b
Steven Hahn <hahnse@ornl.gov>
2021-03-01 16:51:45 -0500

    flake8 warning
    Signed-off-by: Steven Hahn <hahnse@ornl.gov>
    
    Former-commit-id: 2db3d9aeca93f25822dd9605aaa3fd4492b98f01

--- tests/unit/new/drtsans/test_i_of_q_2d_binning.py
+++ tests/unit/new/drtsans/test_i_of_q_2d_binning.py
@@ -105,7 +105,7 @@ def test_2d_bin_no_sub_no_wt_wavelength():
     test_i_q = IQazimuthal(intensity=intensities, error=sigmas, qx=qx_array, qy=qy_array,
                            delta_qx=dqx_array, delta_qy=dqy_array, wavelength=wl_array)
     binned_iq_2d = bin_intensity_into_q2d(test_i_q, qx_bins, qy_bins, BinningMethod.NOWEIGHT)
- 
+
     # Verify Qx and Qy
     assert qx_bins.centers[1] == pytest.approx(-0.003254, abs=1.E-6), 'Qx is not correct'
     assert qy_bins.centers[1] == pytest.approx(-0.001713, abs=1.E-6), 'Qy is not correct'

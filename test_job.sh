45ee4d5d81e67bb2d19e8cdbd9d40d8952853e05
Zhang, Chen <zhangc@ornl.gov>
2022-07-06 14:16:49 +0000

    [SANS910] Upgrade base image to use conda package mantidworkbench
    Former-commit-id: 24ca4ceff4616ab2cbc8fa70a22f670a1e032d55

--- test_job.sh
+++ test_job.sh
@@ -4,7 +4,8 @@ set -x
 
 TEST_SCOPE=$1  # 'unit' or 'integration'
 
-source activate mantid
+# the default environment in bash should already be drtsans-dev
+source activate drtsans-dev
 cd /opt/sans-backend
 echo "Writing tests results to $(pwd)/${TEST_SCOPE}_test_results.xml"
 pytest --dist loadscope -v /opt/sans-backend/tests/${TEST_SCOPE} -n 4 --junitxml=./${TEST_SCOPE}_test_results.xml

82ff46c267605943d02bc1702b8b294f0212a626
Pete Peterson <petersonpf@ornl.gov>
2022-09-08 10:56:56 -0400

    Change when conda packages are built
    Former-commit-id: 73f6496b94607f7d0de4832767dbcab5c7bd31d8

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -9,7 +9,12 @@ conda build --output-folder . . -c neutrons -c mantid/label/nightly
 # Verify
 conda-verify ./linux-64/drtsans-*.tar.bz2
 
-# Deploy to Anaconda
-CONDA_LABEL="main"
-echo pushing $CI_COMMIT_REF_SLUG with label $CONDA_LABEL
-anaconda upload --label $CONDA_LABEL ./linux-64/drtsans-*.tar.bz2
+# Deploy tags to anaconda.org
+if [ -n "${CI_COMMIT_TAG}" ]; then
+    CONDA_LABEL="main"
+    if [ "${CI_COMMIT_TAG}" = "*rc*" ]; then
+        CONDA_LABEL="rc"
+    fi
+    echo pushing $CI_COMMIT_REF_SLUG with label $CONDA_LABEL
+    anaconda upload --label $CONDA_LABEL ./linux-64/drtsans-*.tar.bz2
+fi

819f5216c464bd6bc02426b590312bac9f54580a
Pete Peterson <petersonpf@ornl.gov>
2023-05-26 10:45:22 -0400

    Try another way of setting the token
    Former-commit-id: 0c97a4c7519547b654d89fe2d721bc55c7492bf6

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -19,12 +19,16 @@ conda-verify ./noarch/drtsans-*.tar.bz2
 
 # Deploy tags to anaconda.org
 if [ -n "${CI_COMMIT_TAG}" ]; then
+    if [ -z "${ANACONDA_TOKEN}" ]; then
+	echo "ANACONDA_TOKEN is not set"
+	exit -1
+    fi
     CONDA_LABEL="main"
     if [ "${CI_COMMIT_TAG}" = "*rc*" ]; then
         CONDA_LABEL="rc"
     fi
     echo pushing $CI_COMMIT_REF_SLUG with label $CONDA_LABEL
-    anaconda upload --label $CONDA_LABEL ./noarch/drtsans-*.tar.bz2
+    anaconda --token ${ANACONDA_TOKEN} upload --label $CONDA_LABEL ./noarch/drtsans-*.tar.bz2
 else
     echo "Not publishing package to anaconda.org"
 fi

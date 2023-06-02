86e18b51cbd192a37f682d9c98b6b5c9d256423f
Jose Borreguero <borreguero@gmail.com>
2023-06-01 21:02:59 -0400

    assert the numer of events in the central tube
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: 06cbb04597b74b8b6bc74d56de3b8b21dd5b9545

--- tests/unit/new/drtsans/mono/biosans/test_simulated_events.py
+++ tests/unit/new/drtsans/mono/biosans/test_simulated_events.py
@@ -7,6 +7,7 @@ from drtsans.mono.biosans.simulated_events import (
     insert_events_sin_squared,
 )
 from drtsans.mono.biosans.geometry import (
+    PIXELS_IN_TUBE,
     set_angle_wing_detector,
     set_angle_midrange_detector,
     set_position_south_detector,
@@ -14,10 +15,18 @@ from drtsans.mono.biosans.geometry import (
 from drtsans.samplelogs import SampleLogs
 
 # third party imports
-from mantid.simpleapi import LoadEmptyInstrument, SaveNexus
+from mantid.api import mtd
+from mantid.simpleapi import DeleteWorkspace, LoadEmptyInstrument
 import pytest
 
 # standard imports
+
+
+def _event_count_in_central_tube(input_workspace):
+    r"""event count in tube 1 of bank13 (middle tube in the South detector)"""
+    workspace = mtd[str(input_workspace)]
+    first_id = 24276
+    return sum([workspace.getSpectrum(first_id + i).getNumberEvents() for i in range(PIXELS_IN_TUBE)])
 
 
 def test_update_idf(fetch_idf):
@@ -29,62 +38,81 @@ def test_update_idf(fetch_idf):
 
 
 def test_insert_events_isotropic(fetch_idf):
-    workspace = LoadEmptyInstrument(
-        InstrumentName="BIOSANS", Filename=fetch_idf("BIOSANS_Definition.xml"), MakeEventWorkspace=True
+    LoadEmptyInstrument(
+        InstrumentName="BIOSANS",
+        Filename=fetch_idf("BIOSANS_Definition.xml"),
+        OutputWorkspace="test_insert_events_isotropic",
+        MakeEventWorkspace=True,
     )
-    SampleLogs(workspace).insert("start_time", "2023-08-01 00:00:00")
-    set_position_south_detector(workspace, distance=5.0)  # meters
+    SampleLogs("test_insert_events_isotropic").insert("start_time", "2023-08-01 00:00:00")
+    set_position_south_detector("test_insert_events_isotropic", distance=5.0)  # meters
     insert_events_isotropic(
-        workspace,
+        "test_insert_events_isotropic",
         counts_per_pixel=100,
         components=["detector1", "wing_detector", "midrange_detector"],
         efficiencies=[1.0, 0.1, 1.0],
     )
+    assert _event_count_in_central_tube("test_insert_events_isotropic") == 256
+    DeleteWorkspace("test_insert_events_isotropic")
 
 
 def test_insert_events_ring(fetch_idf):
-    workspace = LoadEmptyInstrument(
-        InstrumentName="BIOSANS", Filename=fetch_idf("BIOSANS_Definition.xml"), MakeEventWorkspace=True
+    LoadEmptyInstrument(
+        InstrumentName="BIOSANS",
+        Filename=fetch_idf("BIOSANS_Definition.xml"),
+        OutputWorkspace="test_insert_events_ring",
+        MakeEventWorkspace=True,
     )
-    SampleLogs(workspace).insert("start_time", "2023-08-01 00:00:00")
-    set_position_south_detector(workspace, distance=5.0)  # meters
-    set_angle_midrange_detector(workspace, angle=5.0)  # degrees
-    set_angle_wing_detector(workspace, angle=4.0)  # degrees
+    SampleLogs("test_insert_events_ring").insert("start_time", "2023-08-01 00:00:00")
+    set_position_south_detector("test_insert_events_ring", distance=5.0)  # meters
+    set_angle_midrange_detector("test_insert_events_ring", angle=5.0)  # degrees
+    set_angle_wing_detector("test_insert_events_ring", angle=4.0)  # degrees
     insert_events_ring(
-        workspace,
+        "test_insert_events_ring",
         twotheta_center=6.0,
         twotheta_dev=1.0,
         max_counts_in_pixel=100,
         components=["detector1", "wing_detector", "midrange_detector"],
         efficiencies=[1.0, 0.1, 1.0],
     )
+    assert _event_count_in_central_tube("test_insert_events_ring") == 34
+    DeleteWorkspace("test_insert_events_ring")
 
 
 def test_insert_events_sin_squared(fetch_idf):
-    workspace = LoadEmptyInstrument(
-        InstrumentName="BIOSANS", Filename=fetch_idf("BIOSANS_Definition.xml"), MakeEventWorkspace=True
+    LoadEmptyInstrument(
+        InstrumentName="BIOSANS",
+        Filename=fetch_idf("BIOSANS_Definition.xml"),
+        OutputWorkspace="test_insert_events_sin_squared",
+        MakeEventWorkspace=True,
     )
-    SampleLogs(workspace).insert("start_time", "2023-08-01 00:00:00")
-    set_position_south_detector(workspace, distance=5.0)  # meters
-    set_angle_midrange_detector(workspace, angle=5.0)  # degrees
-    set_angle_wing_detector(workspace, angle=15.0)  # degrees
+    SampleLogs("test_insert_events_sin_squared").insert("start_time", "2023-08-01 00:00:00")
+    set_position_south_detector("test_insert_events_sin_squared", distance=5.0)  # meters
+    set_angle_midrange_detector("test_insert_events_sin_squared", angle=5.0)  # degrees
+    set_angle_wing_detector("test_insert_events_sin_squared", angle=15.0)  # degrees
     insert_events_sin_squared(
-        workspace,
+        "test_insert_events_sin_squared",
         period=16.0,
         max_counts_in_pixel=100,
         components=["detector1", "wing_detector", "midrange_detector"],
         efficiencies=[1.0, 0.1, 1.0],
     )
+    assert _event_count_in_central_tube("test_insert_events_sin_squared") == 170
+    DeleteWorkspace("test_insert_events_sin_squared")
 
 
 def test_insert_events_offset_center(fetch_idf):
-    workspace = LoadEmptyInstrument(
-        InstrumentName="BIOSANS", Filename=fetch_idf("BIOSANS_Definition.xml"), MakeEventWorkspace=True
+    LoadEmptyInstrument(
+        InstrumentName="BIOSANS",
+        Filename=fetch_idf("BIOSANS_Definition.xml"),
+        OutputWorkspace="test_insert_events_offset_center",
+        MakeEventWorkspace=True,
     )
-    SampleLogs(workspace).insert("start_time", "2023-08-01 00:00:00")
-    set_position_south_detector(workspace, distance=5.0)  # meters
-    insert_events_offset_center(workspace, detector1_shift_x=-0.05, detector1_shift_y=-0.05)
-    SaveNexus(InputWorkspace=workspace, Filename="/tmp/test_insert_events_offset_center.nxs")
+    SampleLogs("test_insert_events_offset_center").insert("start_time", "2023-08-01 00:00:00")
+    set_position_south_detector("test_insert_events_offset_center", distance=5.0)  # meters
+    insert_events_offset_center("test_insert_events_offset_center", detector1_shift_x=-0.05, detector1_shift_y=-0.05)
+    assert _event_count_in_central_tube("test_insert_events_offset_center") == 4282
+    DeleteWorkspace("test_insert_events_offset_center")
 
 
 if __name__ == "__main__":

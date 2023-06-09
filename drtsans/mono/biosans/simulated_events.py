5a4ffbdef1095471eba541ae2d28f7c68e61835e
Jose Borreguero <borreguero@gmail.com>
2023-06-08 21:07:20 -0400

    set RewriteSpectraMap to False
    also allow for extra options to Mantid algorithm LoadInstrument
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    Former-commit-id: a1d2b59ccbe0fd04eb3b700aa2c105b452e99bea

--- drtsans/mono/biosans/simulated_events.py
+++ drtsans/mono/biosans/simulated_events.py
@@ -20,7 +20,7 @@ import math
 import tempfile
 
 
-def update_idf(input_workspace):
+def update_idf(input_workspace, **load_instrument_kwargs):
     r"""
     Download the latest BIOSANS IDF from the Mantid GitHub repository and load it into a workspace.
 
@@ -29,14 +29,20 @@ def update_idf(input_workspace):
     input_workspace : str, ~Mantid.api.Workspace
         The workspace to load the instrument into.
 
+    load_instrument_kwargs : dict
+        Keyword arguments to pass to Mantid algorithm LoadInstrument.
+
     Returns
     -------
     ~Mantid.api.Workspace
         The workspace with the instrument loaded.
     """
+    kwargs = dict(RewriteSpectraMap=False)  # default options to Mantid algorithm LoadInstrument
+    kwargs.update(load_instrument_kwargs)
+
     with tempfile.TemporaryDirectory() as temp_dir:
         idf = fetch_idf("BIOSANS_Definition.xml", output_directory=temp_dir)
-        LoadInstrument(Workspace=input_workspace, Filename=idf, RewriteSpectraMap=True)
+        LoadInstrument(Workspace=input_workspace, Filename=idf, **kwargs)
     return mtd[str(input_workspace)]
 
 

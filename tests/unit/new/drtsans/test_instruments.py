eb627ff15eb05a1d02df6b7ebb58c86e7fe3ab3e
Borreguero Calvo, Jose <borreguerojm@ornl.gov>
2023-06-27 12:58:16 +0000

    Update the instrument of a workspace to the latest instrument
    Former-commit-id: 2d523d0873fe1588ed2203a3f4a871d29a082a53

--- tests/unit/new/drtsans/test_instruments.py
+++ tests/unit/new/drtsans/test_instruments.py
@@ -1,15 +1,23 @@
-import pytest
-
-from mantid.simpleapi import CreateWorkspace, DeleteWorkspace
-
-from drtsans.settings import unique_workspace_dundername
+# local imports
 from drtsans.instruments import (
     InstrumentEnumName,
+    copy_to_newest_instrument,
     extract_run_number,
+    empty_instrument_workspace,
     instrument_enum_name,
     is_time_of_flight,
-    empty_instrument_workspace,
 )
+from drtsans.mono.biosans.geometry import get_angle_wing_detector, get_position_south_detector
+from drtsans.settings import amend_config, unique_workspace_dundername
+
+# third party imports
+from mantid.dataobjects import EventWorkspace
+from mantid.simpleapi import CreateWorkspace, DeleteWorkspace, LoadEmptyInstrument, LoadEventNexus
+from numpy.testing import assert_almost_equal
+import pytest
+
+# standard imports
+#
 
 
 class TestInstrumentEnumName:
@@ -64,6 +72,43 @@ def test_empty_instrument_workspace():
     assert workspace.getNumberHistograms() == len(detector_ids)
     assert list(workspace.getSpectrum(0).getDetectorIDs()) == [0]
     DeleteWorkspace(workspace)
+
+
+def test_copy_to_newest_instrument(fetch_idf, reference_dir, clean_workspace):
+    #
+    # assert that spectra originally assigned to monitors is respected
+    workspace1 = LoadEmptyInstrument(
+        Filename=fetch_idf("BIOSANS_Definition_2019_2023.xml"), OutputWorkspace=unique_workspace_dundername()
+    )
+    clean_workspace(workspace1)
+    old_histogram_count = workspace1.getNumberHistograms()
+    workspace1 = copy_to_newest_instrument(workspace1)
+    assert workspace1.getSpectrum(0).getDetectorIDs()[0] < 0
+    assert workspace1.getNumberHistograms() > old_histogram_count
+    #
+    # assert that event workspace is preserved
+    workspace2 = LoadEmptyInstrument(
+        Filename=fetch_idf("BIOSANS_Definition_2019_2023.xml"),
+        OutputWorkspace=unique_workspace_dundername(),
+        MakeEventWorkspace=True,
+    )
+    clean_workspace(workspace2)
+    old_histogram_count = workspace2.getNumberHistograms()
+    workspace2 = copy_to_newest_instrument(workspace2)
+    assert isinstance(workspace2, EventWorkspace)
+    assert workspace2.getNumberHistograms() > old_histogram_count
+    #
+    # assert intensities and detector positions
+    with amend_config(new_config={"default.instrument": "CG3"}, data_dir=reference_dir.new.biosans):
+        workspace3 = LoadEventNexus(Filename="1322", OutputWorkspace=unique_workspace_dundername())
+    clean_workspace(workspace3)
+    pixel_counts = workspace3.getSpectrum(24956).getNumberEvents()
+    position_detector1 = get_position_south_detector(workspace3)
+    angle_wing = get_angle_wing_detector(workspace3)
+    workspace3 = copy_to_newest_instrument(workspace3)
+    assert workspace3.getSpectrum(24956).getNumberEvents() == pixel_counts
+    assert_almost_equal(get_position_south_detector(workspace3), position_detector1, decimal=4)
+    assert_almost_equal(get_angle_wing_detector(workspace3), angle_wing, decimal=3)
 
 
 if __name__ == "__main__":

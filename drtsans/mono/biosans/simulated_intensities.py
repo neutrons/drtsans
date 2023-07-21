5932365e672605d8e8568a1cf28a345253f44d02
Borreguero Calvo, Jose <borreguerojm@ornl.gov>
2023-07-21 13:59:04 +0000

    Use pixel ranges instead of TubeCollection in clone_component_intensities
    Former-commit-id: dbe1772eba4775cd352e987d4948d7bbf046fc63

--- drtsans/mono/biosans/simulated_intensities.py
+++ drtsans/mono/biosans/simulated_intensities.py
@@ -1,45 +1,53 @@
 from drtsans.instruments import fetch_idf, empty_instrument_workspace
 from drtsans.mono.biosans.geometry import has_midrange_detector
-from drtsans.tubecollection import TubeCollection
+from drtsans.geometry import spectrum_info_ranges
 
 # third party imports
 from mantid.api import mtd
+from mantid.dataobjects import Workspace2D
 from mantid.simpleapi import CopyLogs
+
+# standard imports
+from typing import Union
 
 
 def clone_component_intensities(
-    input_workspace,
-    output_workspace,
-    input_component="wing_detector",
-    output_component="midrange_detector",
-    copy_logs=True,
-):
+    input_workspace: Union[str, Workspace2D],
+    output_workspace: str = None,
+    input_component: str = "wing_detector",
+    output_component: str = "midrange_detector",
+    copy_logs: bool = True,
+) -> Workspace2D:
     r"""
     Clone intensities into the detector pixels of a component from another component.
 
     Parameters
     ----------
-    input_workspace : str, ~Mantid.api.Workspace
+    input_workspace
         The workspace to copy intensities from.
-
-    output_workspace : str
+    output_workspace
         If :py:obj:`None`, the name of the input_workspace will be used, thus overwriting the input workspace.
-
-    input_component : str
-        The component to copy intensities from. Default: "wing_detector".
-
-    output_component : str
-        The component to copy intensities to. Default: "midrange_detector".
-
-    copy_logs : bool
+    input_component
+        The component to copy intensities from.
+    output_component
+        The component to copy intensities to.
+    copy_logs
         If True, the sample logs will be copied to the output workspace
 
     Returns
     -------
-    ~Mantid.api.Workspace
-        The workspace with intensities inserted.
+    The workspace with intensities inserted.
+
+    Raises
+    ------
+    ValueError
+        If ``input_component=="midrange_detector" but ``input_workspace`` lacks the midrange detector.
+    ValueError
+        If ``output_component=="midrange_detector" but ``output_workspace`` lacks the midrange detector.
     """
-    input_workspace = str(input_workspace)
+    if input_component == "midrange_detector" and not has_midrange_detector(input_workspace):
+        raise ValueError("No midrange detector in input workspace")
+
     if output_workspace is None:
         output_workspace = str(input_workspace)
     else:
@@ -52,30 +60,17 @@ def clone_component_intensities(
         if copy_logs:
             CopyLogs(input_workspace, output_workspace)
 
-    if input_component == "midrange_detector" and not has_midrange_detector(input_workspace):
-        raise ValueError("No midrange detector in input workspace")
+    if output_component == "midrange_detector" and not has_midrange_detector(output_workspace):
+        raise ValueError("No midrange detector in output workspace")
 
-    input_collection = TubeCollection(input_workspace, input_component)
-    output_collection = TubeCollection(output_workspace, output_component)
-    # verify that the number of pixels and tubes is greater or equal in input_component
-    if len(input_collection) < len(output_collection):
-        raise ValueError(f"Not enough tubes to copy {len(input_collection)} < {len(output_collection)}.")
-    if len(input_collection[0]) < len(output_collection[0]):
-        raise ValueError(f"Not enough tube pixels to copy {len(input_collection[0])} < {len(output_collection[0])}.")
+    in_first, in_last = spectrum_info_ranges(input_workspace, input_component)
+    out_first, out_last = spectrum_info_ranges(output_workspace, output_component)
+    assert in_last - in_first >= out_last - out_first, "Not enough pixels to clone"
 
-    # copy data tube for tube, pixel for pixel
-    for input_tube, output_tube in zip(input_collection, output_collection):
-        input_pixel_indexes = input_tube.spectrum_info_index
-        output_pixel_indexes = output_tube.spectrum_info_index
-        output_dataX = mtd[str(output_workspace)].dataX
-        output_dataY = mtd[str(output_workspace)].dataY
-        output_dataE = mtd[str(output_workspace)].dataE
-        input_readX = mtd[str(input_workspace)].readX
-        input_readY = mtd[str(input_workspace)].readY
-        input_readE = mtd[str(input_workspace)].readE
-        for input_pixel, output_pixel in zip(input_pixel_indexes, output_pixel_indexes):
-            output_dataX(output_pixel)[:] = input_readX(input_pixel)
-            output_dataY(output_pixel)[:] = input_readY(input_pixel)
-            output_dataE(output_pixel)[:] = input_readE(input_pixel)
+    readers = [getattr(mtd[str(input_workspace)], reader) for reader in ("readX", "readY", "readE")]
+    writers = [getattr(mtd[str(output_workspace)], writer) for writer in ("dataX", "dataY", "dataE")]
+    for in_wi, out_wi in zip(range(in_first, in_last), range(out_first, out_last)):
+        for reader, writer in zip(readers, writers):
+            writer(out_wi)[:] = reader(in_wi)
 
     return mtd[str(output_workspace)]

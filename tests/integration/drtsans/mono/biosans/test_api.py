bee24a8e039d9669b7aac282028b0bb4d8e891bd
Chen Zhang <chenzhang8722@gmail.com>
2023-10-06 11:15:39 -0400

    add additional patch
    Former-commit-id: 6eb505564bbd0e28b25f641d304c539942c76a30

--- tests/integration/drtsans/mono/biosans/test_api.py
+++ tests/integration/drtsans/mono/biosans/test_api.py
@@ -558,6 +558,7 @@ def test_reduce_single_configuration_synthetic_dataset(mock_monitor_counts, bios
     assert np.nanmax(second_curve) == pytest.approx(23099, rel=1e-3)
 
 
+@pytest.mark.datarepo
 @mock_patch("drtsans.load.LoadEventNexus", new=_mock_LoadEventNexus)
 @mock_patch("drtsans.load.__monitor_counts")
 def test_reduce_single_configuration_with_wedges_synthetic_dataset(
@@ -661,6 +662,7 @@ def test_reduce_single_configuration_with_wedges_synthetic_dataset(
     assert np.nanmax(wedge2_second_curve) == pytest.approx(18478, rel=1e-3)
 
 
+@pytest.mark.datarepo
 @mock_patch("drtsans.load.LoadEventNexus", new=_mock_LoadEventNexus)
 @mock_patch("drtsans.load.__monitor_counts")
 def test_reduce_single_configuration_ignore_midrange(mock_monitor_counts, biosans_synthetic_dataset, temp_directory):

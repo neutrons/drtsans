a44bcdc85467e4676354f913a167561776a5f984
Marie Backman <backmanma@ornl.gov>
2023-10-09 11:21:09 -0400

    add back skip if HFIR file not found
    Former-commit-id: adfebb45c95433fd7047197ce3be3c24cb22b58c

--- tests/integration/drtsans/test_gaussian_fitting.py
+++ tests/integration/drtsans/test_gaussian_fitting.py
@@ -5,6 +5,7 @@ from drtsans.mono import gpsans as sans
 import drtsans
 import lmfit
 import pytest
+import os
 
 
 # defining 2D Gaussian fitting functions
@@ -21,6 +22,9 @@ def Gaussian2D(x1, y1, amp, sigma_x, sigma_y, theta, x0, y0):
 def test_gaussian_fit(has_sns_mount):
     if not has_sns_mount:
         pytest.skip("SNS mount is not available")
+
+    if not os.path.exists("/HFIR/CG2/IPTS-26004/nexus/CG2_13078.nxs.h5"):
+        pytest.skip("Required data is not available")
 
     flood_file = "/HFIR/CG2/shared/drt_sensitivity/sens_c489_bar.nxs"
     # Find beam center for main detector

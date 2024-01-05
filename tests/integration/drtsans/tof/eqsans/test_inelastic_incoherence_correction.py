4e16a95426fe0dcb49d8d673673bb14bef72c71c
Ross Whitfield <whitfieldre1@ornl.gov>
2024-01-05 11:22:38 +1100

    Update json parse tests to use files from data repo
    Former-commit-id: 25a8423c9b7664160bde948de90903da34206122

--- tests/integration/drtsans/tof/eqsans/test_inelastic_incoherence_correction.py
+++ tests/integration/drtsans/tof/eqsans/test_inelastic_incoherence_correction.py
@@ -20,25 +20,22 @@ import glob
 from typing import Tuple, Dict
 
 
-@pytest.mark.mount_eqsans
-def test_parse_json(has_sns_mount, reference_dir):
+def test_parse_json(datarepo_dir):
     """Test the JSON to dictionary"""
-    if not has_sns_mount:
-        pytest.skip("SNS mount is not available")
 
-    elastic_reference_run = "124680"
+    elastic_reference_run = "92160"
     elastic_reference_bkgd_run = ""
     # Specify JSON input
     reduction_input = {
         "instrumentName": "EQSANS",
         "iptsNumber": "26015",
-        "sample": {"runNumber": "115363", "thickness": "1.0"},
+        "sample": {"runNumber": "105428", "thickness": "1.0"},
         "background": {
-            "runNumber": "115361",
-            "transmission": {"runNumber": "115357", "value": ""},
+            "runNumber": "104088",
+            "transmission": {"runNumber": "101595", "value": ""},
         },
-        "emptyTransmission": {"runNumber": "115356", "value": ""},
-        "beamCenter": {"runNumber": "115356"},
+        "emptyTransmission": {"runNumber": "101595", "value": ""},
+        "beamCenter": {"runNumber": "92353"},
         "outputFileName": "test_wavelength_step",
         "configuration": {
             "outputDir": "/path/to/nowhere",
@@ -46,11 +43,7 @@ def test_parse_json(has_sns_mount, reference_dir):
             "wavelengthStepType": "constant Delta lambda/lambda",
             "sampleApertureSize": "10",
             "fluxMonitorRatioFile": None,
-            "sensitivityFileName": (
-                "/SNS/EQSANS/"
-                "shared/NeXusFiles/EQSANS/"
-                "2020A_mp/Sensitivity_patched_thinPMMA_2o5m_113514_mantid.nxs"
-            ),
+            "sensitivityFileName": "/bin/true",
             "numQBins": "100",
             "WedgeMinAngles": "-30, 60",
             "WedgeMaxAngles": "30, 120",
@@ -67,11 +60,13 @@ def test_parse_json(has_sns_mount, reference_dir):
                 "transmission": {"runNumber": "", "value": "0.9"},
             },
             "selectMinIncoh": True,
+            "maskFileName": "/bin/true",
+            "darkFileName": "/bin/true",
         },
     }
 
     # Validate
-    with amend_config(data_dir=reference_dir.eqsans):
+    with amend_config(data_dir=datarepo_dir.eqsans):
         input_config = reduction_parameters(reduction_input)
 
     # Check that inelastic incoherence config items were parsed
@@ -85,31 +80,28 @@ def test_parse_json(has_sns_mount, reference_dir):
     correction = parse_correction_config(input_config)
     assert correction.do_correction
     assert correction.elastic_reference
-    assert correction.elastic_reference.run_number == "124680"
+    assert correction.elastic_reference.run_number == "92160"
     assert correction.elastic_reference.thickness == 1.0
     assert correction.elastic_reference.transmission_value == 0.89
     assert correction.elastic_reference.background_run_number is None
 
 
-@pytest.mark.mount_eqsans
-def test_parse_invalid_json(has_sns_mount):
+def test_parse_invalid_json(datarepo_dir):
     """Test the JSON to dictionary"""
-    if not has_sns_mount:
-        pytest.skip("SNS mount is not available")
 
     invalid_run_num = "260159121"
-    valid_run_num = "115363"
+    valid_run_num = "85550"
     # Specify JSON input
     reduction_input = {
         "instrumentName": "EQSANS",
         "iptsNumber": "26015",
-        "sample": {"runNumber": "115363", "thickness": "1.0"},
+        "sample": {"runNumber": "85550", "thickness": "1.0"},
         "background": {
-            "runNumber": "115361",
-            "transmission": {"runNumber": "115357", "value": ""},
+            "runNumber": "86217",
+            "transmission": {"runNumber": "88565", "value": ""},
         },
-        "emptyTransmission": {"runNumber": "115356", "value": ""},
-        "beamCenter": {"runNumber": "115356"},
+        "emptyTransmission": {"runNumber": "88901", "value": ""},
+        "beamCenter": {"runNumber": "88973"},
         "outputFileName": "test_wavelength_step",
         "configuration": {
             "outputDir": "/path/to/nowhere",
@@ -117,11 +109,7 @@ def test_parse_invalid_json(has_sns_mount):
             "wavelengthStepType": "constant Delta lambda/lambda",
             "sampleApertureSize": "10",
             "fluxMonitorRatioFile": None,
-            "sensitivityFileName": (
-                "/SNS/EQSANS/"
-                "shared/NeXusFiles/EQSANS/"
-                "2020A_mp/Sensitivity_patched_thinPMMA_2o5m_113514_mantid.nxs"
-            ),
+            "sensitivityFileName": "/bin/true",
             "numQBins": "100",
             "WedgeMinAngles": "-30, 60",
             "WedgeMaxAngles": "30, 120",
@@ -138,170 +126,30 @@ def test_parse_invalid_json(has_sns_mount):
                 "transmission": {"runNumber": valid_run_num, "value": "0.9"},
             },
             "selectMinIncoh": True,
+            "maskFileName": "/bin/true",
+            "darkFileName": "/bin/true",
         },
     }
 
     # Validate
-    with pytest.raises(ValidationError):
+    with pytest.raises(ValidationError) as excinfo:
         # expect to fail as elastic reference run 260159121 does not exist
-        reduction_parameters(reduction_input)
+        with amend_config(data_dir=datarepo_dir.eqsans):
+            reduction_parameters(reduction_input)
+
+    assert "Cannot find events file associated to 260159121" in str(excinfo.value)
 
     # Respecify to use a valid run
     # json_str.replace('260159121', '26015')
     reduction_input["configuration"]["elasticReference"]["runNumber"] = valid_run_num
     # Defaults and Validate
-    input_config = reduction_parameters(reduction_input)
+    with amend_config(data_dir=datarepo_dir.eqsans):
+        input_config = reduction_parameters(reduction_input)
 
     # Check that inelastic incoherence config items were parsed
     assert input_config["configuration"].get("fitInelasticIncoh")
     assert input_config["configuration"]["elasticReference"].get("runNumber") == valid_run_num
     assert input_config["configuration"].get("selectMinIncoh")
-
-
-def generate_configuration_with_correction(output_dir: str = "/tmp/") -> Dict:
-    """Generate configuration dictionary (JSON) from test 2 in issue 689
-
-    Source: https://code.ornl.gov/sns-hfir-scse/sans/sans-backend/-/issues/689  *Test 2*
-
-    Returns
-    -------
-    ~dict
-        Reduction donfiguration JSON
-
-    """
-    reduction_configuration = {
-        "schemaStamp": "2020-05-21T17:18:11.528041",
-        "instrumentName": "EQSANS",
-        "iptsNumber": "24876",
-        "sample": {
-            "runNumber": "113915",
-            "thickness": 1,
-            "transmission": {"runNumber": "113914", "value": ""},
-        },
-        "background": {
-            "runNumber": "113919",
-            "transmission": {"runNumber": "113918", "value": ""},
-        },
-        "emptyTransmission": {"runNumber": "113682", "value": None},
-        "beamCenter": {"runNumber": "113682"},
-        "outputFileName": "water65D_2o5m2o5a_full",
-        "configuration": {
-            "outputDir": f"{output_dir}",
-            "useTimeSlice": False,
-            "timeSliceInterval": 300,
-            "useLogSlice": False,
-            "logSliceName": None,
-            "logSliceInterval": 10,
-            "cutTOFmin": 500,
-            "cutTOFmax": 2000,
-            "wavelengthStep": 0.1,
-            "wavelengthStepType": "constant Delta lambda",
-            "sampleOffset": 314.5,
-            "useDetectorOffset": True,
-            "detectorOffset": 80,
-            "sampleApertureSize": 10,
-            "sourceApertureDiameter": None,
-            "usePixelCalibration": None,
-            "maskFileName": "/SNS/EQSANS/shared/NeXusFiles/EQSANS/2020A_mp/mask_4m_extended.nxs",
-            "useDefaultMask": True,
-            "defaultMask": None,
-            "useMaskBackTubes": False,
-            "darkFileName": "/SNS/EQSANS/shared/NeXusFiles/EQSANS/2020A_mp/EQSANS_113569.nxs.h5",
-            "normalization": "Total charge",
-            "fluxMonitorRatioFile": None,
-            "beamFluxFileName": "/SNS/EQSANS/shared/instrument_configuration/bl6_flux_at_sample",
-            "sensitivityFileName": "/SNS/EQSANS/shared/NeXusFiles/EQSANS/2020A_mp/"
-            "Sensitivity_patched_thinPMMA_2o5m_113514_mantid.nxs",
-            "useSolidAngleCorrection": True,
-            "useThetaDepTransCorrection": True,
-            "mmRadiusForTransmission": 25,
-            "absoluteScaleMethod": "standard",
-            "StandardAbsoluteScale": 1.0,
-            "numQxQyBins": 80,
-            "1DQbinType": "scalar",
-            "QbinType": "linear",
-            "numQBins": 100,
-            "LogQBinsPerDecade": None,
-            "useLogQBinsDecadeCenter": False,
-            "useLogQBinsEvenDecade": False,
-            "WedgeMinAngles": "-30, 60",
-            "WedgeMaxAngles": "30, 120",
-            "autoWedgeQmin": None,
-            "autoWedgeQmax": None,
-            "autoWedgeQdelta": None,
-            "autoWedgeAzimuthalDelta": None,
-            "autoWedgePeakWidth": None,
-            "autoWedgeBackgroundWidth": None,
-            "autoWedgeSignalToNoiseMin": None,
-            "AnnularAngleBin": 5.0,
-            "Qmin": None,
-            "Qmax": None,
-            "useErrorWeighting": True,
-            "smearingPixelSizeX": None,
-            "smearingPixelSizeY": None,
-            "useSubpixels": None,
-            "subpixelsX": None,
-            "subpixelsY": None,
-            "useSliceIDxAsSuffix": None,
-            # inelastic/incoherent correction
-            "fitInelasticIncoh": True,
-            "elasticReference": {
-                "runNumber": None,
-                "thickness": "1.0",
-                "transmission": {"runNumber": None, "value": "0.9"},
-            },
-            "elasticReferenceBkgd": {
-                "runNumber": None,
-                "transmission": {"runNumber": None, "value": "0.9"},
-            },
-            "selectMinIncoh": True,
-        },
-    }
-
-    return reduction_configuration
-
-
-# @pytest.mark.skipif(
-#     reason="The test is either incorrect or using wrong ref values",
-# )
-# def test_incoherence_correction_step4only(reference_dir, temp_directory):
-#     """Test incoherence correction without elastic correction"""
-#     # Set up the configuration dict
-#     configuration = generate_configuration_with_correction()
-#
-#     # Create temp output directory
-#     test_dir = temp_directory()
-#     base_name = "EQSANS_113915_Incoh_1d"
-#
-#     assert os.path.exists(test_dir), f"Output dir {test_dir} does not exit"
-#     configuration["configuration"]["outputDir"] = test_dir
-#     configuration["outputFileName"] = base_name
-#     configuration["dataDirectories"] = test_dir
-#
-#     # validate and clean configuration
-#     input_config = reduction_parameters(configuration)
-#     loaded = load_all_files(input_config)
-#
-#     # Reduce
-#     reduction_output = reduce_single_configuration(loaded, input_config, not_apply_incoherence_correction=False)
-#
-#     # Gold data directory
-#     gold_dir = os.path.join(reference_dir.eqsans, "gold_data/Incoherence_Corrected_113915/")
-#     assert os.path.exists(gold_dir), f"Gold/expected data directory {gold_dir} does not exist"
-#
-#     # Verify with gold data
-#     gold_file_dict = dict()
-#     for frame_index in range(1):
-#         iq1d_h5_name = os.path.join(gold_dir, f"EQSANS_11395iq1d_{frame_index}_0.h5")
-#         gold_file_dict[1, frame_index, 0] = iq1d_h5_name
-#         iq2d_h5_name = os.path.join(gold_dir, f"EQSANS_11395iq2d_{frame_index}.h5")
-#         gold_file_dict[2, frame_index] = iq2d_h5_name
-#         assert os.path.exists(iq1d_h5_name) and os.path.exists(iq2d_h5_name), (
-#             f"{iq1d_h5_name} and/or {iq2d_h5_name}" f"do not exist"
-#         )
-#
-#     # Verify
-#     verify_binned_iq(gold_file_dict, reduction_output)
 
 
 @pytest.mark.mount_eqsans
